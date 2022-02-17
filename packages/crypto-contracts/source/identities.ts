35c7dd8066b99f83f3028105be2a8336a680849f
Brian Faust <hello@basecode.sh>
2022-02-17 04:48:55 +0200

    feat(crypto-keypair-secp256k1): initial implementation (#44)

--- packages/crypto-contracts/source/identities.ts
+++ packages/crypto-contracts/source/identities.ts
@@ -3,3 +3,9 @@ export interface IKeyPair {
     privateKey: string;
     compressed: boolean;
 }
+
+export interface IKeyPairFactory {
+    fromMnemonic(mnemonic: string, compressed?: boolean): IKeyPair;
+
+    fromPrivateKey(privateKey: Buffer | string, compressed?: boolean): IKeyPair;
+}
