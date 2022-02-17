3ce2c8511934d9e28a64a43ed74f18c8fa8787fd
Brian Faust <hello@basecode.sh>
2022-02-17 04:41:35 +0200

    feat(crypto-signatures-schnorr): initial implementation (#39)

---
+++ packages/crypto-signatures-schnorr/source/index.ts
@@ -0,0 +1,16 @@
+import { Signatory as Contract } from "@arkecosystem/crypto-contracts";
+import { schnorr } from "bcrypto";
+
+export class Signatory implements Contract {
+    public sign(hash: Buffer, privateKey: Buffer): string {
+        return schnorr.sign(hash, privateKey).toString("hex");
+    }
+
+    public verify(hash: Buffer, signature: Buffer | string, publicKey: Buffer | string): boolean {
+        return schnorr.verify(
+            hash,
+            signature instanceof Buffer ? signature : Buffer.from(signature, "hex"),
+            publicKey instanceof Buffer ? publicKey : Buffer.from(publicKey, "hex"),
+        );
+    }
+}
