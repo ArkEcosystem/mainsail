bcb346ec77b33652a800ab7aa23b9573a4921e96
Brian Faust <hello@basecode.sh>
2022-02-18 12:07:00 +0200

    feat(crypto-hash-noble): initial implementation (#91)

---
+++ packages/crypto-hash-noble/source/index.test.ts
@@ -0,0 +1,26 @@
+import { describe } from "@arkecosystem/core-test-framework";
+
+import { HashFactory } from "./index";
+
+describe("HashFactory", ({ assert, it }) => {
+	it("should create a hash with the RIPEMD160 method", async () => {
+		assert.is(
+			Buffer.from(await new HashFactory().ripemd160(Buffer.from("Hello World"))).toString("hex"),
+			"a830d7beb04eb7549ce990fb7dc962e499a27230",
+		);
+	});
+
+	it("should create a hash with the SHA256 method", async () => {
+		assert.is(
+			Buffer.from(await new HashFactory().sha256(Buffer.from("Hello World"))).toString("hex"),
+			"a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e",
+		);
+	});
+
+	it("should create a hash with the HASH256 method", async () => {
+		assert.is(
+			Buffer.from(await new HashFactory().hash256(Buffer.from("Hello World"))).toString("hex"),
+			"42a873ac3abd02122d27e80486c6fa1ef78694e8505fcec9cbcc8a7728ba8949",
+		);
+	});
+});
---
+++ packages/crypto-hash-noble/source/index.ts
@@ -0,0 +1,17 @@
+import { IHashFactory as Contract } from "@arkecosystem/crypto-contracts";
+import { ripemd160 } from "@noble/hashes/ripemd160";
+import { sha256 } from "@noble/hashes/sha256";
+
+export class HashFactory implements Contract {
+	public async ripemd160(data: Buffer): Promise<Buffer> {
+		return Buffer.from(ripemd160(data));
+	}
+
+	public async sha256(data: Buffer): Promise<Buffer> {
+		return Buffer.from(sha256(data));
+	}
+
+	public async hash256(data: Buffer): Promise<Buffer> {
+		return Buffer.from(sha256(Buffer.from(sha256(data))));
+	}
+}
