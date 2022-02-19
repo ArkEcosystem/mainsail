86b0eabd2d1cdeb6b95e85bc2ed28c2d364339fe
Brian Faust <hello@basecode.sh>
2022-02-19 06:52:18 +0200

    feat(crypto-key-pair-ed25519): initial implementation (#103)

--- packages/crypto-key-pair-ed25519/source/index.ts
+++ packages/crypto-key-pair-ed25519/source/index.ts
@@ -1,22 +1,22 @@
 import { IKeyPair, IKeyPairFactory as Contract } from "@arkecosystem/crypto-contracts";
-import { getPublicKey } from "@noble/bls12-381";
-import { deriveChild, deriveMaster } from "bls12-381-keygen";
+import { getPublicKey } from "@noble/ed25519";
 import { mnemonicToSeedSync } from "@scure/bip39";
+import { sha256 } from "@noble/hashes/sha256";
 
 export class KeyPairFactory implements Contract {
 	public async fromMnemonic(mnemonic: string): Promise<IKeyPair> {
-		return this.#fromPrivateKey(deriveChild(deriveMaster(mnemonicToSeedSync(mnemonic)), 0));
+		return this.#fromPrivateKey(sha256(mnemonicToSeedSync(mnemonic)));
 	}
 
 	public async fromPrivateKey(privateKey: Buffer): Promise<IKeyPair> {
 		return this.#fromPrivateKey(privateKey);
 	}
 
-	#fromPrivateKey(privateKey: Uint8Array): IKeyPair {
+	async #fromPrivateKey(privateKey: Uint8Array): Promise<IKeyPair> {
 		return {
 			compressed: true,
 			privateKey: Buffer.from(privateKey).toString("hex"),
-			publicKey: Buffer.from(getPublicKey(privateKey)).toString("hex"),
+			publicKey: Buffer.from(await getPublicKey(privateKey)).toString("hex"),
 		};
 	}
 }
