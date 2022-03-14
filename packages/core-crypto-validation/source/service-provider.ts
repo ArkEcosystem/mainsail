4e9c1dae4c505ab756dbfa8fbee0744cb0b07879
Josip Crnković <crnkovic@users.noreply.github.com>
2022-03-14 10:18:43 +0100

    refactor(core-crypto-validation): replace `private` keyword with `#` (#255)

--- packages/core-crypto-validation/source/service-provider.ts
+++ packages/core-crypto-validation/source/service-provider.ts
@@ -7,14 +7,14 @@ import { schemas } from "./schemas";
 
 export class ServiceProvider extends Providers.ServiceProvider {
 	public async register(): Promise<void> {
-		await this.registerFormats();
+		await this.#registerFormats();
 
-		await this.registerKeywords();
+		await this.#registerKeywords();
 
-		await this.registerSchemas();
+		await this.#registerSchemas();
 	}
 
-	private async registerFormats(): Promise<void> {
+	async #registerFormats(): Promise<void> {
 		for (const [name, format] of Object.entries(
 			registerFormats(this.app.get(Identifiers.Cryptography.Configuration)),
 		)) {
@@ -23,7 +23,7 @@ export class ServiceProvider extends Providers.ServiceProvider {
 		}
 	}
 
-	private async registerKeywords(): Promise<void> {
+	async #registerKeywords(): Promise<void> {
 		for (const [name, format] of Object.entries(
 			registerKeywords(this.app.get(Identifiers.Cryptography.Configuration)),
 		)) {
@@ -32,7 +32,7 @@ export class ServiceProvider extends Providers.ServiceProvider {
 		}
 	}
 
-	private async registerSchemas(): Promise<void> {
+	async #registerSchemas(): Promise<void> {
 		for (const schema of Object.values(schemas)) {
 			this.app.get<Contracts.Crypto.IValidator>(Identifiers.Cryptography.Validator).addSchema(schema);
 		}
