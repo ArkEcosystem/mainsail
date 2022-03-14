ae47b01225958dc48f0b7e95d7b3f540e6c02e81
Mariano Goldman <marianogoldman@users.noreply.github.com>
2022-03-14 07:35:03 -0300

    refactor(core-libp2p): replace `private` keyword with `#` (#259)

--- packages/core-libp2p/source/server.ts
+++ packages/core-libp2p/source/server.ts
@@ -10,21 +10,21 @@ export class Server {
 	@inject(Identifiers.LogService)
 	private readonly logger: Contracts.Kernel.Logger;
 
-	private server: libp2p;
+	#server: libp2p;
 
 	public async register(options: libp2p.Libp2pOptions): Promise<void> {
-		this.server = await libp2p.create(options);
+		this.#server = await libp2p.create(options);
 	}
 
 	public async boot(): Promise<void> {
 		try {
-			await this.server.pubsub.start();
+			await this.#server.pubsub.start();
 
-			await this.server.start();
+			await this.#server.start();
 
-			for (const addr of this.server.multiaddrs) {
+			for (const addr of this.#server.multiaddrs) {
 				this.logger.info(
-					`P2P server started listening on address: ${addr.toString()}/p2p/${this.server.peerId.toB58String()}`,
+					`P2P server started listening on address: ${addr.toString()}/p2p/${this.#server.peerId.toB58String()}`,
 				);
 			}
 		} catch {
@@ -34,31 +34,31 @@ export class Server {
 
 	public async dispose(): Promise<void> {
 		try {
-			for (const addr of this.server.multiaddrs) {
+			for (const addr of this.#server.multiaddrs) {
 				this.logger.info(
-					`P2P server stopped listening on address: ${addr.toString()}/p2p/${this.server.peerId.toB58String()}`,
+					`P2P server stopped listening on address: ${addr.toString()}/p2p/${this.#server.peerId.toB58String()}`,
 				);
 			}
 
-			await this.server.stop();
+			await this.#server.stop();
 		} catch {
 			await this.app.terminate(`Failed to stop P2P server!`);
 		}
 	}
 
 	public listen(event: string, listener: (...arguments_: any[]) => void): void {
-		this.server.pubsub.on(event, listener);
+		this.#server.pubsub.on(event, listener);
 	}
 
 	public unlisten(event: string, listener: (...arguments_: any[]) => void): void {
-		this.server.pubsub.off(event, listener);
+		this.#server.pubsub.off(event, listener);
 	}
 
 	public subscribe(topic: string): void {
-		this.server.pubsub.subscribe(topic);
+		this.#server.pubsub.subscribe(topic);
 	}
 
 	public unsubscribe(topic: string): void {
-		this.server.pubsub.unsubscribe(topic);
+		this.#server.pubsub.unsubscribe(topic);
 	}
 }
