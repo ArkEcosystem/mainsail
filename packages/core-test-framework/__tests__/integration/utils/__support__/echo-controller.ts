2dccfa701d30e2da1fb2dbed0cc4f36d18ce1939
Brian Faust <hello@basecode.sh>
2022-03-15 07:02:17 +0200

    chore: remove outdated tests (#274)

--- packages/core-test-framework/__tests__/integration/utils/__support__/echo-controller.ts
+++
@@ -1,9 +0,0 @@
-import Hapi from "@hapi/hapi";
-import { Container } from "@packages/core-kernel";
-
-@injectable()
-export class EchoController {
-	public index(request: Hapi.Request): { query: Hapi.RequestQuery; payload: Hapi.RequestPayload } {
-		return { query: request.query, payload: request.payload };
-	}
-}
