8ba4543493831f6f2bbaba79bf0938b447713508
Brian Faust <hello@basecode.sh>
2022-03-15 06:42:24 +0200

    chore(crypto): remove some outdated tests (#272)

--- packages/crypto/source/identities/wif.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe } from "@arkecosystem/core-test-framework";
-import { WIF } from "./wif";
-import { data, passphrase } from "../../test/identities/fixture.json";
-
-describe("Identities - WIF", ({ it, assert }) => {
-	it("fromPassphrase - should be OK", () => {
-		assert.equal(WIF.fromPassphrase(passphrase), data.wif);
-	});
-});
