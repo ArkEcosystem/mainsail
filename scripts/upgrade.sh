d6502433835440750c9c6da2e28818b26c08ca58
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-05-04 09:02:28 +0000

    Rename core-forger* to forger*

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,11 +2,11 @@
 
 pm2 delete ark-core > /dev/null 2>&1
 pm2 delete ark-core-relay > /dev/null 2>&1
-pm2 delete ark-core-forger > /dev/null 2>&1
+pm2 delete ark-forger > /dev/null 2>&1
 
 pm2 delete core > /dev/null 2>&1
 pm2 delete core-relay > /dev/null 2>&1
-pm2 delete core-forger > /dev/null 2>&1
+pm2 delete forger > /dev/null 2>&1
 
 node ./scripts/upgrade/upgrade.js
 
