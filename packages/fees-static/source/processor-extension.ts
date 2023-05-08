6036f0f1bd798f743b3144a06480c96745e789ca
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-05-08 15:27:26 +0000

    Make interface from ProcessorExtension

--- packages/fees-static/source/processor-extension.ts
+++ packages/fees-static/source/processor-extension.ts
@@ -2,7 +2,7 @@ import { inject, injectable } from "@mainsail/container";
 import { Contracts, Identifiers } from "@mainsail/contracts";
 
 @injectable()
-export class ProcessorExtension extends Contracts.TransactionPool.ProcessorExtension {
+export class ProcessorExtension implements Contracts.TransactionPool.ProcessorExtension {
 	@inject(Identifiers.Fee.Matcher)
 	private readonly feeMatcher!: Contracts.TransactionPool.FeeMatcher;
 
