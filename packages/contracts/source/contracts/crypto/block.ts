72bd16782253c82b9968f6c0ccf5f436ce375872
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-05-11 11:43:07 +0000

    Remove signatures form block

--- packages/contracts/source/contracts/crypto/block.ts
+++ packages/contracts/source/contracts/crypto/block.ts
@@ -31,7 +31,6 @@ export interface IBlockData {
 	payloadHash: string;
 	generatorPublicKey: string;
 
-	blockSignature?: string;
 	serialized?: string;
 	transactions?: ITransactionData[];
 }
@@ -51,7 +50,6 @@ export interface IBlockJson {
 	payloadHash: string;
 	generatorPublicKey: string;
 
-	blockSignature?: string;
 	serialized?: string;
 	transactions?: ITransactionJson[];
 }
