33e2abaf412d2800919b6bc79dc5081ef74c4bbf
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-05-11 07:13:42 +0000

    Fix container tests

--- packages/container/source/decorator.test.ts
+++ packages/container/source/decorator.test.ts
@@ -1,6 +1,6 @@
 import { Container } from "inversify";
 
-import { describe } from "../../../test-framework";
+import { describe } from "../../test-framework";
 import { decorateInjectable } from "./decorator";
 
 class ThirdPartyClass {}
