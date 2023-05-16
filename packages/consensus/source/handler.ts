615be13e20a1c736a14177efeaff592cf5b9c04c
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-05-16 13:10:44 +0000

    Forward proposal

--- packages/consensus/source/handler.ts
+++ packages/consensus/source/handler.ts
@@ -1,13 +1,23 @@
-import { injectable } from "@mainsail/container";
+import { inject, injectable } from "@mainsail/container";
+import { Contracts, Identifiers } from "@mainsail/contracts";
 
 import { Precommit } from "./precommit";
 import { Prevote } from "./prevote";
 import { Proposal } from "./proposal";
-import { IHandler } from "./types";
+import { IConsensus, IHandler } from "./types";
 
 @injectable()
 export class Handler implements IHandler {
-	async onProposal(proposal: Proposal): Promise<void> {}
+	@inject(Identifiers.Application)
+	private readonly app!: Contracts.Kernel.Application;
+
+	async onProposal(proposal: Proposal): Promise<void> {
+		await this.#getConsensus().onProposal(proposal);
+	}
 	async onPrevote(prevote: Prevote): Promise<void> {}
 	async onPrecommit(precommit: Precommit): Promise<void> {}
+
+	#getConsensus(): IConsensus {
+		return this.app.get<IConsensus>(Identifiers.Consensus.Service);
+	}
 }
