d1b8e587ffca23ef08f935419976e7e3759a4f23
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-05-16 11:36:35 +0000

    Use bound validator.secrets

--- packages/consensus/source/index.ts
+++ packages/consensus/source/index.ts
@@ -1,7 +1,6 @@
 import { Contracts, Identifiers } from "@mainsail/contracts";
 import { Providers } from "@mainsail/kernel";
 
-import { secrets } from "../../core/bin/config/testnet/validators.json";
 import { Consensus } from "./consensus";
 import { Validator } from "./validator";
 
@@ -11,7 +10,9 @@ export class ServiceProvider extends Providers.ServiceProvider {
 			Identifiers.Cryptography.Identity.KeyPairFactory,
 		);
 
-		const keyPairs = await Promise.all(secrets.map(async (menonic) => await keyPairFactory.fromMnemonic(menonic)));
+		const keyPairs = await Promise.all(
+			this.app.config("validators.secrets").map(async (menonic) => await keyPairFactory.fromMnemonic(menonic)),
+		);
 		const validators = keyPairs.map((keyPair) => this.app.resolve<Validator>(Validator).configure(keyPair));
 
 		this.app.bind(Identifiers.Consensus.Service).toConstantValue(
@@ -20,5 +21,9 @@ export class ServiceProvider extends Providers.ServiceProvider {
 				validators,
 			),
 		);
+	}
+
+	public async boot(): Promise<void> {
+		void this.app.get<Consensus>(Identifiers.Consensus.Service).run();
 	}
 }
