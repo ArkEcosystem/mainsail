e10ab1e1fd04487b89e8a72839ec357524198caa
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-05-17 11:02:42 +0000

    Implement and test scheduler

--- packages/consensus/source/scheduler.test.ts
+++ packages/consensus/source/scheduler.test.ts
@@ -7,7 +7,11 @@ describe<{
 	sandbox: Sandbox;
 	scheduler: Scheduler;
 }>("Scheduler", ({ beforeEach, it, assert, spy, clock }) => {
+	const delays = [1000, 2000, 4000];
+
 	const consensus = {
+		onTimeoutPrecommit: () => {},
+		onTimeoutPrevote: () => {},
 		onTimeoutPropose: () => {},
 	};
 
@@ -23,16 +27,99 @@ describe<{
 		assert.instance(scheduler, Scheduler);
 	});
 
-	it("should call onTimeoutPropose", async ({ scheduler }) => {
+	it("#scheduleTimeoutPropose - should call onTimeoutPropose ", async ({ scheduler }) => {
 		const fakeTimers = clock();
 		const spyOnTimeoutPropose = spy(consensus, "onTimeoutPropose");
 
-		const promise = scheduler.scheduleTimeoutPropose(1, 0);
-
-		fakeTimers.tick(1000);
-
-		await promise;
+		void scheduler.scheduleTimeoutPropose(1, 2);
+		await fakeTimers.nextAsync();
 
 		spyOnTimeoutPropose.calledOnce();
+	});
+
+	it("#scheduleTimeoutPropose - should increase delay on higher round ", async ({ scheduler }) => {
+		const fakeTimers = clock();
+
+		const timerValues: number[] = [];
+
+		void scheduler.scheduleTimeoutPropose(1, 0);
+		await fakeTimers.nextAsync();
+		timerValues.push(fakeTimers.now);
+		fakeTimers.now = 0;
+
+		void scheduler.scheduleTimeoutPropose(1, 1);
+		await fakeTimers.nextAsync();
+		timerValues.push(fakeTimers.now);
+		fakeTimers.now = 0;
+
+		void scheduler.scheduleTimeoutPropose(1, 3);
+		await fakeTimers.nextAsync();
+		timerValues.push(fakeTimers.now);
+
+		assert.equal(timerValues, delays);
+	});
+
+	it("#scheduleTimeoutPrevote - should call onTimeoutPrevote ", async ({ scheduler }) => {
+		const fakeTimers = clock();
+		const spyOnTimeoutPropose = spy(consensus, "onTimeoutPrevote");
+
+		void scheduler.scheduleTimeoutPrevote(1, 2);
+		await fakeTimers.nextAsync();
+
+		spyOnTimeoutPropose.calledOnce();
+	});
+
+	it("#scheduleTimeoutPrevote - should increase delay on higher round ", async ({ scheduler }) => {
+		const fakeTimers = clock();
+
+		const timerValues: number[] = [];
+
+		void scheduler.scheduleTimeoutPrevote(1, 0);
+		await fakeTimers.nextAsync();
+		timerValues.push(fakeTimers.now);
+		fakeTimers.now = 0;
+
+		void scheduler.scheduleTimeoutPrevote(1, 1);
+		await fakeTimers.nextAsync();
+		timerValues.push(fakeTimers.now);
+		fakeTimers.now = 0;
+
+		void scheduler.scheduleTimeoutPrevote(1, 3);
+		await fakeTimers.nextAsync();
+		timerValues.push(fakeTimers.now);
+
+		assert.equal(timerValues, delays);
+	});
+
+	it("#scheduleTimeoutPrecommit - should call onTimeoutPrecommit ", async ({ scheduler }) => {
+		const fakeTimers = clock();
+		const spyOnTimeoutPropose = spy(consensus, "onTimeoutPrecommit");
+
+		void scheduler.scheduleTimeoutPrecommit(1, 2);
+		await fakeTimers.nextAsync();
+
+		spyOnTimeoutPropose.calledOnce();
+	});
+
+	it("#scheduleTimeoutPrecommit - should increase delay on higher round ", async ({ scheduler }) => {
+		const fakeTimers = clock();
+
+		const timerValues: number[] = [];
+
+		void scheduler.scheduleTimeoutPrecommit(1, 0);
+		await fakeTimers.nextAsync();
+		timerValues.push(fakeTimers.now);
+		fakeTimers.now = 0;
+
+		void scheduler.scheduleTimeoutPrecommit(1, 1);
+		await fakeTimers.nextAsync();
+		timerValues.push(fakeTimers.now);
+		fakeTimers.now = 0;
+
+		void scheduler.scheduleTimeoutPrecommit(1, 3);
+		await fakeTimers.nextAsync();
+		timerValues.push(fakeTimers.now);
+
+		assert.equal(timerValues, delays);
 	});
 });
--- packages/consensus/source/scheduler.ts
+++ packages/consensus/source/scheduler.ts
@@ -10,14 +10,24 @@ export class Scheduler implements IScheduler {
 	private readonly app!: Contracts.Kernel.Application;
 
 	public async scheduleTimeoutPropose(height: number, round: number): Promise<void> {
-		await delay(1000);
-
+		await this.#wait(round);
 		await this.#getConsensus().onTimeoutPropose(height, round);
 	}
 
-	public async scheduleTimeoutPrevote(height: number, round: number): Promise<void> {}
+	public async scheduleTimeoutPrevote(height: number, round: number): Promise<void> {
+		await this.#wait(round);
+		await this.#getConsensus().onTimeoutPrevote(height, round);
+	}
 
-	public async scheduleTimeoutPrecommit(height: number, round: number): Promise<void> {}
+	public async scheduleTimeoutPrecommit(height: number, round: number): Promise<void> {
+		await this.#wait(round);
+		await this.#getConsensus().onTimeoutPrecommit(height, round);
+	}
+
+	async #wait(round: number) {
+		// TODO: Find a better way to calculate the delay
+		await delay((round + 1) * 1000);
+	}
 
 	#getConsensus(): IConsensus {
 		return this.app.get<IConsensus>(Identifiers.Consensus.Service);
