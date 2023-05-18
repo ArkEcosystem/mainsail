df97b8752be622e2b2f43cb76b059aedd300eed1
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-05-18 11:56:40 +0000

    Implements validator set

---
+++ packages/consensus/source/validator-set.ts
@@ -0,0 +1,27 @@
+import { inject, injectable, tagged } from "@mainsail/container";
+import { Contracts, Identifiers } from "@mainsail/contracts";
+
+@injectable()
+export class ValidatorSet {
+	@inject(Identifiers.Application)
+	private readonly app!: Contracts.Kernel.Application;
+
+	@inject(Identifiers.WalletRepository)
+	@tagged("state", "blockchain")
+	private readonly walletRepository!: Contracts.State.WalletRepository;
+
+	@inject(Identifiers.Cryptography.Identity.KeyPairFactory)
+	private readonly keyPairFactory!: Contracts.Crypto.IKeyPairFactory;
+
+	async getActiveValidators(height: number, round: number): Promise<Contracts.State.Wallet[]> {
+		const keyPairs = await Promise.all(
+			this.app
+				.config("validators.secrets")
+				.map(async (menonic) => await this.keyPairFactory.fromMnemonic(menonic)),
+		);
+
+		return await Promise.all(
+			keyPairs.map(async (keyPair) => await this.walletRepository.findByPublicKey(keyPair.publicKey)),
+		);
+	}
+}
