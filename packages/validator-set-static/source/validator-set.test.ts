5026f091fb907c2375312395084268a71c70d1c7
oXtxNt9U <oXtxNt9U@users.noreply.github.com>
2023-05-23 05:31:24 +0000

    style: resolve style guide violations

--- packages/validator-set-static/source/validator-set.test.ts
+++ packages/validator-set-static/source/validator-set.test.ts
@@ -4,23 +4,24 @@ import { prepareSandbox } from "../test/helpers/prepare-sandbox";
 import { ValidatorSet } from "./validator-set";
 
 describe<{
-    sandbox: Sandbox;
-    validatorSet: ValidatorSet;
+	sandbox: Sandbox;
+	validatorSet: ValidatorSet;
 }>("ValidatorSet", ({ it, assert, beforeEach }) => {
-    beforeEach(async (context) => {
-        await prepareSandbox(context);
+	beforeEach(async (context) => {
+		await prepareSandbox(context);
 
-        context.sandbox.app.get<Contracts.Kernel.Repository>(Identifiers.ConfigRepository)
-            .set("validators.secrets", [
-                "endless deposit bright clip school doctor later surround strategy blouse damage drink diesel erase scrap inside over pledge talent blood bus luggage glad whale",
-                "number hero hen release sock solution powder front museum dignity tell invest turkey blast arrest resemble program rule grace card dash error fat fog",
-            ]);
+		context.sandbox.app
+			.get<Contracts.Kernel.Repository>(Identifiers.ConfigRepository)
+			.set("validators.secrets", [
+				"endless deposit bright clip school doctor later surround strategy blouse damage drink diesel erase scrap inside over pledge talent blood bus luggage glad whale",
+				"number hero hen release sock solution powder front museum dignity tell invest turkey blast arrest resemble program rule grace card dash error fat fog",
+			]);
 
-        context.validatorSet = await context.sandbox.app.resolve(ValidatorSet).configure();
-    });
+		context.validatorSet = await context.sandbox.app.resolve(ValidatorSet).configure();
+	});
 
-    it("#getActiveValidators - should return active validators", async ({ validatorSet }) => {
-        const validators = await validatorSet.getActiveValidators();
-        assert.equal(validators.length, 2);
-    });
+	it("#getActiveValidators - should return active validators", async ({ validatorSet }) => {
+		const validators = await validatorSet.getActiveValidators();
+		assert.equal(validators.length, 2);
+	});
 });
--- packages/validator-set-static/test/helpers/prepare-sandbox.ts
+++ packages/validator-set-static/test/helpers/prepare-sandbox.ts
@@ -20,29 +20,29 @@ import { ServiceProvider as CoreState } from "../../../state";
 import { Configuration } from "../../../crypto-config/source/configuration";
 
 export const prepareSandbox = async (context) => {
-    context.sandbox = new Sandbox();
+	context.sandbox = new Sandbox();
 
-    await context.sandbox.app.resolve(CoreTriggers).register();
-    await context.sandbox.app.resolve(CoreEvents).register();
+	await context.sandbox.app.resolve(CoreTriggers).register();
+	await context.sandbox.app.resolve(CoreEvents).register();
 
-    await context.sandbox.app.resolve(CoreSerializer).register();
-    await context.sandbox.app.resolve(CoreValidation).register();
-    await context.sandbox.app.resolve(CoreCryptoConfig).register();
+	await context.sandbox.app.resolve(CoreSerializer).register();
+	await context.sandbox.app.resolve(CoreValidation).register();
+	await context.sandbox.app.resolve(CoreCryptoConfig).register();
 
-    await context.sandbox.app.resolve(CoreCryptoHashBcrypto).register();
+	await context.sandbox.app.resolve(CoreCryptoHashBcrypto).register();
 
-    await context.sandbox.app.resolve(CoreCryptoSignatureSchnorr).register();
-    await context.sandbox.app.resolve(CoreCryptoKeyPairSchnorr).register();
+	await context.sandbox.app.resolve(CoreCryptoSignatureSchnorr).register();
+	await context.sandbox.app.resolve(CoreCryptoKeyPairSchnorr).register();
 
-    await context.sandbox.app.resolve(CoreCryptoAddressBech32m).register();
-    await context.sandbox.app.resolve(CoreCryptoWif).register();
-    await context.sandbox.app.resolve(CoreConsensusBls12381).register();
-    await context.sandbox.app.resolve(CoreState).register();
-    await context.sandbox.app.resolve(CoreCryptoTransaction).register();
-    await context.sandbox.app.resolve(CoreTransactions).register();
+	await context.sandbox.app.resolve(CoreCryptoAddressBech32m).register();
+	await context.sandbox.app.resolve(CoreCryptoWif).register();
+	await context.sandbox.app.resolve(CoreConsensusBls12381).register();
+	await context.sandbox.app.resolve(CoreState).register();
+	await context.sandbox.app.resolve(CoreCryptoTransaction).register();
+	await context.sandbox.app.resolve(CoreTransactions).register();
 
-    // @ts-ignore
-    context.sandbox.app.get<Services.Attributes.AttributeSet>(Identifiers.WalletAttributes).set("consensus.publicKey");
-    // @ts-ignore
-    context.sandbox.app.get<Configuration>(Identifiers.Cryptography.Configuration).setConfig(crypto);
+	// @ts-ignore
+	context.sandbox.app.get<Services.Attributes.AttributeSet>(Identifiers.WalletAttributes).set("consensus.publicKey");
+	// @ts-ignore
+	context.sandbox.app.get<Configuration>(Identifiers.Cryptography.Configuration).setConfig(crypto);
 };
