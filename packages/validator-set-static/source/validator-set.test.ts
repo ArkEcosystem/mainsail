28e2a9a9e05de78cd659a298cb5113a8305bb123
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2023-05-23 15:48:47 +0900

    test(validator-set-static): increase test coverage (#61)
    * tests
    
    * style: resolve style guide violations
    
    ---------
    
    Co-authored-by: oXtxNt9U <oXtxNt9U@users.noreply.github.com>

---
+++ packages/validator-set-static/source/validator-set.test.ts
@@ -0,0 +1,27 @@
+import { Contracts, Identifiers } from "@mainsail/contracts";
+import { describe, Sandbox } from "../../test-framework";
+import { prepareSandbox } from "../test/helpers/prepare-sandbox";
+import { ValidatorSet } from "./validator-set";
+
+describe<{
+	sandbox: Sandbox;
+	validatorSet: ValidatorSet;
+}>("ValidatorSet", ({ it, assert, beforeEach }) => {
+	beforeEach(async (context) => {
+		await prepareSandbox(context);
+
+		context.sandbox.app
+			.get<Contracts.Kernel.Repository>(Identifiers.ConfigRepository)
+			.set("validators.secrets", [
+				"endless deposit bright clip school doctor later surround strategy blouse damage drink diesel erase scrap inside over pledge talent blood bus luggage glad whale",
+				"number hero hen release sock solution powder front museum dignity tell invest turkey blast arrest resemble program rule grace card dash error fat fog",
+			]);
+
+		context.validatorSet = await context.sandbox.app.resolve(ValidatorSet).configure();
+	});
+
+	it("#getActiveValidators - should return active validators", async ({ validatorSet }) => {
+		const validators = await validatorSet.getActiveValidators();
+		assert.equal(validators.length, 2);
+	});
+});
