0eeb1569d636ad65ea43f47e17efb2e4a36150c0
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-05-29 10:49:56 +0000

    Fix tests

--- packages/crypto-messages/source/proposal.test.ts
+++ packages/crypto-messages/source/proposal.test.ts
@@ -15,6 +15,7 @@ describe<{
 	const proposal = new Proposal(
 		1,
 		1,
+		-1,
 		block,
 		"95af988701a6fb60e09da41d2ca1a9e0b49e43501bda4255b3ca01073f490c34102b6bbcafde6333185e9980745d72cb",
 		"b22317bfdb10ba592724c27d0cdc51378e5cd94a12cd7e85c895d2a68e8589e8d3c5b3c80f4fe905ef67aa7827617d04110c5c5248f2bb36df97a58c541961ed0f2fcd0760e9de5ae1598f27638dd3ddaebeea08bf313832a57cfdb7f2baaa03",
@@ -26,6 +27,10 @@ describe<{
 
 	it("#round", async () => {
 		assert.equal(proposal.round, 1);
+	});
+
+	it("#validRound", async () => {
+		assert.equal(proposal.validRound, -1);
 	});
 
 	it("#block", async () => {
@@ -57,6 +62,7 @@ describe<{
 		assert.equal(proposal.toData(), {
 			height: 1,
 			round: 1,
+			validRound: -1,
 			block,
 			signature:
 				"b22317bfdb10ba592724c27d0cdc51378e5cd94a12cd7e85c895d2a68e8589e8d3c5b3c80f4fe905ef67aa7827617d04110c5c5248f2bb36df97a58c541961ed0f2fcd0760e9de5ae1598f27638dd3ddaebeea08bf313832a57cfdb7f2baaa03",
