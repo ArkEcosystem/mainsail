7aad233d409db55b0bd412f0860a50b6e8cf9ff7
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-06-07 17:42:10 +0000

    Add codec

--- packages/p2p/source/socket-server/codecs/index.ts
+++ packages/p2p/source/socket-server/codecs/index.ts
@@ -3,6 +3,7 @@ import { getCommonBlocks } from "./get-common-blocks";
 import { getPeers } from "./get-peers";
 import { getStatus } from "./get-status";
 import { postBlock } from "./post-block";
+import { postPrecommit } from "./post-precommit";
 import { postPrevote } from "./post-prevote";
 import { postTransactions } from "./post-transactions";
 
@@ -12,6 +13,7 @@ export const Codecs = {
 	getPeers,
 	getStatus,
 	postBlock,
+	postPrecommit,
 	postPrevote,
 	postTransactions,
 };
---
+++ packages/p2p/source/socket-server/codecs/post-precommit.ts
@@ -0,0 +1,20 @@
+import { postPrecommit as proto } from "./proto/protos";
+
+export const postPrecommit = {
+	request: {
+		deserialize: (payload: Buffer) => {
+			const decoded = proto.PostPrecommitRequest.decode(payload);
+			return {
+				...decoded,
+				block: Buffer.from(decoded.prevote),
+			};
+		},
+		serialize: (object: proto.IPostPrecommitRequest): Buffer =>
+			Buffer.from(proto.PostPrecommitRequest.encode(object).finish()),
+	},
+	response: {
+		deserialize: (payload: Buffer): {} => proto.PostPrecommitResponse.decode(payload),
+		serialize: (object: proto.IPostPrecommitResponse): Buffer =>
+			Buffer.from(proto.PostPrecommitResponse.encode(object).finish()),
+	},
+};
