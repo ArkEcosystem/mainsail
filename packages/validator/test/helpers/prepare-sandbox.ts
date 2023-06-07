bdf2069289adbf0d7e82bf3fe63b936892261c29
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-06-07 10:48:00 +0000

    Fix validator tests

--- packages/validator/test/helpers/prepare-sandbox.ts
+++ packages/validator/test/helpers/prepare-sandbox.ts
@@ -1,26 +1,24 @@
 import { Identifiers } from "@mainsail/contracts";
 
 import crypto from "../../../core/bin/config/testnet/crypto.json";
-import { Sandbox } from "../../../test-framework";
-import { ServiceProvider as CoreTriggers } from "../../../kernel/source/services/triggers";
-import { ServiceProvider as CoreEvents } from "../../../kernel/source/services/events";
-import { ServiceProvider as CoreSerializer } from "../../../serializer";
-import { ServiceProvider as CoreValidation } from "../../../validation";
-import { ServiceProvider as CoreCryptoConfig } from "../../../crypto-config";
-import { ServiceProvider as CoreCryptoHashBcrypto } from "../../../crypto-hash-bcrypto";
 import { ServiceProvider as CoreCryptoAddressBech32m } from "../../../crypto-address-bech32m";
-import { ServiceProvider as CoreCryptoWif } from "../../../crypto-wif";
+import { ServiceProvider as CoreCryptoBlock } from "../../../crypto-block";
+import { ServiceProvider as CoreCryptoConfig } from "../../../crypto-config";
+import { Configuration } from "../../../crypto-config/source/configuration";
 import { ServiceProvider as CoreConsensusBls12381 } from "../../../crypto-consensus-bls12-381";
+import { ServiceProvider as CoreCryptoHashBcrypto } from "../../../crypto-hash-bcrypto";
 import { ServiceProvider as CoreCryptoKeyPairSchnorr } from "../../../crypto-key-pair-schnorr";
+import { ServiceProvider as CoreCryptoMessages } from "../../../crypto-messages";
 import { ServiceProvider as CoreCryptoSignatureSchnorr } from "../../../crypto-signature-schnorr";
 import { ServiceProvider as CoreCryptoTransaction } from "../../../crypto-transaction";
-import { ServiceProvider as CoreCryptoBlock } from "../../../crypto-block";
-import { ServiceProvider as CoreCryptoTime } from "../../../crypto-time";
-import { ServiceProvider as CoreCryptoMessages } from "../../../crypto-messages";
-import { ServiceProvider as CoreTransactions } from "../../../transactions";
+import { ServiceProvider as CoreCryptoWif } from "../../../crypto-wif";
+import { ServiceProvider as CoreEvents } from "../../../kernel/source/services/events";
+import { ServiceProvider as CoreTriggers } from "../../../kernel/source/services/triggers";
+import { ServiceProvider as CoreSerializer } from "../../../serializer";
 import { ServiceProvider as CoreState } from "../../../state";
-
-import { Configuration } from "../../../crypto-config/source/configuration";
+import { Sandbox } from "../../../test-framework";
+import { ServiceProvider as CoreTransactions } from "../../../transactions";
+import { ServiceProvider as CoreValidation } from "../../../validation";
 
 export const prepareSandbox = async (context) => {
 	context.sandbox = new Sandbox();
@@ -43,26 +41,21 @@ export const prepareSandbox = async (context) => {
 	await context.sandbox.app.resolve(CoreState).register();
 	await context.sandbox.app.resolve(CoreCryptoTransaction).register();
 	await context.sandbox.app.resolve(CoreTransactions).register();
-	await context.sandbox.app.resolve(CoreCryptoTime).register();
 	await context.sandbox.app.resolve(CoreCryptoBlock).register();
 	await context.sandbox.app.resolve(CoreCryptoMessages).register();
 
 	context.sandbox.app.bind(Identifiers.LogService).toConstantValue({});
 	context.sandbox.app.bind(Identifiers.TransactionPoolCollator).toConstantValue({
-		getBlockCandidateTransactions: () => {
-			return [];
-		},
+		getBlockCandidateTransactions: () => [],
 	});
 	context.sandbox.app.bind(Identifiers.TransactionPoolService).toConstantValue({});
 	context.sandbox.app.bind(Identifiers.Database.Service).toConstantValue({
-		getLastBlock: () => {
-			return {
-				data: {
-					height: 1,
-					id: "0000000000000000000000000000000000000000000000000000000000000000",
-				},
-			};
-		},
+		getLastBlock: () => ({
+			data: {
+				height: 1,
+				id: "0000000000000000000000000000000000000000000000000000000000000000",
+			},
+		}),
 	});
 
 	// @ts-ignore
