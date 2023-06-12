50c51ead73daa321a96834753086389eb78facc2
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-06-12 13:12:23 +0000

    Fix lint errors

--- packages/processor/source/verifiers/index.ts
+++ packages/processor/source/verifiers/index.ts
@@ -2,5 +2,5 @@ export * from "./chained-verifier";
 export * from "./forged-transactions-verifier";
 export * from "./incompatible-transactions-verifier";
 export * from "./nonce-verifier";
-// export * from "./validator-verifier";
+export * from "./validator-verifier";
 export * from "./verify-block-verifier";
