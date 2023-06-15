197f8d3ecbe8cfb68edcd62ef7117c713a3c55b7
sebastijankuzner <sebastijankuzner@users.noreply.github.com>
2023-06-15 09:14:40 +0000

    style: resolve style guide violations

--- packages/crypto-messages/source/prevote.test.ts
+++ packages/crypto-messages/source/prevote.test.ts
@@ -36,7 +36,10 @@ describe<{
 	});
 
 	it("#toString", async () => {
-		assert.equal(prevote.toString(), `{"height":1,"round":1,"signature":"b22317bfdb10ba592724c27d0cdc51378e5cd94a12cd7e85c895d2a68e8589e8d3c5b3c80f4fe905ef67aa7827617d04110c5c5248f2bb36df97a58c541961ed0f2fcd0760e9de5ae1598f27638dd3ddaebeea08bf313832a57cfdb7f2baaa03","validatorIndex":0}`);
+		assert.equal(
+			prevote.toString(),
+			`{"height":1,"round":1,"signature":"b22317bfdb10ba592724c27d0cdc51378e5cd94a12cd7e85c895d2a68e8589e8d3c5b3c80f4fe905ef67aa7827617d04110c5c5248f2bb36df97a58c541961ed0f2fcd0760e9de5ae1598f27638dd3ddaebeea08bf313832a57cfdb7f2baaa03","validatorIndex":0}`,
+		);
 	});
 
 	it("#toData", async () => {
