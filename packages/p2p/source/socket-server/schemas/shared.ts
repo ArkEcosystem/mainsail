8d1f63bec227da815e6328865b18f10509d96382
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-06-19 15:24:34 +0000

    Make field required

--- packages/p2p/source/socket-server/schemas/shared.ts
+++ packages/p2p/source/socket-server/schemas/shared.ts
@@ -1,8 +1,8 @@
 import Joi from "joi";
 
 export const headers = Joi.object({
-	height: Joi.number().integer().min(1),
-	round: Joi.number().integer().min(0),
-	step: Joi.number().integer().min(0).max(2),
-	version: Joi.string(),
-});
+	height: Joi.number().integer().min(1).required(),
+	round: Joi.number().integer().min(0).required(),
+	step: Joi.number().integer().min(0).max(2).required(),
+	version: Joi.string().required(),
+}).required();
