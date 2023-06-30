cf647b0a37f3d2c1118864e25d3ec6b7d87b5e6b
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2023-06-30 12:59:03 +0900

    shuffle validator index at beginning of each round

---
+++ packages/consensus/source/proposer-picker.test.ts
@@ -0,0 +1,75 @@
+import { Contracts, Identifiers } from "@mainsail/contracts";
+import { describe, Sandbox } from "@mainsail/test-framework";
+
+import { ProposerPicker } from "./proposer-picker";
+
+type Context = {
+	sandbox: Sandbox;
+	state: any;
+	validatorSet: any;
+	proposerPicker: ProposerPicker;
+	logger: any;
+};
+
+describe<Context>("ProposerPicker", ({ it, beforeEach, assert }) => {
+	beforeEach((context) => {
+		context.state = {
+			getLastBlock: () => { },
+			getLastCommittedRound: () => 0,
+		};
+		context.validatorSet = {
+			getActiveValidators: () => { },
+		};
+
+		context.logger = {
+			info: () => { },
+		};
+
+		context.sandbox = new Sandbox();
+
+		context.sandbox.app.bind(Identifiers.StateStore).toConstantValue(context.state);
+		context.sandbox.app.bind(Identifiers.Consensus.ProposerPicker).toConstantValue(context.proposerPicker);
+		context.sandbox.app.bind(Identifiers.LogService).toConstantValue(context.logger);
+
+		const config = {
+			getMilestone: () => ({
+				activeValidators: 51,
+			}),
+		};
+		context.sandbox.app.bind(Identifiers.Cryptography.Configuration).toConstantValue(config);
+
+		context.proposerPicker = context.sandbox.app.resolve(ProposerPicker);
+	});
+
+	it("#firstValidatorIndex - should return initial value", async ({ proposerPicker }) => {
+		assert.equal(proposerPicker.firstValidatorIndex, 0);
+	});
+
+	it("#getValidatorIndex - should return validator index for round", async ({ proposerPicker }) => {
+		for (let i = 0; i < 51; i++) {
+			assert.equal(proposerPicker.getValidatorIndex(i), i);
+		}
+
+		assert.equal(proposerPicker.getValidatorIndex(51), 0);
+		assert.equal(proposerPicker.getValidatorIndex(52), 1);
+	});
+
+	it("#handleCommittedBlock - should shuffle validator index on full round", async ({ proposerPicker }) => {
+		assert.equal(proposerPicker.firstValidatorIndex, 0);
+
+		for (let i = 0; i < 51; i++) {
+			proposerPicker.handleCommittedBlock({ height: i } as Contracts.Crypto.IBlockCommit);
+			assert.equal(proposerPicker.firstValidatorIndex, 13);
+		}
+
+		// Starts next round
+		proposerPicker.handleCommittedBlock({ height: 51 } as Contracts.Crypto.IBlockCommit);
+		assert.equal(proposerPicker.firstValidatorIndex, 25);
+
+		// shuffled index wraps around
+		for (let i = 0; i < 51; i++) {
+			const index = (proposerPicker.firstValidatorIndex + i) % 51
+			assert.equal(proposerPicker.getValidatorIndex(i), index);
+		}
+	});
+});
--- packages/consensus/source/proposer-picker.ts
+++ packages/consensus/source/proposer-picker.ts
@@ -10,12 +10,33 @@ export class ProposerPicker implements Contracts.Consensus.IProposerPicker {
 	@inject(Identifiers.StateStore)
 	private readonly state!: Contracts.State.StateStore;
 
-	public getValidatorIndex(round: number): number {
-		const seed = this.#calculateSeed(round);
+	#firstValidatorIndex: number = 0;
 
+	get firstValidatorIndex(): number { return this.#firstValidatorIndex };
+
+	public handleCommittedBlock(commit: Contracts.Crypto.IBlockCommit): void {
 		const { activeValidators } = this.configuration.getMilestone();
+
+		const { height: currentHeight } = commit;
+		const roundHeight = currentHeight - (currentHeight % activeValidators);
+
+		this.#updateValidatorIndex(activeValidators, roundHeight);
+	}
+
+	public getValidatorIndex(round: number): number {
+		const totalRound = this.#getTotalRound(round);
+		const { activeValidators } = this.configuration.getMilestone();
+		// On each round, move from the first index by offset with wrap around
+		// to traverse all active validators once each.
+		const offset = (totalRound - 1) % activeValidators;
+		return (this.#firstValidatorIndex + offset) % activeValidators;
+	}
+
+	#updateValidatorIndex(activeValidators: number, height: number): void {
+		// Calculate a random validator starting index for the next 'activeValidators' rounds.
+		const seed = this.#calculateSeed(height);
 		const rng = seedrandom(seed);
-		return Math.floor(rng() * (activeValidators - 1));
+		this.#firstValidatorIndex = Math.floor(rng() * (activeValidators - 1));
 	}
 
 	#calculateSeed(round: number): string {
