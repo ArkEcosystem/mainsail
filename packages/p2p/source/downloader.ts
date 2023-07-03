825f9f5c9f0386e597ebf357f769e9a0a0f76636
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-07-03 07:58:50 +0000

    Limit 1 parallel action

--- packages/p2p/source/downloader.ts
+++ packages/p2p/source/downloader.ts
@@ -18,49 +18,87 @@ export class Downloader {
 	@inject(Identifiers.Cryptography.Block.Factory)
 	private readonly blockFactory!: Contracts.Crypto.IBlockFactory;
 
+	#isDownloadingBlocks = false;
+	#isDownloadingProposal = false;
+	#isDownloadingMessages = false;
+
 	public async downloadBlocks(peer: Contracts.P2P.Peer): Promise<void> {
-		console.log("Downloading blocks");
+		if (this.#isDownloadingBlocks) {
+			return;
+		}
 
-		const result = await this.communicator.getBlocks(peer, {
-			fromHeight: this.state.getLastBlock().data.height + 1,
-		});
+		this.#isDownloadingBlocks = true;
 
-		const blocks = await Promise.all(
-			result.blocks.map(async (hex) => await this.blockFactory.fromCommittedBytes(Buffer.from(hex, "hex"))),
-		);
+		try {
+			const result = await this.communicator.getBlocks(peer, {
+				fromHeight: this.state.getLastBlock().data.height + 1,
+			});
 
-		for (const block of blocks) {
-			await this.handler.onCommittedBlock(block);
+			const blocks = await Promise.all(
+				result.blocks.map(async (hex) => await this.blockFactory.fromCommittedBytes(Buffer.from(hex, "hex"))),
+			);
+
+			for (const block of blocks) {
+				await this.handler.onCommittedBlock(block);
+			}
+		} catch {
+			// TODO: Handle errors
+		} finally {
+			this.#isDownloadingBlocks = false;
 		}
 	}
 
 	// TODO: Handle errors & response checks
 	public async downloadProposal(peer: Contracts.P2P.Peer): Promise<void> {
-		const result = await this.communicator.getProposal(peer);
-
-		if (result.proposal.length === 0) {
+		if (this.#isDownloadingProposal) {
 			return;
 		}
 
-		const proposal = await this.messageFactory.makeProposalFromBytes(Buffer.from(result.proposal, "hex"));
+		this.#isDownloadingProposal = true;
 
-		await this.handler.onProposal(proposal);
+		try {
+			const result = await this.communicator.getProposal(peer);
+
+			if (result.proposal.length === 0) {
+				return;
+			}
+
+			const proposal = await this.messageFactory.makeProposalFromBytes(Buffer.from(result.proposal, "hex"));
+
+			await this.handler.onProposal(proposal);
+		} catch {
+			// TODO: Handle errors
+		} finally {
+			this.#isDownloadingProposal = false;
+		}
 	}
 
 	// TODO: Handle errors
 	public async downloadMessages(peer: Contracts.P2P.Peer): Promise<void> {
-		const result = await this.communicator.getMessages(peer);
-
-		for (const prevoteHex of result.prevotes) {
-			const prevote = await this.messageFactory.makePrevoteFromBytes(Buffer.from(prevoteHex, "hex"));
-
-			await this.handler.onPrevote(prevote);
+		if (this.#isDownloadingMessages) {
+			return;
 		}
 
-		for (const precommitHex of result.precommits) {
-			const precommit = await this.messageFactory.makePrecommitFromBytes(Buffer.from(precommitHex, "hex"));
+		this.#isDownloadingMessages = true;
 
-			await this.handler.onPrecommit(precommit);
+		try {
+			const result = await this.communicator.getMessages(peer);
+
+			for (const prevoteHex of result.prevotes) {
+				const prevote = await this.messageFactory.makePrevoteFromBytes(Buffer.from(prevoteHex, "hex"));
+
+				await this.handler.onPrevote(prevote);
+			}
+
+			for (const precommitHex of result.precommits) {
+				const precommit = await this.messageFactory.makePrecommitFromBytes(Buffer.from(precommitHex, "hex"));
+
+				await this.handler.onPrecommit(precommit);
+			}
+		} catch {
+			// TODO: Handle errors
+		} finally {
+			this.#isDownloadingMessages = false;
 		}
 	}
 }
