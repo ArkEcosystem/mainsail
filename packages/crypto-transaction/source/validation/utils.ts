e52429d6442884870cbf01663155cf264933dc2a
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-07-10 14:14:37 +0000

    Fix transactions schema

--- packages/crypto-transaction/source/validation/utils.ts
+++ packages/crypto-transaction/source/validation/utils.ts
@@ -1,25 +1,24 @@
 import { Contracts } from "@mainsail/contracts";
 import deepmerge from "deepmerge";
 
-const signedTransaction = {
-	anyOf: [
-		{ required: ["id", "signature"] },
-		{ required: ["id", "signature", "signatures"] },
-		{ required: ["id", "signatures"] },
-	],
-};
-
 const strictTransaction = {
 	unevaluatedProperties: false,
 };
 
 export const extendSchema = (parent, properties): Contracts.Crypto.ITransactionSchema => deepmerge(parent, properties);
 
-export const signedSchema = (schema: Contracts.Crypto.ITransactionSchema): Contracts.Crypto.ITransactionSchema => {
-	const signed = extendSchema(schema, signedTransaction);
-	signed.$id = `${schema.$id}Signed`;
-	return signed;
-};
+export const signedSchema = (schema: Contracts.Crypto.ITransactionSchema): Contracts.Crypto.ITransactionSchema => ({
+	$id: `${schema.$id}Signed`,
+	anyOf: [
+		extendSchema({ properties: schema.properties, required: schema.required }, { required: ["id", "signature"] }),
+		extendSchema(
+			{ properties: schema.properties, required: schema.required },
+			{ required: ["id", "signature", "signatures"] },
+		),
+		extendSchema({ properties: schema.properties, required: schema.required }, { required: ["id", "signatures"] }),
+	],
+	type: "object",
+});
 
 export const strictSchema = (schema: Contracts.Crypto.ITransactionSchema): Contracts.Crypto.ITransactionSchema => {
 	const signed = signedSchema(schema);
