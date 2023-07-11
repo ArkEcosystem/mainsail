cacd8ebc68f52b3c7a0f944f1728046aad15c985
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-07-11 10:25:42 +0000

    GetPeers schema

--- packages/p2p/source/reply-schemas/get-peers.ts
+++ packages/p2p/source/reply-schemas/get-peers.ts
@@ -1,30 +1,35 @@
 import { constants } from "../constants";
+import { headers } from "./headers";
 
 export const getPeers = {
-	// TODO: Add headers
-	items: {
-		properties: {
-			ip: {
-				anyOf: [
-					{
-						format: "ipv4",
-						type: "string",
-					},
-					{
-						format: "ipv6",
-						type: "string",
-					},
-				],
+	properties: {
+		headers,
+		maxItems: constants.MAX_PEERS_GET_PEERS,
+		peers: {
+			properties: {
+				ip: {
+					anyOf: [
+						{
+							format: "ipv4",
+							type: "string",
+						},
+						{
+							format: "ipv6",
+							type: "string",
+						},
+					],
+				},
+				port: {
+					maximum: 65_535,
+					minimum: 0,
+					type: "integer",
+				},
 			},
-			port: {
-				maximum: 65_535,
-				minimum: 0,
-				type: "integer",
-			},
+			required: ["ip", "port"],
+			type: "object",
 		},
-		required: ["ip", "port"],
-		type: "object",
+		type: "array",
 	},
-	maxItems: constants.MAX_PEERS_GET_PEERS,
-	type: "array",
+	required: ["headers", "peers"],
+	type: "object",
 };
