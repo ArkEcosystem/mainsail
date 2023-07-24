d5ae4d6afbb82025155cf4439dc92b4dd02a0397
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-07-24 14:40:20 +0000

    Fix schema

--- packages/p2p/source/socket-server/schemas/get-blocks.ts
+++ packages/p2p/source/socket-server/schemas/get-blocks.ts
@@ -5,5 +5,5 @@ import { headers } from "./shared";
 export const getBlocks = Joi.object({
 	fromHeight: Joi.number().integer().min(1),
 	headers,
-	limit: Joi.number().integer().min(1).max(400),
-});
+	limit: Joi.number().integer().min(1).max(400).required(),
+}).required();
