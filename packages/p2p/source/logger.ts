da2279334c80454061cf0c97eac0d96ab6fc2f31
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-08-01 14:08:55 +0000

    Rename flag

--- packages/p2p/source/logger.ts
+++ packages/p2p/source/logger.ts
@@ -51,6 +51,6 @@ export class Logger implements Contracts.P2P.Logger {
 	}
 
 	#allowExtra(): boolean {
-		return process.env[Constants.Flags.CORE_P2P_PEER_VERIFIER_DEBUG_EXTRA] === "true";
+		return process.env[Constants.Flags.CORE_P2P_PEER_LOG_EXTRA] === "true";
 	}
 }
