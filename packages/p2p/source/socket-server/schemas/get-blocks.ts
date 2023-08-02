68564e387fdf0d19e1f90d1eee8a06bda394b2ce
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-08-02 08:51:02 +0000

    Fix get blocks schema

--- packages/p2p/source/socket-server/schemas/get-blocks.ts
+++ packages/p2p/source/socket-server/schemas/get-blocks.ts
@@ -5,7 +5,7 @@ import { makeHeaders } from "./shared";
 
 export const getBlocks = (configuration: Contracts.Crypto.IConfiguration) =>
 	Joi.object({
-		fromHeight: Joi.number().integer().min(1).required(),
+		fromHeight: Joi.number().integer().min(0).required(),
 		headers: makeHeaders(configuration),
 		limit: Joi.number().integer().min(1).max(400).required(),
 	}).required();
