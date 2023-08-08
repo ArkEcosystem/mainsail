fb49206169ffe87ea6e00441a51249178024a98b
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-08-08 10:56:17 +0000

    Decrease broadcast settings

--- packages/p2p/source/defaults.ts
+++ packages/p2p/source/defaults.ts
@@ -7,7 +7,7 @@ export const defaults = {
 	},
 	getBlocksTimeout: 30_000,
 	maxPeerSequentialErrors: process.env[Constants.Flags.CORE_P2P_MAX_PEER_SEQUENTIAL_ERRORS] || 3,
-	maxPeersBroadcast: 20,
+	maxPeersBroadcast: 3,
 	maxSameSubnetPeers: process.env[Constants.Flags.CORE_P2P_MAX_PEERS_SAME_SUBNET] || 5,
 	minimumNetworkReach: process.env[Constants.Flags.CORE_P2P_MIN_NETWORK_REACH] || 20,
 	minimumVersions: ["^0.0.1"],
