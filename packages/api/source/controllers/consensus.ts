716304414410886e1d43a9e075af08458ebe0c0c
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2023-08-10 16:49:18 +0900

    include validator name

--- packages/api/source/controllers/consensus.ts
+++ packages/api/source/controllers/consensus.ts
@@ -26,22 +26,28 @@ export class ConsensusController extends Controller {
 		const prevotes = await this.storage.getPrevotes();
 
 		const validators = this.validatorSet.getActiveValidators();
+		const nameLookup = new Map<string, string>();
+
+		for (const validator of validators) {
+			nameLookup.set(validator.getPublicKey()!, validator.getAttribute<string>("validator.username"));
+		}
 
 		const collectMessages = (messages: ReadonlyArray<Contracts.Crypto.IPrevote | Contracts.Crypto.IPrecommit>) => {
 			const collected = {
-				absent: validators.map((v) => v.getPublicKey()),
+				absent: validators.map((v) => nameLookup.get(v.getPublicKey()!) ?? v.getPublicKey()),
 			};
 
 			for (const message of messages) {
-				const validatorKey = this.validatorSet.getValidatorPublicKeyByIndex(message.validatorIndex);
+				const validator = validators[message.validatorIndex];
 				if (message.blockId) {
 					const key = `b/${message.height}/${message.round}/${message.blockId}`;
 					if (!collected[key]) {
 						collected[key] = {};
 					}
 
-					collected[key][`v-${validatorKey}`] = message.signature;
-					collected.absent.splice(collected.absent.indexOf(validatorKey), 1);
+					const name = nameLookup.get(validator.getPublicKey()!) ?? validator.getPublicKey();
+					collected[key][name] = message.signature;
+					collected.absent.splice(collected.absent.indexOf(name), 1);
 				}
 			}
 
@@ -53,20 +59,22 @@ export class ConsensusController extends Controller {
 				height: state.height,
 				lockedRound: state.lockedRound,
 				lockedValue: state.lockedValue ? state.lockedValue.getProposal()?.block.block.header.id : null,
-				precommits: collectMessages(precommits),
-				prevotes: collectMessages(prevotes),
 				proposals: proposals.map((p) => ({
+					name: nameLookup.get(validators[p.validatorIndex].getPublicKey()!) ?? validators[p.validatorIndex].getPublicKey(),
 					data: p.toData(),
 					lockProof: p.block.lockProof,
 				})),
+				precommits: collectMessages(precommits),
+				prevotes: collectMessages(prevotes),
 				round: state.round,
 				step: state.step,
 				validRound: state.validRound,
 				validValue: state.validValue ? state.validValue.getProposal()?.block.block.header.id : null,
 				validators: validators.map((v) => ({
-					consensusPublicKey: v.getAttribute<string>("validator.consensusPublicKey"),
+					name: v.getAttribute<string>("validator.username"),
 					index: this.validatorSet.getValidatorIndexByPublicKey(v.getPublicKey()!),
-					publicKey: v.getPublicKey(),
+					walletPublicKey: v.getPublicKey(),
+					consensusPublicKey: v.getAttribute<string>("validator.consensusPublicKey"),
 				})),
 			},
 		};
