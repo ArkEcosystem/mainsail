31dd6eb1d81b6733467093709e7e7a5ede894e51
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2023-08-10 11:58:08 +0900

    extend consensus state endpoint

--- packages/api/source/controllers/consensus.ts
+++ packages/api/source/controllers/consensus.ts
@@ -10,6 +10,9 @@ export class ConsensusController extends Controller {
 	@inject(Identifiers.Consensus.Storage)
 	private readonly storage!: Contracts.Consensus.IConsensusStorage;
 
+	@inject(Identifiers.ValidatorSet)
+	private readonly validatorSet!: Contracts.ValidatorSet.IValidatorSet;
+
 	public async state(request: Hapi.Request) {
 		const state = await this.storage.getState();
 		if (!state) {
@@ -17,6 +20,35 @@ export class ConsensusController extends Controller {
 		}
 
 		Utils.assert.defined<Contracts.Consensus.IConsensusState>(state);
+
+		const proposals = await this.storage.getProposals();
+		const precommits = await this.storage.getPrecommits();
+		const prevotes = await this.storage.getPrevotes();
+
+		const validators = this.validatorSet.getActiveValidators();
+
+		const collectMessages = (messages: ReadonlyArray<Contracts.Crypto.IPrevote | Contracts.Crypto.IPrecommit>) => {
+			const collected = {
+				// public keys
+				absent: [] as string[],
+			};
+
+			for (const message of messages) {
+				const validatorKey = this.validatorSet.getValidatorPublicKeyByIndex(message.validatorIndex);
+				if (message.blockId) {
+					const key = `b/${message.height}/${message.round}/${message.blockId}`;
+					if (!collected[key]) {
+						collected[key] = {};
+					}
+
+					collected[key][`v-${validatorKey}`] = message.signature;
+				} else {
+					collected.absent.push(validatorKey);
+				}
+			}
+
+			return collected;
+		}
 
 		return {
 			data: {
@@ -27,6 +59,17 @@ export class ConsensusController extends Controller {
 				step: state.step,
 				validRound: state.validRound,
 				validValue: state.validValue ? state.validValue.getProposal()?.block.block.header.id : null,
+				proposals: proposals.map(p => ({
+					lockProof: p.block.lockProof,
+					data: p.toData(),
+				})),
+				prevotes: collectMessages(prevotes),
+				precommits: collectMessages(precommits),
+				validators: validators.map(v => ({
+					index: this.validatorSet.getValidatorIndexByPublicKey(v.getPublicKey()!),
+					publicKey: v.getPublicKey(),
+					consensusPublicKey: v.getAttribute<string>("validator.consensusPublicKey")
+				})),
 			},
 		};
 	}
