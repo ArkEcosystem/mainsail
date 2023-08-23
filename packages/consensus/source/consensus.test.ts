b608684ba6cbe40434e2134ab99a509d08d066c5
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-08-23 11:10:43 +0000

    Update tests

--- packages/consensus/source/consensus.test.ts
+++ packages/consensus/source/consensus.test.ts
@@ -265,7 +265,7 @@ describe<Context>("Consensus", ({ it, beforeEach, assert, stub, spy, clock, each
 		spyValidatorPrepareBlock.calledOnce();
 		spyValidatorPrepareBlock.calledWith(2, 0);
 		spyValidatorPropose.calledOnce();
-		spyValidatorPropose.calledWith(0, block);
+		spyValidatorPropose.calledWith(0, undefined, block);
 		spyProposalProcess.calledOnce();
 		spyProposalProcess.calledWith(proposal.serialized);
 		spyLoggerInfo.calledWith(`>> Starting new round: ${2}/${0} with proposer: ${proposer.getUsername()}`);
