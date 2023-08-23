5348b3e6cfb8d9f9ff2f2caa473350e0ff5cc548
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-08-23 12:36:38 +0000

    Fix export

--- packages/processor/source/verifiers/index.ts
+++ packages/processor/source/verifiers/index.ts
@@ -2,5 +2,4 @@ export * from "./chained-verifier";
 export * from "./forged-transactions-verifier";
 export * from "./incompatible-transactions-verifier";
 export * from "./nonce-verifier";
-export * from "./validator-verifier";
 export * from "./verify-block-verifier";
