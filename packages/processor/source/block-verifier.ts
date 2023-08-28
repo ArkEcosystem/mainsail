a93d0fb4069b3747a566e8a53f478fa932f2c5a1
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-08-28 08:02:33 +0000

    Remove commit verification form the processor

--- packages/processor/source/block-verifier.ts
+++ packages/processor/source/block-verifier.ts
@@ -8,27 +8,11 @@ import {
 	NonceVerifier,
 	VerifyBlockVerifier,
 } from "./verifiers";
-import { CommitVerifier } from "./verifiers/commit-verifier";
 
 @injectable()
 export class BlockVerifier implements Contracts.BlockProcessor.Verifier {
 	@inject(Identifiers.Application)
 	protected readonly app!: Contracts.Kernel.Application;
-
-	public async verifyCommittedBlock(unit: Contracts.BlockProcessor.IProcessableUnit): Promise<boolean> {
-		if (!(await this.verify(unit))) {
-			return false;
-		}
-
-		// NOTE: We need the CommitVerifier when dealing with downloaded blocks.
-		// The normal consensus flow already verifies each individual validator signature for the commit and
-		// can therefore be skipped.
-		if (!(await this.app.resolve(CommitVerifier).execute(unit))) {
-			return false;
-		}
-
-		return true;
-	}
 
 	public async verify(unit: Contracts.BlockProcessor.IProcessableUnit): Promise<boolean> {
 		if (!(await this.app.resolve(VerifyBlockVerifier).execute(unit))) {
