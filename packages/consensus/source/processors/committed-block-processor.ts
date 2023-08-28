0173bb38ff48a0e490d5eaf0d09cdef048104598
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-08-28 07:59:43 +0000

    Move commit check under committed block processor

--- packages/consensus/source/processors/committed-block-processor.ts
+++ packages/consensus/source/processors/committed-block-processor.ts
@@ -1,19 +1,29 @@
 import { inject, injectable } from "@mainsail/container";
 import { Contracts, Identifiers } from "@mainsail/contracts";
+import { Utils } from "@mainsail/kernel";
 
 import { CommittedBlockState } from "../committed-block-state";
 import { AbstractProcessor } from "./abstract-processor";
 
 @injectable()
 export class CommittedBlockProcessor extends AbstractProcessor {
+	@inject(Identifiers.Cryptography.Configuration)
+	private readonly configuration!: Contracts.Crypto.IConfiguration;
+
 	@inject(Identifiers.BlockProcessor)
 	private readonly processor!: Contracts.BlockProcessor.Processor;
 
 	@inject(Identifiers.Cryptography.Block.Factory)
 	private readonly blockFactory!: Contracts.Crypto.IBlockFactory;
 
-	@inject(Identifiers.BlockVerifier)
-	private readonly verifier!: Contracts.BlockProcessor.Verifier;
+	@inject(Identifiers.ValidatorSet)
+	private readonly validatorSet!: Contracts.ValidatorSet.IValidatorSet;
+
+	@inject(Identifiers.Cryptography.Message.Serializer)
+	private readonly serializer!: Contracts.Crypto.IMessageSerializer;
+
+	@inject(Identifiers.Consensus.Aggregator)
+	private readonly aggregator!: Contracts.Consensus.IAggregator;
 
 	async process(data: Buffer): Promise<Contracts.Consensus.ProcessorResult> {
 		const committedBlock = await this.#makeCommittedBlock(data);
@@ -22,10 +32,11 @@ export class CommittedBlockProcessor extends AbstractProcessor {
 			return Contracts.Consensus.ProcessorResult.Invalid;
 		}
 
-		const committedBlockState = this.app.resolve(CommittedBlockState).configure(committedBlock);
-		if (!(await this.verifier.verifyCommittedBlock(committedBlockState))) {
+		if (!(await this.#isCommitValid(committedBlock))) {
 			return Contracts.Consensus.ProcessorResult.Invalid;
 		}
+
+		const committedBlockState = this.app.resolve(CommittedBlockState).configure(committedBlock);
 
 		const result = await this.processor.process(committedBlockState);
 
@@ -46,5 +57,32 @@ export class CommittedBlockProcessor extends AbstractProcessor {
 		} catch {
 			return undefined;
 		}
+	}
+
+	async #isCommitValid(committedBlock: Contracts.Crypto.ICommittedBlock): Promise<boolean> {
+		const { commit, block } = committedBlock;
+
+		const publicKeys: Buffer[] = [];
+		for (const [index, validator] of commit.validators.entries()) {
+			if (!validator) {
+				continue;
+			}
+
+			const validatorPublicKey = this.validatorSet.getValidator(index).getConsensusPublicKey();
+			publicKeys.push(Buffer.from(validatorPublicKey, "hex"));
+		}
+
+		if (!Utils.isMajority(publicKeys.length, this.configuration)) {
+			return false;
+		}
+
+		const precommit = await this.serializer.serializePrecommitForSignature({
+			blockId: block.data.id,
+			height: block.data.height,
+			round: commit.round,
+			type: Contracts.Crypto.MessageType.Precommit,
+		});
+
+		return this.aggregator.verify(commit, precommit);
 	}
 }
