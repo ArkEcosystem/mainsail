67f90f5eb9d131248b9a64c2793077c01b3b0e3d
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-08-29 13:56:57 +0000

    Handle post precommit

--- packages/p2p/source/socket-server/controllers/post-precommit.ts
+++ packages/p2p/source/socket-server/controllers/post-precommit.ts
@@ -2,6 +2,8 @@ import Hapi from "@hapi/hapi";
 import { inject, injectable } from "@mainsail/container";
 import { Contracts, Identifiers } from "@mainsail/contracts";
 
+import { getPeerIp } from "../../utils";
+
 @injectable()
 export class PostPrecommitController implements Contracts.P2P.Controller {
 	@inject(Identifiers.Consensus.PrecommitProcessor)
@@ -10,12 +12,26 @@ export class PostPrecommitController implements Contracts.P2P.Controller {
 	@inject(Identifiers.Cryptography.Message.Factory)
 	private readonly factory!: Contracts.Crypto.IMessageFactory;
 
+	@inject(Identifiers.PeerDisposer)
+	private readonly peerDisposer!: Contracts.P2P.PeerDisposer;
+
+	@inject(Identifiers.PeerRepository)
+	private readonly peerRepository!: Contracts.P2P.PeerRepository;
+
 	public async handle(
 		request: Contracts.P2P.IPostPrecommitRequest,
 		h: Hapi.ResponseToolkit,
 	): Promise<Contracts.P2P.IPostPrecommitResponse> {
-		const precommit = await this.factory.makePrecommitFromBytes(request.payload.precommit);
-		await this.precommitProcessor.process(precommit);
+		try {
+			const precommit = await this.factory.makePrecommitFromBytes(request.payload.precommit);
+			const result = await this.precommitProcessor.process(precommit);
+
+			if (result === Contracts.Consensus.ProcessorResult.Invalid) {
+				throw new Error("Invalid precommit");
+			}
+		} catch (error) {
+			this.peerDisposer.banPeer(this.peerRepository.getPeer(getPeerIp(request)), error.message);
+		}
 
 		return {};
 	}
