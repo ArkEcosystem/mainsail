07e7a3e785ea5e44f904e7aa74d222c1df85ea28
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-08-29 13:54:05 +0000

    Handle post prevote

--- packages/p2p/source/socket-server/controllers/post-prevote.ts
+++ packages/p2p/source/socket-server/controllers/post-prevote.ts
@@ -2,6 +2,8 @@ import Hapi from "@hapi/hapi";
 import { inject, injectable } from "@mainsail/container";
 import { Contracts, Identifiers } from "@mainsail/contracts";
 
+import { getPeerIp } from "../../utils";
+
 @injectable()
 export class PostPrevoteController implements Contracts.P2P.Controller {
 	@inject(Identifiers.Consensus.PrevoteProcessor)
@@ -10,13 +12,27 @@ export class PostPrevoteController implements Contracts.P2P.Controller {
 	@inject(Identifiers.Cryptography.Message.Factory)
 	private readonly factory!: Contracts.Crypto.IMessageFactory;
 
+	@inject(Identifiers.PeerDisposer)
+	private readonly peerDisposer!: Contracts.P2P.PeerDisposer;
+
+	@inject(Identifiers.PeerRepository)
+	private readonly peerRepository!: Contracts.P2P.PeerRepository;
+
 	public async handle(
 		request: Contracts.P2P.IPostPrevoteRequest,
 		h: Hapi.ResponseToolkit,
 	): Promise<Contracts.P2P.IPostPrevoteResponse> {
-		const prevote = await this.factory.makePrevoteFromBytes(request.payload.prevote);
-		// TODO: Handle response
-		await this.prevoteProcessor.process(prevote);
+		try {
+			const prevote = await this.factory.makePrevoteFromBytes(request.payload.prevote);
+
+			const result = await this.prevoteProcessor.process(prevote);
+
+			if (result === Contracts.Consensus.ProcessorResult.Invalid) {
+				throw new Error("Invalid prevote");
+			}
+		} catch (error) {
+			this.peerDisposer.banPeer(this.peerRepository.getPeer(getPeerIp(request)), error.message);
+		}
 
 		return {};
 	}
