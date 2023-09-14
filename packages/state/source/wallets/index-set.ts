f464ec07f4cb209861b83fa4c853e9cde579191b
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-09-14 20:31:24 +0000

    Throw if index is already registered

--- packages/state/source/wallets/index-set.ts
+++ packages/state/source/wallets/index-set.ts
@@ -6,6 +6,9 @@ export class IndexSet implements Contracts.State.IndexSet {
 	#indexNames = new Set<string>();
 
 	set(indexName: string) {
+		if (this.#indexNames.has(indexName)) {
+			throw new Error(`The wallet index ${indexName} is already registered`);
+		}
 		this.#indexNames.add(indexName);
 	}
 
