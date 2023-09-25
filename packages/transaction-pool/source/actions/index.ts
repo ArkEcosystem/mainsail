9e9a40512c197584c98b0e53b1f5712e6228d682
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-09-25 10:43:19 +0000

    Remove revertTransaction action

--- packages/transaction-pool/source/actions/index.ts
+++ packages/transaction-pool/source/actions/index.ts
@@ -1,4 +1,3 @@
 export { ApplyTransactionAction } from "./apply-transaction";
-export { RevertTransactionAction } from "./revert-transaction";
 export { ThrowIfCannotEnterPoolAction } from "./throw-if-cannot-enter-pool";
 export { VerifyTransactionAction } from "./verify-transaction";
