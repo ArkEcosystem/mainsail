9cd946d22f764ac05322d629fba3af138f3a85fe
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2023-10-04 14:53:47 +0900

    implement blockchain controller

--- packages/api-http/source/handlers.ts
+++ packages/api-http/source/handlers.ts
@@ -1,6 +1,7 @@
 import Hapi from "@hapi/hapi";
 
 import * as Blocks from "./routes/blocks";
+import * as Blockchain from "./routes/blockchain";
 import * as Node from "./routes/node";
 import * as Transactions from "./routes/transactions";
 import * as ValidatorRounds from "./routes/validator-rounds";
@@ -10,7 +11,7 @@ import * as Wallets from "./routes/wallets";
 export = {
 	name: "Public API",
 	async register(server: Hapi.Server): Promise<void> {
-		const handlers = [Blocks, Transactions, Node, ValidatorRounds, Votes, Wallets];
+		const handlers = [Blocks, Blockchain, Transactions, Node, ValidatorRounds, Votes, Wallets];
 
 		for (const handler of handlers) {
 			handler.register(server);
