39da16bdbbe643dcbb7c090d152831ef7b036d2d
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-10-09 13:49:41 +0000

    Prepare paths

--- packages/state/source/exporter.ts
+++ packages/state/source/exporter.ts
@@ -1,14 +1,21 @@
-import { injectable } from "@mainsail/container";
-import { Contracts } from "@mainsail/contracts";
+import { inject, injectable } from "@mainsail/container";
+import { Contracts, Identifiers } from "@mainsail/contracts";
+import { join } from "path";
 
 @injectable()
 export class Exporter {
+	@inject(Identifiers.Application)
+	private readonly app!: Contracts.Kernel.Application;
+
 	public async export(
 		stateStore: Contracts.State.StateStore,
 		walletRepository: Contracts.State.WalletRepository,
 	): Promise<void> {
 		const heigh = stateStore.getLastHeight();
 
-		console.log(`Exporting state at height ${heigh}...`);
+		const temporaryPath = this.app.tempPath(join("state-export", `${heigh}.zip`));
+		const dataPath = this.app.dataPath(join("state-export", `${heigh}.zip`));
+
+		console.log(`Exporting state at height ${heigh} at path: ${temporaryPath}, ${dataPath}`);
 	}
 }
