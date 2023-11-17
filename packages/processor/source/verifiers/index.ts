f8ecdb9490dcccd613bd0cb13778a4d86bc03975
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-11-17 12:05:54 +0000

    Remove ForgedTransactionsVerifier

--- packages/processor/source/verifiers/index.ts
+++ packages/processor/source/verifiers/index.ts
@@ -1,5 +1,4 @@
 export * from "./chained-verifier";
-export * from "./forged-transactions-verifier";
 export * from "./incompatible-transactions-verifier";
 export * from "./nonce-verifier";
 export * from "./timestamp-verifier";
