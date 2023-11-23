fb425ddda74763ef76885d02d767bc86976896e0
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-11-23 10:57:20 +0000

    Remove TransactionWithBlockResource

--- packages/api-development/source/resources/index.ts
+++ packages/api-development/source/resources/index.ts
@@ -3,5 +3,4 @@ export * from "./block-with-transactions";
 export * from "./peer";
 export * from "./ports";
 export * from "./transaction";
-export * from "./transaction-with-block";
 export * from "./wallet";
