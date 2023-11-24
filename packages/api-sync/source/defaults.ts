9ec17d7dfbec3cbfb01357bdd498a0fa1c24d465
sebastijankuzner <sebastijan.kuzner@outlook.com>
2023-11-24 15:35:31 +0000

    Update api-sync

--- packages/api-sync/source/defaults.ts
+++ packages/api-sync/source/defaults.ts
@@ -1,13 +1,17 @@
+import { Environment } from "@mainsail/kernel";
+
 export const defaults = {
 	database: {
 		applicationName: "mainsail/api-sync",
-		database: process.env.CORE_DB_DATABASE || `${process.env.CORE_TOKEN}_${process.env.CORE_NETWORK_NAME}`,
+		database:
+			Environment.get("CORE_DB_DATABASE") ??
+			`${Environment.get("CORE_TOKEN")}_${Environment.get("CORE_NETWORK_NAME")}`,
 		// TODO
 		dropSchema: true,
 
 		entityPrefix: "public.",
 
-		host: process.env.CORE_DB_HOST || "localhost",
+		host: Environment.get("CORE_DB_HOST", "localhost"),
 
 		// TODO
 		logger: "simple-console",
@@ -15,16 +19,16 @@ export const defaults = {
 		// TODO
 		logging: false,
 
-		password: process.env.CORE_DB_PASSWORD || "password",
+		password: Environment.get("CORE_DB_PASSWORD", "password"),
 
-		port: process.env.CORE_DB_PORT || 5432,
+		port: Environment.get("CORE_DB_PORT" || 5432),
 
 		synchronize: true,
 
 		type: "postgres",
 
-		username: process.env.CORE_DB_USERNAME || process.env.CORE_TOKEN, // TODO
+		username: Environment.get("CORE_DB_USERNAME") ?? Environment.get("CORE_TOKEN"), // TODO
 	},
 
-	truncateDatabase: process.env.CORE_API_SYNC_TRUNCATE_DATABASE || false,
+	truncateDatabase: Environment.isSet("CORE_API_SYNC_TRUNCATE_DATABASE"),
 };
