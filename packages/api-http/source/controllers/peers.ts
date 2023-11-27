2f7279ff153dc274af4db4c67b77b542d9d0cfa3
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2023-11-27 16:08:59 +0900

    disable total count estimation on peers table

--- packages/api-http/source/controllers/peers.ts
+++ packages/api-http/source/controllers/peers.ts
@@ -6,6 +6,7 @@ import {
 	Search,
 } from "@mainsail/api-database";
 import { inject, injectable } from "@mainsail/container";
+import { Contracts } from "@mainsail/contracts";
 
 import { PeerResource } from "../resources/peer";
 import { Controller } from "./controller";
@@ -40,5 +41,11 @@ export class PeersController extends Controller {
 		}
 
 		return this.respondWithResource(peer, PeerResource, request.query.transform);
+	}
+
+	protected getListingOptions(): Contracts.Api.Options {
+		return {
+			estimateTotalCount: false
+		};
 	}
 }
