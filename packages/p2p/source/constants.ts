d2cbe7df831111269458aba8dcdce22677249938
oXtxNt9U <oXtxNt9U@users.noreply.github.com>
2023-12-04 23:51:55 +0000

    style: resolve style guide violations

--- packages/p2p/source/constants.ts
+++ packages/p2p/source/constants.ts
@@ -7,8 +7,9 @@ export const constants = {
 	MAX_DOWNLOAD_BLOCKS_JOBS: 10, // maximum number of blocks jobs
 
 	MAX_PAYLOAD_CLIENT: 5 * Constants.Units.MEGABYTE, // default maxPayload value on the WS socket client
-	MAX_PAYLOAD_SERVER: 3 * Constants.Units.MEGABYTE, // default maxPayload value on the  WS socket server
+	MAX_PAYLOAD_SERVER: 3 * Constants.Units.MEGABYTE,
 
-	MAX_PEERS_GET_PEERS: 500,
 	MAX_PEERS_GET_API_NODES: 100,
+	// default maxPayload value on the  WS socket server
+	MAX_PEERS_GET_PEERS: 500,
 };
