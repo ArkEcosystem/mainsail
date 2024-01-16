ebd9e3addec4a5d9cb44b678f86953f779bc2186
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-01-16 19:26:31 +0000

    Cleanup

--- packages/contracts/source/contracts/state/repository.ts
+++ packages/contracts/source/contracts/state/repository.ts
@@ -9,10 +9,7 @@ export interface Repository {
 	setAttribute<T = any>(key: string, value: T): void;
 	forgetAttribute(key: string): void;
 
-	// clone(walletRepository: WalletRepository): Wallet;
 	isClone(): boolean;
-	// getOriginal(): Wallet;
-
 	commitChanges(commitChanges: Repository): void;
 
 	toJson(): JsonObject;
