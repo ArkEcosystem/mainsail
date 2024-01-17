9c6fa2175109e2f4a79c0c212afc03e20d42a1b2
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-01-17 17:43:27 +0900

    use isWorker()

--- packages/kernel/source/bootstrap/load-service-providers.ts
+++ packages/kernel/source/bootstrap/load-service-providers.ts
@@ -41,8 +41,6 @@ export class LoadServiceProviders implements Bootstrapper {
 		const pluginPath: string | undefined = this.app.config<string>("pluginPath", undefined, "");
 		assert.string(pluginPath);
 
-		const isWorker = this.app.config("worker", undefined, false);
-
 		for (const plugin of plugins) {
 			const installedPlugin = installedPlugins.find((installedPlugin) => installedPlugin.name === plugin.package);
 			const packageId = installedPlugin ? installedPlugin.path : plugin.package;
@@ -50,7 +48,7 @@ export class LoadServiceProviders implements Bootstrapper {
 			const packageModule = join(pluginPath, packageId);
 			const serviceProvider: ServiceProvider = this.app.resolve(require(packageModule).ServiceProvider);
 
-			if (isWorker && !serviceProvider.requiredByWorker()) {
+			if (this.app.isWorker() && !serviceProvider.requiredByWorker()) {
 				continue;
 			}
 
