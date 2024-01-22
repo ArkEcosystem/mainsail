bfafa4da307d1a6d26ba120815228053e9d7c1af
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-01-22 18:28:36 +0000

    Fix tests

---
+++ packages/state/source/state.test.ts
@@ -0,0 +1,22 @@
+import { describe, Sandbox } from "../../test-framework/distribution";
+import { State } from "./state";
+
+describe<{
+	sandbox: Sandbox;
+	state: State;
+}>("State", ({ it, beforeEach, assert }) => {
+	beforeEach(async (context) => {
+		context.sandbox = new Sandbox();
+
+		context.state = context.sandbox.app.resolve(State);
+	});
+
+	it("#isBootstrap - should return true by default", ({ state }) => {
+		assert.true(state.isBootstrap());
+	});
+
+	it("#setBootstrap - should set bootstrap", ({ state }) => {
+		state.setBootstrap(false);
+		assert.false(state.isBootstrap());
+	});
+});
