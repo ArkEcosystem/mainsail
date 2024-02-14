20e9a716a91fc7e3684e315f0677c590911b02f5
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-14 15:06:11 +0000

    Epi development variables

--- packages/api-development/source/defaults.ts
+++ packages/api-development/source/defaults.ts
@@ -1,3 +1,4 @@
+import { Constants } from "@mainsail/contracts";
 import { Environment } from "@mainsail/kernel";
 
 export const defaults = {
@@ -6,31 +7,36 @@ export const defaults = {
 			limit: 100,
 		},
 		rateLimit: {
-			blacklist: Environment.get<undefined>("CORE_API_DEV_RATE_LIMIT_BLACKLIST")?.split(",") ?? [],
-			duration: Environment.get("CORE_API_DEV_RATE_LIMIT_USER_EXPIRES", 60), // Sec
-			enabled: Environment.isTrue("CORE_API_DEV_RATE_LIMIT_DISABLED"),
-			points: Environment.get("CORE_API_DEV_RATE_LIMIT_USER_LIMIT", 100),
+			blacklist:
+				Environment.get<undefined>(Constants.EnvironmentVariables.CORE_API_DEV_RATE_LIMIT_BLACKLIST)?.split(
+					",",
+				) ?? [],
+			duration: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_RATE_LIMIT_USER_EXPIRES, 60), // Sec
+			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_DEV_RATE_LIMIT_DISABLED),
+			points: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_RATE_LIMIT_USER_LIMIT, 100),
 
-			whitelist: Environment.get<undefined>("CORE_API_DEV_RATE_LIMIT_WHITELIST")?.split(",") ?? [],
+			whitelist:
+				Environment.get<undefined>(Constants.EnvironmentVariables.CORE_API_DEV_RATE_LIMIT_WHITELIST)?.split(
+					",",
+				) ?? [],
 		},
 		socketTimeout: 5000,
-		trustProxy: Environment.isTrue("CORE_API_DEV_TRUST_PROXY"),
+		trustProxy: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_DEV_TRUST_PROXY),
 		whitelist: ["*"],
 	},
 	server: {
 		http: {
-			enabled: Environment.isTrue("CORE_API_DEV_ENABLED"),
-			host: Environment.get("CORE_API_DEV_HOST", "127.0.0.1"),
-			port: Environment.get("CORE_API_DEV_PORT", 4006),
+			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_DEV_ENABLED),
+			host: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_HOST, "127.0.0.1"),
+			port: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_PORT, 4006),
 		},
-		// @see https://hapijs.com/api#-serveroptionstls
 		https: {
-			enabled: Environment.isTrue("CORE_API_DEV_SSL"),
-			host: Environment.get("CORE_API_DEV_SSL_HOST", "127.0.0.1"),
-			port: Environment.get("CORE_API_DEV_SSL_PORT", 8446),
+			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_DEV_SSL),
+			host: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_SSL_HOST, "127.0.0.1"),
+			port: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_SSL_PORT, 8446),
 			tls: {
-				cert: Environment.get("CORE_API_DEV_SSL_CERT"),
-				key: Environment.get("CORE_API_DEV_SSL_KEY"),
+				cert: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_SSL_CERT),
+				key: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_SSL_KEY),
 			},
 		},
 	},
