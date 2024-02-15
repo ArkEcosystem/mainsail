adee68737389151d271c9625a9cc9399cd454683
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-15 17:55:33 +0000

    Remove token from config

--- packages/cli/source/services/config.test.ts
+++ packages/cli/source/services/config.test.ts
@@ -22,29 +22,11 @@ describe<{
 		assert.equal(config.all(), {
 			channel: "next",
 			plugins: [],
-			token: "ark",
 		});
 	});
 
 	it("should setup a new config with default values", ({ config }) => {
-		assert.equal(config.get("token"), "ark");
 		assert.equal(config.get("channel"), "next");
-	});
-
-	it("should set and get a value", ({ config }) => {
-		assert.equal(config.get("token"), "ark");
-
-		config.set("token", "btc");
-
-		assert.equal(config.get("token"), "btc");
-
-		config.forget("token");
-
-		assert.undefined(config.get("token"));
-
-		config.set("token", "btc");
-
-		assert.equal(config.get("token"), "btc");
 	});
 
 	it("#load - should restore the defaults if the config has been corrupted", ({ config, configPath }) => {
@@ -58,33 +40,27 @@ describe<{
 	});
 
 	it("#save - should restore the defaults if the config has been corrupted", ({ config }) => {
-		assert.equal(config.get("token"), "ark");
 		assert.equal(config.get("channel"), "next");
 		assert.equal(config.get("plugins"), []);
 
-		config.set("token", "btc");
 		config.set("channel", "latest");
 		config.set("plugins", ["something"]);
 
 		config.save();
 
-		assert.equal(config.get("token"), "btc");
 		assert.equal(config.get("channel"), "latest");
 		assert.equal(config.get("plugins"), ["something"]);
 	});
 
 	it("#restoreDefaults - should restore the defaults if the config has been corrupted", ({ config }) => {
-		config.forget("token");
 		config.forget("channel");
 		config.forget("plugins");
 
-		assert.undefined(config.get("token"));
 		assert.undefined(config.get("channel"));
 		assert.undefined(config.get("plugins"));
 
 		config.restoreDefaults();
 
-		assert.equal(config.get("token"), "ark");
 		assert.equal(config.get("channel"), "next");
 		assert.equal(config.get("plugins"), []);
 	});
