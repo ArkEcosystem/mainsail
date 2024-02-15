39f049c2c6d979ba0e68c65602ac6525307206ab
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-15 18:00:33 +0000

    Config cli test

--- packages/core/source/commands/config-cli.test.ts
+++ packages/core/source/commands/config-cli.test.ts
@@ -13,26 +13,6 @@ describe<{
 		context.config = context.cli.app.get<Contracts.Config>(Identifiers.Config);
 	});
 
-	it("should change the token", async ({ cli, config }) => {
-		await cli.execute(Command);
-
-		assert.equal(config.get("token"), "ark");
-
-		await cli.withFlags({ token: "btc" }).execute(Command);
-
-		assert.equal(config.get("token"), "btc");
-	});
-
-	it("should not set config token if no token is passed to command", async ({ config }) => {
-		const cli = new Console(false);
-		stub(cli.app.get(Identifiers.Environment), "getPaths");
-		const spySetToken = stub(config, "set");
-
-		await assert.resolves(() => cli.execute(Command));
-
-		spySetToken.neverCalled();
-	});
-
 	it("should change the channel and install the new version", async ({ cli, config }) => {
 		stub(execa, "sync").returnValue({
 			exitCode: 0,
