a2feb565185caf7b37422321c9f63dfc1a5acf5e
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-22 18:15:38 +0000

    Use async calls

--- scripts/deps/missing.js
+++ scripts/deps/missing.js
@@ -2,31 +2,35 @@ const depcheck = require("depcheck");
 const { resolve } = require("path");
 const { lstatSync, readdirSync } = require("fs");
 
-const source = resolve(__dirname, "../../packages");
+const main = async () => {
+	const source = resolve(__dirname, "../../packages");
 
-const pkgs = readdirSync(source)
-	.filter((name) => lstatSync(`${source}/${name}`).isDirectory())
-	.sort();
+	const pkgs = readdirSync(source)
+		.filter((name) => lstatSync(`${source}/${name}`).isDirectory())
+		.sort();
 
-for (const pkg of pkgs) {
-	depcheck(
-		`${source}/${pkg}`,
-		{
-			ignoreDirs: ["__tests__", "benchmark", "distribution", "docker", "scripts"],
-			ignoreMatches: ["@types/*"],
-		},
-		(unused) => {
-			const missing = Object.keys(unused.missing);
+	for (const package_ of pkgs) {
+		await depcheck(
+			`${source}/${package_}`,
+			{
+				ignoreDirs: ["__tests__", "benchmark", "distribution", "docker", "scripts"],
+				ignoreMatches: ["@types/*"],
+			},
+			(unused) => {
+				const missing = Object.keys(unused.missing);
 
-			if (missing.length > 0) {
-				console.log(`[FAIL] ${pkg}`);
+				if (missing.length > 0) {
+					console.log(`[FAIL] ${package_}`);
 
-				for (const dep of missing) {
-					console.log(`lerna add ${dep} --scope=@mainsail/${pkg}`);
+					for (const dep of missing) {
+						console.log(`lerna add ${dep} --scope=@mainsail/${package_}`);
+					}
+				} else {
+					console.log(`[PASS] ${package_}`);
 				}
-			} else {
-				console.log(`[PASS] ${pkg}`);
-			}
-		},
-	);
-}
+			},
+		);
+	}
+};
+
+main();
