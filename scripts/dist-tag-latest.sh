163ba87631ee990a7715fe4dfcb7dcdd44cdd1db
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2024-02-22 19:02:30 +0100

    chore: set `latest` NPM tag on release (#446)
    * Add script
    
    * Remove publish scripts
    
    * Update workflow
    
    * Fix path

---
+++ scripts/dist-tag-latest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+for dir in `find packages -mindepth 1 -maxdepth 1 -type d | sort -nr`; do
+    json=$(<"$dir/package.json")
+	# Remove leading/trailing whitespace
+	json=$(echo "$json" | tr -d '\n' | tr -d '\r' | sed 's/ //g')
+	# Extract name and version
+	name=$(echo "$json" | sed 's/.*"name":"\([^"]*\)".*/\1/')
+	version=$(echo "$json" | sed 's/.*"version":"\([^"]*\)".*/\1/')
+	echo $name@$version
+
+	pnpm dist-tag add $name@$version latest
+done
