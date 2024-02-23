2aa43852e42d73c2ab7ed2818c7b08ce760f5841
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-23 12:01:11 +0000

    Calculate deps

--- scripts/deps/custom.js
+++ scripts/deps/custom.js
@@ -18,9 +18,57 @@ class Dependency {
 	}
 
 	isTestOnly() {
-		return this.paths.every((path) => path.endsWith(".test.ts"));
+		return this.paths.every((path) => path.endsWith(".test.ts") || !path.includes("/source/"));
 	}
 }
+
+testDependencies = (packageJson, dependencies) => {
+	const testDependencies = dependencies.filter((dep) => !dep.testOnly).map((dep) => dep.name);
+
+	const combined = new Set([...testDependencies, ...packageJson.dependencies]);
+
+	const missing = [];
+	const unused = [];
+	const used = [];
+
+	for (const dep of combined.values()) {
+		if (testDependencies.includes(dep) && packageJson.dependencies.includes(dep)) {
+			used.push(dep);
+			continue;
+		}
+
+		if (testDependencies.includes(dep)) {
+			missing.push(dep);
+		} else {
+			unused.push(dep);
+		}
+	}
+
+	return { missing, unused };
+};
+
+testDevDependencies = (packageJson, dependencies) => {
+	const testDependencies = dependencies.filter((dep) => dep.testOnly).map((dep) => dep.name);
+
+	const combined = new Set([...testDependencies, ...packageJson.devDependencies]);
+
+	const missing = [];
+	const unused = [];
+
+	for (const dep of combined.values()) {
+		if (testDependencies.includes(dep) && packageJson.devDependencies.includes(dep)) {
+			continue;
+		}
+
+		if (testDependencies.includes(dep)) {
+			missing.push(dep);
+		} else {
+			unused.push(dep);
+		}
+	}
+
+	return { missing, unused };
+};
 
 const main = async () => {
 	const source = resolve(__dirname, "../../packages");
@@ -35,21 +83,22 @@ const main = async () => {
 
 		const package = new Package(packageJson);
 
-		console.log("Checking: ", package);
+		// console.log("Checking: ", package);
 
 		await depcheck(
 			join(source, pkg),
 			{
 				// ignorePatterns: ["*.test.ts"],
-				ignoreDirs: ["node_modules", "benchmark", "distribution", "test"],
+				ignoreDirs: ["node_modules", "distribution"],
 				ignoreMatches: ["@types/*"],
 			},
-			(unused) => {
-				// const missing = Object.keys(unused.missing);packageJson
-				// console.log("Missing: ", unused.missing);
-				// console.log("Dependencies: ", unused.dependencies);
-				// console.log("DevDependencies: ", unused.devDependencies);
-				// console.log("Using: ", unused.using);
+			(result) => {
+				// const missing = Object.keys(result.missing);
+				// packageJson;
+				// console.log("Missing: ", result.missing);
+				// console.log("Dependencies: ", result.dependencies);
+				// console.log("DevDependencies: ", result.devDependencies);
+				// console.log("Using: ", result.using);
 				// if (missing.length > 0) {
 				// 	console.log(`[FAIL] ${package_}`);
 				// 	for (const dep of missing) {
@@ -59,12 +108,29 @@ const main = async () => {
 				// 	console.log(`[PASS] ${package_}`);
 				// }
 
-				const dependencies = Object.keys(unused.using).map((name) => new Dependency(name, unused.using[name]));
+				const dependencies = Object.keys(result.using).map((name) => new Dependency(name, result.using[name]));
 
-				console.log(
-					"Dependencies: ",
-					dependencies.filter((dep) => dep.testOnly),
-				);
+				// console.log(
+				// 	"Dependencies: ",
+				// 	dependencies.filter((dep) => dep.testOnly),
+				// );
+
+				const { missing, unused, used } = testDependencies(package, dependencies);
+
+				console.log("Used: ", used);
+				console.log("Missing: ", missing);
+				console.log("Unused: ", unused);
+
+				// const { missing: devMissing, unused: devUnused } = testDevDependencies(package, dependencies);
+
+				// console.log(
+				// 	"DevMissing: ",
+				// 	devMissing.map((dep) => dep.name),
+				// );
+				// console.log(
+				// 	"DevUnused: ",
+				// 	devUnused.map((dep) => dep.name),
+				// );
 			},
 		);
 	}
