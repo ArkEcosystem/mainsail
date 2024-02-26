124165ce7be3050c114afdc43c22992ff57a73f1
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-02-26 16:02:09 +0900

    ensure validator funds

--- packages/evm-development/source/deployer.ts
+++ packages/evm-development/source/deployer.ts
@@ -1,23 +1,31 @@
 import { inject, injectable } from "@mainsail/container";
 import { Contracts, Identifiers } from "@mainsail/contracts";
+import { Utils } from "@mainsail/kernel";
 import { Bindings } from "@mainsail/evm";
 import { ERC20 } from "./contracts.ts";
 import { ethers } from "ethers";
 
 @injectable()
 export class Deployer {
+    @inject(Identifiers.Application.Instance)
+    private readonly app!: Contracts.Kernel.Application;
+
     @inject(Identifiers.Services.Log.Service)
     private readonly logger!: Contracts.Kernel.Logger;
 
     @inject(Identifiers.Evm.Instance)
     private readonly evm!: Bindings.Evm;
 
+    @inject(Identifiers.Cryptography.Identity.Address.Factory)
+    private readonly addressFactory!: Contracts.Crypto.AddressFactory;
+
+    #genesis1 = "0x0000000000000000000000000000000000000001";
+
     public async deploy(): Promise<void> {
-        const genesis1 = "0x0000000000000000000000000000000000000001";
 
         const result = await this.evm.transact({
             data: Buffer.from(ethers.getBytes(ERC20.abi.bytecode)),
-            caller: genesis1,
+            caller: this.#genesis1,
         });
 
         if (!result.success) {
@@ -25,6 +33,31 @@ export class Deployer {
         }
 
         this.logger.info(`Deployed ERC20 dummy contract: ${result.deployedContractAddress}`);
+
+        await this.ensureFunds(
+            result.deployedContractAddress!
+        );
     }
 
+    private async ensureFunds(erc20ContractAddress: string): Promise<void> {
+        const secrets = this.app.config("validators.secrets");
+        Utils.assert.defined<string[]>(secrets);
+
+        for (const secret of secrets) {
+            const address = await this.addressFactory.fromMnemonic(secret);
+
+            const iface = new ethers.Interface(ERC20.abi.abi);
+            const encodedCall = iface.encodeFunctionData("transfer", [address, 1]);
+
+            const result = await this.evm.transact({
+                caller: this.#genesis1,
+                recipient: erc20ContractAddress,
+                data: Buffer.from(ethers.getBytes(encodedCall)),
+            });
+
+            if (!result.success) {
+                throw new Error("failed to ensure funds");
+            }
+        }
+    }
 }
--- packages/evm-development/source/index.ts
+++ packages/evm-development/source/index.ts
@@ -1,17 +1,14 @@
-import { inject, injectable } from "@mainsail/container";
+import { injectable } from "@mainsail/container";
 import { Contracts, Identifiers } from "@mainsail/contracts";
 import { Providers } from "@mainsail/kernel";
 import { Deployer } from "./deployer";
 
 @injectable()
 export class ServiceProvider extends Providers.ServiceProvider {
-	@inject(Identifiers.Services.Log.Service)
-	private readonly logger!: Contracts.Kernel.Logger;
-
 	public async register(): Promise<void> { }
 
 	public async boot(): Promise<void> {
-		this.logger.info("Setting up EVM for development...");
+		this.app.get<Contracts.Kernel.Logger>(Identifiers.Services.Log.Service).info("Setting up EVM for development...");
 
 		await this.app.resolve(Deployer).deploy();
 	}
