cdf4c4fd7e5acf9a744632c80bb0492f2534fc76
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-02-26 17:20:02 +0900

    feat(evm-developmen): add package (#449)
    * implement evm-development package
    
    * remove superfluous log
    
    * ensure validator funds
    
    * deploy from genesis address
    
    * style: resolve style guide violations
    
    * increase amount
    
    * infer recipients from genesis block
    
    * style: resolve style guide violations
    
    * commit Cargo.lock
    
    * style: resolve style guide violations
    
    ---------
    
    Co-authored-by: oXtxNt9U <oXtxNt9U@users.noreply.github.com>

---
+++ packages/evm-development/README.md
@@ -0,0 +1,19 @@
+# Mainsail - EVM Development
+
+![banner](https://raw.githubusercontent.com/ArkEcosystem/mainsail/main/banner.png)
+
+## Documentation
+
+You can find installation instructions and detailed instructions on how to use this package at the [dedicated documentation site](https://ark.dev/docs/mainsail).
+
+## Security
+
+If you discover a security vulnerability within this package, please send an e-mail to [security@ark.io](mailto:security@ark.io). All security vulnerabilities will be promptly addressed.
+
+## Credits
+
+This project exists thanks to all the people who [contribute](https://github.com/ArkEcosystem/mainsail/graphs/contributors).
+
+## License
+
+[GPL-3.0-only](https://github.com/ArkEcosystem/mainsail/blob/main/LICENSE) © [ARK Ecosystem](https://ark.io)
--- packages/evm/source/index.ts
+++ packages/evm/source/index.ts
@@ -1,20 +1,18 @@
-import { inject } from "@mainsail/container";
-import { Contracts, Identifiers } from "@mainsail/contracts";
+import { Identifiers } from "@mainsail/contracts";
 import { Providers } from "@mainsail/kernel";
 
 import { Instance } from "./instance";
 
+export * as Bindings from "./generated/bindings";
+
 export class ServiceProvider extends Providers.ServiceProvider {
-	@inject(Identifiers.Services.Log.Service)
-	private readonly logger!: Contracts.Kernel.Logger;
+	public async register(): Promise<void> {}
 
-	public async register(): Promise<void> {
-		this.logger.info("Booting EVM...");
-
+	public async boot(): Promise<void> {
 		this.app.bind(Identifiers.Evm.Instance).to(Instance).inSingletonScope();
 	}
 
 	public async dispose(): Promise<void> {
-		this.logger.info("Disposing EVM...");
+		// TODO
 	}
 }
