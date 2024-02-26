fdc004531a9129ddf0a63934eded9b6a63964673
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-02-26 14:57:25 +0900

    remove superfluous log

--- packages/evm/source/index.ts
+++ packages/evm/source/index.ts
@@ -1,5 +1,4 @@
-import { inject } from "@mainsail/container";
-import { Contracts, Identifiers } from "@mainsail/contracts";
+import { Identifiers } from "@mainsail/contracts";
 import { Providers } from "@mainsail/kernel";
 
 import { Instance } from "./instance";
@@ -7,17 +6,13 @@ import { Instance } from "./instance";
 export * as Bindings from "./generated/bindings";
 
 export class ServiceProvider extends Providers.ServiceProvider {
-	@inject(Identifiers.Services.Log.Service)
-	private readonly logger!: Contracts.Kernel.Logger;
-
 	public async register(): Promise<void> { }
 
 	public async boot(): Promise<void> {
-		this.logger.info("Booting EVM...");
 		this.app.bind(Identifiers.Evm.Instance).to(Instance).inSingletonScope();
 	}
 
 	public async dispose(): Promise<void> {
-		this.logger.info("Disposing EVM...");
+		// TODO
 	}
 }
