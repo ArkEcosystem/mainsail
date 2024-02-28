991578bebd05e32b05435979b5c6f8099fecc686
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-02-28 12:31:58 +0900

    evm call builder

--- packages/crypto-transaction-evm-call/source/builder.ts
+++ packages/crypto-transaction-evm-call/source/builder.ts
@@ -1,5 +1,5 @@
 import { injectable, postConstruct } from "@mainsail/container";
-import { Contracts } from "@mainsail/contracts";
+import { Contracts, Exceptions } from "@mainsail/contracts";
 import { TransactionBuilder } from "@mainsail/crypto-transaction";
 import { BigNumber } from "@mainsail/utils";
 
@@ -15,11 +15,48 @@ export class EvmCallBuilder extends TransactionBuilder<EvmCallBuilder> {
 		this.data.typeGroup = EvmCallTransaction.typeGroup;
 		this.data.amount = BigNumber.ZERO;
 		this.data.senderPublicKey = "";
+		this.data.asset = {
+			evmCall: {
+				// TODO: gas parameters
+				gasLimit: 1_000_000,
+				payload: "",
+			},
+		};
+	}
+
+	public payload(payload: string): EvmCallBuilder {
+		if (this.data.asset && this.data.asset.evmCall) {
+			this.data.asset.evmCall.payload = payload;
+		}
+
+		return this;
+	}
+
+	public gasLimit(gasLimit: number): EvmCallBuilder {
+		if (this.data.asset && this.data.asset.evmCall) {
+			this.data.asset.evmCall.gasLimit = gasLimit;
+		}
+
+		return this;
 	}
 
 	public async getStruct(): Promise<Contracts.Crypto.TransactionData> {
+		if (!this.data.asset || !this.data.asset.evmCall || !this.data.asset.evmCall.payload) {
+			throw new Exceptions.EvmCallIncompleteAssetError();
+		}
+
+		if (!this.data.recipientId && !this.data.asset.evmCall.payload) {
+			// recipient may only be absent if payload is present in which case
+			// it would be a contract creation
+			throw new Exceptions.EvmCallMissingRecipientError();
+		}
+
 		const struct: Contracts.Crypto.TransactionData = await super.getStruct();
+
+		struct.recipientId = this.data.recipientId;
 		struct.amount = this.data.amount;
+		struct.asset = this.data.asset;
+
 		return struct;
 	}
 
