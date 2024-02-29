11c48ceaff457a07f8fb2c852944b4895a921b44
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-29 18:56:36 +0000

    Fix dependencies

--- packages/api-evm/source/service-provider.ts
+++ packages/api-evm/source/service-provider.ts
@@ -1,6 +1,6 @@
 import { AbstractServiceProvider, Plugins, ServerConstructor } from "@mainsail/api-common";
+import { Contracts } from "@mainsail/contracts";
 import Joi from "joi";
-import { Contracts } from "packages/contracts/distribution";
 
 import { CallAction } from "./actions";
 import Handlers from "./handlers";
