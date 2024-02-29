e231184d99d8354303c79b20605b234270e6d180
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-29 14:30:39 +0000

    Fix validation

--- packages/api-common/source/rcp/processor.ts
+++ packages/api-common/source/rcp/processor.ts
@@ -1,15 +1,12 @@
 import Hapi from "@hapi/hapi";
-import { inject, injectable } from "@mainsail/container";
-import { Contracts, Identifiers } from "@mainsail/contracts";
+import { injectable } from "@mainsail/container";
+import { Contracts } from "@mainsail/contracts";
 import Joi from "joi";
 
 import { getRcpId } from "./utils";
 
 @injectable()
 export class Processor implements Contracts.Api.RPC.Processor {
-	@inject(Identifiers.Services.Validation.Service)
-	private readonly validator!: Contracts.Kernel.Validator;
-
 	async process(request: Hapi.Request): Promise<Contracts.Api.RPC.Response | Contracts.Api.RPC.Error> {
 		if (!this.#validatePayload(request)) {
 			return this.#invalidRequest(getRcpId(request));
@@ -25,7 +22,7 @@ export class Processor implements Contracts.Api.RPC.Processor {
 	#validatePayload(request: Hapi.Request): boolean {
 		const schema = Joi.object({
 			// eslint-disable-next-line unicorn/no-null
-			id: Joi.alternatives().try(Joi.string(), Joi.number(), Joi.allow(null)).required(),
+			id: Joi.alternatives(Joi.string().allow(null), Joi.number()).required(),
 			jsonrpc: Joi.string().valid("2.0").required(),
 			method: Joi.string().required(),
 			params: Joi.any(),
@@ -33,11 +30,7 @@ export class Processor implements Contracts.Api.RPC.Processor {
 
 		const payload = request.payload as Contracts.Types.JsonObject;
 
-		this.validator.validate(payload, schema);
-
-		console.log("PASSES:", this.validator.passes(), this.validator.errors());
-
-		return this.validator.passes();
+		return this.#validate(schema, payload);
 	}
 
 	#invalidRequest(id: Contracts.Api.RPC.Id): Contracts.Api.RPC.Error {
@@ -49,5 +42,15 @@ export class Processor implements Contracts.Api.RPC.Processor {
 			id,
 			jsonrpc: "2.0",
 		};
+	}
+
+	#validate(schema: Joi.Schema, data: Contracts.Types.JsonObject): boolean {
+		const { error } = schema.validate(data);
+
+		if (error) {
+			console.log("Validation error:", error.details);
+		}
+
+		return !error;
 	}
 }
