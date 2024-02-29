c2f7b43bbeb896f6c02f1c5f64f58cbba9ad4288
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-29 09:51:47 +0000

    Define EVN variables

--- packages/api-evm/source/defaults.ts
+++ packages/api-evm/source/defaults.ts
@@ -8,35 +8,35 @@ export const defaults = {
 		},
 		rateLimit: {
 			blacklist:
-				Environment.get<undefined>(Constants.EnvironmentVariables.CORE_API_DEV_RATE_LIMIT_BLACKLIST)?.split(
+				Environment.get<undefined>(Constants.EnvironmentVariables.CORE_API_EVM_RATE_LIMIT_BLACKLIST)?.split(
 					",",
 				) ?? [],
-			duration: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_RATE_LIMIT_USER_EXPIRES, 60), // Sec
-			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_DEV_RATE_LIMIT_DISABLED),
-			points: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_RATE_LIMIT_USER_LIMIT, 100),
+			duration: Environment.get(Constants.EnvironmentVariables.CORE_API_EVM_RATE_LIMIT_USER_EXPIRES, 60), // Sec
+			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_EVM_RATE_LIMIT_DISABLED),
+			points: Environment.get(Constants.EnvironmentVariables.CORE_API_EVM_RATE_LIMIT_USER_LIMIT, 100),
 
 			whitelist:
-				Environment.get<undefined>(Constants.EnvironmentVariables.CORE_API_DEV_RATE_LIMIT_WHITELIST)?.split(
+				Environment.get<undefined>(Constants.EnvironmentVariables.CORE_API_EVM_RATE_LIMIT_WHITELIST)?.split(
 					",",
 				) ?? [],
 		},
 		socketTimeout: 5000,
-		trustProxy: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_DEV_TRUST_PROXY),
+		trustProxy: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_EVM_TRUST_PROXY),
 		whitelist: ["*"],
 	},
 	server: {
 		http: {
-			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_DEV_ENABLED),
-			host: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_HOST, "127.0.0.1"),
-			port: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_PORT, 4006),
+			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_EVM_ENABLED),
+			host: Environment.get(Constants.EnvironmentVariables.CORE_API_EVM_HOST, "127.0.0.1"),
+			port: Environment.get(Constants.EnvironmentVariables.CORE_API_EVM_PORT, 4006),
 		},
 		https: {
-			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_DEV_SSL),
-			host: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_SSL_HOST, "127.0.0.1"),
-			port: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_SSL_PORT, 8446),
+			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_EVM_SSL),
+			host: Environment.get(Constants.EnvironmentVariables.CORE_API_EVM_SSL_HOST, "127.0.0.1"),
+			port: Environment.get(Constants.EnvironmentVariables.CORE_API_EVM_SSL_PORT, 8446),
 			tls: {
-				cert: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_SSL_CERT),
-				key: Environment.get(Constants.EnvironmentVariables.CORE_API_DEV_SSL_KEY),
+				cert: Environment.get(Constants.EnvironmentVariables.CORE_API_EVM_SSL_CERT),
+				key: Environment.get(Constants.EnvironmentVariables.CORE_API_EVM_SSL_KEY),
 			},
 		},
 	},
