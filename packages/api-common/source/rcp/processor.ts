2dd0cf3b65a7f1632fe65a7a2859c4082610cfe6
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-29 13:28:48 +0000

    Extract RPC contracts

--- packages/api-common/source/rcp/processor.ts
+++ packages/api-common/source/rcp/processor.ts
@@ -1,8 +1,9 @@
 import Hapi from "@hapi/hapi";
 import { injectable } from "@mainsail/container";
+import { Contracts } from "@mainsail/contracts";
 
 @injectable()
-export class Processor {
+export class Processor implements Contracts.Api.RPC.Processor {
 	process(request: Hapi.Request) {
 		return {};
 	}
