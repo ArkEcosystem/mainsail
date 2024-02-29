304c6764a681cdfbd675980c6af0ed1d47099ec6
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-02-29 11:13:35 +0000

    Validation

--- packages/api-evm/source/routes/base.ts
+++ packages/api-evm/source/routes/base.ts
@@ -1,4 +1,6 @@
 import Hapi from "@hapi/hapi";
+import { Constants } from "@mainsail/contracts";
+import Joi from "joi";
 
 import { BaseController } from "../controllers/base";
 
@@ -10,6 +12,20 @@ export const BaseRoute = {
 		server.route({
 			handler: (request: Hapi.Request) => controller.index(request),
 			method: "POST",
+			options: {
+				payload: {
+					maxBytes: 100 * Constants.Units.KILOBYTE,
+				},
+				validate: {
+					payload: Joi.object({
+						// eslint-disable-next-line unicorn/no-null
+						id: Joi.alternatives().try(Joi.string(), Joi.number(), Joi.allow(null)).required(),
+						jsonrpc: Joi.string().valid("2.0").required(),
+						method: Joi.string().required(),
+						params: Joi.any(),
+					}),
+				},
+			},
 			path: "/",
 		});
 	},
