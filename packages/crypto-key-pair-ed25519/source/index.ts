c981a5fd451a14c14e810369c9770a35721b75cc
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-03-11 13:53:27 +0900

    port crypto-key-pair-ed25519

--- packages/crypto-key-pair-ed25519/source/index.ts
+++ packages/crypto-key-pair-ed25519/source/index.ts
@@ -2,11 +2,11 @@ import { Selectors } from "@mainsail/container";
 import { Contracts, Identifiers } from "@mainsail/contracts";
 import { Providers } from "@mainsail/kernel";
 
-import { KeyPairFactory } from "./pair";
-import { PrivateKeyFactory } from "./private";
-import { PublicKeyFactory } from "./public";
-import { schemas } from "./schemas";
-import { PublicKeySerializer } from "./serializer";
+import { KeyPairFactory } from "./pair.js";
+import { PrivateKeyFactory } from "./private.js";
+import { PublicKeyFactory } from "./public.js";
+import { schemas } from "./schemas.js";
+import { PublicKeySerializer } from "./serializer.js";
 
 export class ServiceProvider extends Providers.ServiceProvider {
 	public async register(): Promise<void> {
