cd52e0289efa2a959e36cfa0d0612270b67ead7c
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-03-11 18:24:29 +0900

    ajv fix

--- packages/validation/source/validator.ts
+++ packages/validation/source/validator.ts
@@ -1,40 +1,39 @@
 import { injectable, postConstruct } from "@mainsail/container";
 import { Contracts } from "@mainsail/contracts";
 import _Ajv, { AnySchema, FormatDefinition, KeywordDefinition, Schema } from "ajv";
-import AjvCore from "ajv/dist/core.js";
+import AjvCore from "ajv/dist/2020.js";
 import formats from "ajv-formats";
 import keywords from "ajv-keywords";
 
 // Can be removed once upstream is fixed:
 // https://github.com/ajv-validator/ajv/issues/2132
 // https://github.com/ajv-validator/ajv/pull/2389
-const Ajv = _Ajv as unknown as typeof _Ajv.default;
+// const Ajv = _Ajv as unknown as typeof _Ajv.default;
 
 @injectable()
 export class Validator implements Contracts.Crypto.Validator {
-	#ajv!: AjvCore.default;
+	ajv_!: AjvCore.default;
 
 	@postConstruct()
 	public postConstruct(): void {
-		// @ts-ignore
-		this.#ajv = new Ajv({
+		this.ajv_ = new AjvCore.default({
 			$data: true,
 			strict: true,
 		});
 
-		keywords.default(this.#ajv);
-		formats.default(this.#ajv);
+		keywords.default(this.ajv_);
+		formats.default(this.ajv_);
 	}
 
 	public validate<T = any>(schemaKeyReference: string | Schema, data: T): Contracts.Crypto.SchemaValidationResult<T> {
 		try {
-			this.#ajv.validate(schemaKeyReference, data);
+			this.ajv_.validate(schemaKeyReference, data);
 
-			this.#ajv.errors;
+			this.ajv_.errors;
 
 			return {
-				error: this.#ajv.errors ? this.#ajv.errorsText() : undefined,
-				errors: this.#ajv.errors || undefined,
+				error: this.ajv_.errors ? this.ajv_.errorsText() : undefined,
+				errors: this.ajv_.errors || undefined,
 				value: data,
 			};
 		} catch (error) {
@@ -43,26 +42,26 @@ export class Validator implements Contracts.Crypto.Validator {
 	}
 
 	public addFormat(name: string, format: FormatDefinition<string> | FormatDefinition<number>): void {
-		this.#ajv.addFormat(name, format);
+		this.ajv_.addFormat(name, format);
 	}
 
 	public addKeyword(definition: KeywordDefinition): void {
-		this.#ajv.addKeyword(definition);
+		this.ajv_.addKeyword(definition);
 	}
 
 	public addSchema(schema: AnySchema | AnySchema[]): void {
-		this.#ajv.addSchema(schema);
+		this.ajv_.addSchema(schema);
 	}
 
 	public removeKeyword(keyword: string): void {
-		this.#ajv.removeKeyword(keyword);
+		this.ajv_.removeKeyword(keyword);
 	}
 
 	public removeSchema(schemaKeyReference: string): void {
-		this.#ajv.removeSchema(schemaKeyReference);
+		this.ajv_.removeSchema(schemaKeyReference);
 	}
 
 	public extend(callback: (ajv: AjvCore.default) => void): void {
-		callback(this.#ajv);
+		callback(this.ajv_);
 	}
 }
