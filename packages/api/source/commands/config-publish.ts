a872b6f500fbd8b022eea9c90b390b0067a7422d
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-03-14 10:24:32 +0900

    fix more commands

--- packages/api/source/commands/config-publish.ts
+++ packages/api/source/commands/config-publish.ts
@@ -29,8 +29,17 @@ export class Command extends Commands.Command {
 	async #performPublishment(flags: Contracts.AnyObject): Promise<void> {
 		this.app.rebind(Identifiers.ApplicationPaths).toConstantValue(this.environment.getPaths());
 
+		const dirname = (() => {
+			try {
+				return new URL(".", import.meta.url).pathname;
+			} catch {
+				// eslint-disable-next-line unicorn/prefer-module
+				return __dirname;
+			}
+		})();
+
 		const configDestination = this.app.getCorePath("config");
-		const configSource = resolve(__dirname, `../../bin/config/${this.app.get(Identifiers.Application.Name)}`);
+		const configSource = resolve(dirname, `../../bin/config/${this.app.get<string>(Identifiers.Application.Name)}`);
 
 		await this.components.taskList([
 			{
