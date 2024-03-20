0cdc5f34fcd0f33f30cd00b98fca38e96c3b4c46
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-03-20 13:12:51 +0000

    Register base packages

--- tests/functional/consensus/source/base.test.ts
+++ tests/functional/consensus/source/base.test.ts
@@ -1,6 +1,13 @@
-import { describe } from "@mainsail/test-framework";
+import { describe, Sandbox } from "@mainsail/test-framework";
+import { setup } from "./setup";
 
-describe("Consensus", ({ it, assert }) => {
+describe<{
+	sandbox: Sandbox;
+}>("Consensus", ({ beforeEach, it, assert }) => {
+	beforeEach((context) => {
+		context.sandbox = setup();
+	});
+
 	it("should be ok", () => {
 		assert.true(true);
 	});
---
+++ tests/functional/consensus/source/setup.ts
@@ -0,0 +1,52 @@
+import { ServiceProvider as CryptoConfig } from "@mainsail/crypto-config";
+import { ServiceProvider as CryptoHashBcrypto } from "@mainsail/crypto-hash-bcrypto";
+import { ServiceProvider as CryptoValidation } from "@mainsail/crypto-validation";
+import { Sandbox } from "@mainsail/test-framework";
+import { ServiceProvider as Validation } from "@mainsail/validation";
+import { ServiceProvider as CryptoSignatureSchnorr } from "@mainsail/crypto-signature-schnorr";
+import { ServiceProvider as CryptoKeyPairSchnorr } from "@mainsail/crypto-key-pair-schnorr";
+import { ServiceProvider as CryptoConsensusBls } from "@mainsail/crypto-consensus-bls12-381";
+import { ServiceProvider as CryptoAddressBeach32m } from "@mainsail/crypto-address-bech32m";
+import { ServiceProvider as CryptoWif } from "@mainsail/crypto-wif";
+import { ServiceProvider as Serializer } from "@mainsail/serializer";
+import { ServiceProvider as CryptoBlock } from "@mainsail/crypto-block";
+import { ServiceProvider as State } from "@mainsail/state";
+import { ServiceProvider as Database } from "@mainsail/database";
+import { ServiceProvider as Transactions } from "@mainsail/transactions";
+import { ServiceProvider as TransactionPool } from "@mainsail/transaction-pool";
+import { ServiceProvider as CryptoMessages } from "@mainsail/crypto-messages";
+import { ServiceProvider as CryptoCommit } from "@mainsail/crypto-commit";
+import { ServiceProvider as Processor } from "@mainsail/processor";
+import { ServiceProvider as Validator } from "@mainsail/validator";
+import { ServiceProvider as Proposer } from "@mainsail/proposer";
+import { ServiceProvider as Consensus } from "@mainsail/consensus";
+
+export const setup = async () => {
+	const sandbox = new Sandbox();
+
+	await sandbox.app.resolve(Validation).register();
+	await sandbox.app.resolve(CryptoConfig).register();
+	await sandbox.app.resolve(CryptoValidation).register();
+	await sandbox.app.resolve(CryptoHashBcrypto).register();
+	await sandbox.app.resolve(CryptoSignatureSchnorr).register();
+	await sandbox.app.resolve(CryptoKeyPairSchnorr).register();
+	await sandbox.app.resolve(CryptoConsensusBls).register();
+	await sandbox.app.resolve(CryptoAddressBeach32m).register();
+	await sandbox.app.resolve(CryptoWif).register();
+	await sandbox.app.resolve(Serializer).register();
+	await sandbox.app.resolve(CryptoBlock).register();
+	await sandbox.app.resolve(State).register();
+	await sandbox.app.resolve(State).register();
+	await sandbox.app.resolve(Database).register();
+	await sandbox.app.resolve(Transactions).register();
+	await sandbox.app.resolve(TransactionPool).register();
+	await sandbox.app.resolve(CryptoMessages).register();
+	await sandbox.app.resolve(CryptoCommit).register();
+	await sandbox.app.resolve(CryptoCommit).register();
+	await sandbox.app.resolve(Processor).register();
+	await sandbox.app.resolve(Validator).register();
+	await sandbox.app.resolve(Proposer).register();
+	await sandbox.app.resolve(Consensus).register();
+
+	return sandbox;
+};
