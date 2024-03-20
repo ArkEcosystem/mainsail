3343679c70a2b2ad763c6ff0c06702f5ec20dc84
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-03-20 14:41:35 +0000

    Export individual bootstrappers

--- packages/kernel/source/bootstrap/index.ts
+++ packages/kernel/source/bootstrap/index.ts
@@ -9,6 +9,17 @@ import { RegisterBaseServiceProviders } from "./register-base-service-providers.
 import { RegisterErrorHandler } from "./register-error-handler.js";
 import { RegisterServiceProviders } from "./register-service-providers.js";
 
+export { BootServiceProviders } from "./boot-service-providers.js";
+export { LoadConfiguration } from "./load-configuration.js";
+export { LoadEnvironmentVariables } from "./load-environment-variables.js";
+export { LoadServiceProviders } from "./load-service-providers.js";
+export { RegisterBaseBindings } from "./register-base-bindings.js";
+export { RegisterBaseConfiguration } from "./register-base-configuration.js";
+export { RegisterBasePaths } from "./register-base-paths.js";
+export { RegisterBaseServiceProviders } from "./register-base-service-providers.js";
+export { RegisterErrorHandler } from "./register-error-handler.js";
+export { RegisterServiceProviders } from "./register-service-providers.js";
+
 export const Bootstrappers = {
 	app: [
 		RegisterBaseServiceProviders,
