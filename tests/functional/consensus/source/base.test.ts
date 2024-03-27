10051b92fb5da881b5f3118bfc9b7ce87e111639
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-03-27 14:18:35 +0000

    Snooze for block

--- tests/functional/consensus/source/base.test.ts
+++ tests/functional/consensus/source/base.test.ts
@@ -4,12 +4,12 @@ import crypto from "../config/crypto.json";
 import validators from "../config/validators.json";
 import { P2PRegistry } from "./p2p";
 import { run, setup, stop } from "./setup";
-import { prepareNodeValidators, snoozeForBlock } from "./utils";
+import { prepareNodeValidators, snoozeForBlock, getLatestBlock } from "./utils";
 
 describe<{
 	node0: Sandbox;
 	node1: Sandbox;
-}>("Consensus", ({ beforeEach, it, assert }) => {
+}>("Consensus", ({ beforeEach, afterEach, it, assert }) => {
 	beforeEach(async (context) => {
 		const p2pRegistry = new P2PRegistry();
 
@@ -22,12 +22,30 @@ describe<{
 		await run(context.node1);
 	});
 
-	it("should be ok", async (context) => {
-		await snoozeForBlock(context.node0);
-
-		console.log("APPLIED");
-
+	afterEach(async (context) => {
 		await stop(context.node0);
 		await stop(context.node1);
+	});
+
+	it("should create new block", async (context) => {
+		await snoozeForBlock([context.node0, context.node1]);
+
+		const blockNode1 = await getLatestBlock(context.node0);
+		const blockNode2 = await getLatestBlock(context.node1);
+
+		assert.equal(blockNode1?.data.height, 1);
+		assert.equal(blockNode2?.data.height, 1);
+		assert.equal(blockNode1?.data.id, blockNode2?.data.id);
+	});
+
+	it("should create new block second time", async (context) => {
+		await snoozeForBlock([context.node0, context.node1]);
+
+		const blockNode1 = await getLatestBlock(context.node0);
+		const blockNode2 = await getLatestBlock(context.node1);
+
+		assert.equal(blockNode1?.data.height, 1);
+		assert.equal(blockNode2?.data.height, 1);
+		assert.equal(blockNode1?.data.id, blockNode2?.data.id);
 	});
 });
--- tests/functional/consensus/source/utils.ts
+++ tests/functional/consensus/source/utils.ts
@@ -14,9 +14,20 @@ export const prepareNodeValidators = (validators: Validators, nodeIndex: number,
 	};
 };
 
-export const snoozeForBlock = async (sandbox: Sandbox): Promise<void> =>
-	new Promise((resolve) => {
-		sandbox.app
-			.get<Contracts.Kernel.EventDispatcher>(Identifiers.Services.EventDispatcher.Service)
-			.listenOnce(Enums.BlockEvent.Applied, { handle: () => resolve() });
-	});
+export const snoozeForBlock = async (sandbox: Sandbox | Sandbox[]): Promise<void> => {
+	const function_ = async (sandbox: Sandbox): Promise<void> =>
+		new Promise((resolve) => {
+			sandbox.app
+				.get<Contracts.Kernel.EventDispatcher>(Identifiers.Services.EventDispatcher.Service)
+				.listenOnce(Enums.BlockEvent.Applied, { handle: () => resolve() });
+		});
+
+	if (Array.isArray(sandbox)) {
+		await Promise.all(sandbox.map((s) => function_(s)));
+	} else {
+		await function_(sandbox);
+	}
+};
+
+export const getLatestBlock = async (sandbox: Sandbox): Promise<Contracts.Crypto.Block | undefined> =>
+	sandbox.app.get<Contracts.Database.DatabaseService>(Identifiers.Database.Service).getLastBlock();
