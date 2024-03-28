24b2b1f4d34c0230a0d19c661e20c24b6ad048c2
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-03-28 14:30:03 +0000

    Fix base test

--- tests/functional/consensus/source/base.test.ts
+++ tests/functional/consensus/source/base.test.ts
@@ -4,49 +4,53 @@ import crypto from "../config/crypto.json";
 import validators from "../config/validators.json";
 import { assertBlockId, assertBockHeight, assertCommitValidators } from "./asserts.js";
 import { P2PRegistry } from "./p2p.js";
-import { run, setup, stop } from "./setup.js";
+import { bootMany, bootstrapMany, runMany, setup, stopMany } from "./setup.js";
 import { getLastCommit, prepareNodeValidators, snoozeForBlock } from "./utils.js";
 
 describe<{
-	node0: Sandbox;
-	node1: Sandbox;
+	nodes: Sandbox[];
 }>("Base", ({ beforeEach, afterEach, it, assert }) => {
-	const allValidators = new Array(validators.secrets.length).fill(true);
+	const allValidators = Array.from<boolean>({ length: validators.secrets.length }).fill(true);
 
 	beforeEach(async (context) => {
 		const p2pRegistry = new P2PRegistry();
 
 		const totalNodes = 2;
 
-		context.node0 = await setup(0, p2pRegistry, crypto, prepareNodeValidators(validators, 0, totalNodes));
-		context.node1 = await setup(1, p2pRegistry, crypto, prepareNodeValidators(validators, 1, totalNodes));
+		context.nodes = [];
 
-		await run(context.node0);
-		await run(context.node1);
+		for (let index = 0; index < totalNodes; index++) {
+			context.nodes.push(
+				await setup(index, p2pRegistry, crypto, prepareNodeValidators(validators, index, totalNodes)),
+			);
+		}
+
+		await bootMany(context.nodes);
+		await bootstrapMany(context.nodes);
+		await runMany(context.nodes);
 	});
 
-	afterEach(async (context) => {
-		await stop(context.node0);
-		await stop(context.node1);
+	afterEach(async ({ nodes }) => {
+		await stopMany(nodes);
 	});
 
-	it("should create new block", async (context) => {
-		await snoozeForBlock([context.node0, context.node1]);
+	it("should create new block", async ({ nodes }) => {
+		await snoozeForBlock(nodes);
 
-		const commit = await getLastCommit(context.node0);
+		const commit = await getLastCommit(nodes[0]);
 
-		await assertBockHeight([context.node0, context.node1], 1);
-		await assertBlockId([context.node0, context.node1], commit.block.data.id);
-		await assertCommitValidators([context.node0, context.node1], allValidators);
+		await assertBockHeight(nodes, 1);
+		await assertBlockId(nodes, commit.block.data.id);
+		await assertCommitValidators(nodes, allValidators);
 	});
 
-	it("should create new block second time", async (context) => {
-		await snoozeForBlock([context.node0, context.node1]);
+	it("should create new block second time", async ({ nodes }) => {
+		await snoozeForBlock(nodes);
 
-		const commit = await getLastCommit(context.node0);
+		const commit = await getLastCommit(nodes[0]);
 
-		await assertBockHeight([context.node0, context.node1], 1);
-		await assertBlockId([context.node0, context.node1], commit.block.data.id);
-		await assertCommitValidators([context.node0, context.node1], allValidators);
+		await assertBockHeight(nodes, 1);
+		await assertBlockId(nodes, commit.block.data.id);
+		await assertCommitValidators(nodes, allValidators);
 	});
 });
