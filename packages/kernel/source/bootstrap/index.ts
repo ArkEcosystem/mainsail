a71f6a69d9ef61e118e58764dac3f4e5b7e4a923
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2024-03-28 13:12:28 +0100

    refactor(kernel): create bootstraper for shutdown signals  (#511)
    * Register error handler first
    
    * Move listen to shutdown signals into bootstrapper

--- packages/kernel/source/bootstrap/index.ts
+++ packages/kernel/source/bootstrap/index.ts
@@ -1,4 +1,5 @@
 import { BootServiceProviders } from "./boot-service-providers.js";
+import { ListenToShutdownSignals } from "./listen-to-shutdown-signals.js";
 import { LoadConfiguration } from "./load-configuration.js";
 import { LoadEnvironmentVariables } from "./load-environment-variables.js";
 import { LoadServiceProviders } from "./load-service-providers.js";
@@ -22,8 +23,9 @@ export { RegisterServiceProviders } from "./register-service-providers.js";
 
 export const Bootstrappers = {
 	app: [
-		RegisterBaseServiceProviders,
 		RegisterErrorHandler,
+		ListenToShutdownSignals,
+		RegisterBaseServiceProviders,
 		RegisterBaseConfiguration,
 		RegisterBaseBindings,
 		RegisterBasePaths,
