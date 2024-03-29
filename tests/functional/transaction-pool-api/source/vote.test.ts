da2ed190846bd04ba9663a6452404bb3d06c24ac
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-03-29 17:06:21 +0900

    votes

--- tests/functional/transaction-pool-api/source/vote.test.ts
+++ tests/functional/transaction-pool-api/source/vote.test.ts
@@ -133,4 +133,78 @@ describe<{
 			},
 		});
 	});
+
+	it("should only accept one vote per sender in pool at the same time", async ({ sandbox, wallets }) => {
+		const [validator1, validator2] = wallets;
+
+		const randomWallet = await getRandomFundedWallet(sandbox, validator1);
+
+		const voteForValidator1Tx = await makeVote(sandbox, {
+			nonceOffset: 0,
+			sender: randomWallet,
+			voteAsset: validator1.publicKey,
+		});
+
+		const voteForValidator1TxDuplicate = await makeVote(sandbox, {
+			nonceOffset: 1,
+			sender: randomWallet,
+			voteAsset: validator1.publicKey,
+		});
+
+		const voteForValidator2Tx = await makeVote(sandbox, {
+			nonceOffset: 2,
+			sender: randomWallet,
+			voteAsset: validator2.publicKey,
+		});
+
+		// Only accepts first vote
+		const result = await addTransactionsToPool(sandbox, [
+			voteForValidator1Tx,
+			voteForValidator1TxDuplicate,
+			voteForValidator2Tx,
+		]);
+		assert.equal(result.accept, [0]);
+		assert.equal(result.invalid, [1, 2]);
+		assert.equal(result.errors, {
+			1: {
+				message: `tx ${voteForValidator1TxDuplicate.id} cannot be applied: Sender ${randomWallet.publicKey} already has a transaction of type '3' in the pool`,
+				type: "ERR_APPLY",
+			},
+			2: {
+				message: `tx ${voteForValidator2Tx.id} cannot be applied: Sender ${randomWallet.publicKey} already has a transaction of type '3' in the pool`,
+				type: "ERR_APPLY",
+			},
+		});
+	});
+
+	it("should reject vote when already voted", async ({ sandbox, wallets }) => {
+		const [validator1, validator2] = wallets;
+
+		const randomWallet = await getRandomFundedWallet(sandbox, validator1);
+
+		const voteForValidator1Tx = await makeVote(sandbox, {
+			nonceOffset: 0,
+			sender: randomWallet,
+			voteAsset: validator1.publicKey,
+		});
+
+		const voteForValidator2Tx = await makeVote(sandbox, {
+			nonceOffset: 1,
+			sender: randomWallet,
+			voteAsset: validator2.publicKey,
+		});
+
+		let result = await addTransactionsToPool(sandbox, [voteForValidator1Tx]);
+		assert.equal(result.accept, [0]);
+		await waitBlock(sandbox);
+
+		result = await addTransactionsToPool(sandbox, [voteForValidator2Tx]);
+		assert.equal(result.invalid, [0]);
+		assert.equal(result.errors, {
+			0: {
+				message: `tx ${voteForValidator2Tx.id} cannot be applied: Failed to apply transaction, because the sender wallet has already voted.`,
+				type: "ERR_APPLY",
+			},
+		});
+	});
 });
