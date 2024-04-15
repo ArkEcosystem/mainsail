96f35698199b7cc2531b2daa8c28b404fa32df36
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-04-15 16:36:21 +0900

    add check for target block height

--- packages/webhooks/source/defaults.ts
+++ packages/webhooks/source/defaults.ts
@@ -2,13 +2,16 @@ import { Constants } from "@mainsail/contracts";
 import { Environment } from "@mainsail/kernel";
 
 export const defaults = {
-	enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_WEBHOOKS_ENABLED),
+	enabled: true,
 	server: {
 		http: {
 			host: Environment.get(Constants.EnvironmentVariables.CORE_WEBHOOKS_HOST, "0.0.0.0"),
 			port: Environment.get(Constants.EnvironmentVariables.CORE_WEBHOOKS_PORT, 4004),
 		},
-		whitelist: ["127.0.0.1", "::ffff:127.0.0.1"],
+		whitelist: Environment.get<undefined>(Constants.EnvironmentVariables.CORE_WEBHOOKS_WHITELIST)?.split(",") ?? [
+			"127.0.0.1",
+			"::ffff:127.0.0.1",
+		],
 	},
 	timeout: Environment.get(Constants.EnvironmentVariables.CORE_WEBHOOKS_TIMEOUT, 1500),
 };
--- tests/e2e/consensus/peer-discovery/index.js
+++ tests/e2e/consensus/peer-discovery/index.js
@@ -5,11 +5,13 @@ const ips = {};
 const sources = [];
 app.get("/", function (req, res) {
     console.log(`received peer discovery request from ${req.ip} !`);
-    if (!ips[req.ip]) {
+
+    if (!ips[req.ip] && !req.headers["x-mainsail-e2e-no-peer"]) {
         ips[req.ip] = true;
         sources.push({ ip: req.ip, port: 4000 });
     }
     console.log(`responding with ${JSON.stringify(sources)}`);
+    res.setHeader("x-mainsail-e2e-my-ip", req.ip);
     res.send(sources);
 });
 
