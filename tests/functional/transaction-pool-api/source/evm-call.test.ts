a3020aecc9295af4bde86d9a6f6061ae5afff4e1
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-04-23 17:29:05 +0900

    fix check error

--- tests/functional/transaction-pool-api/source/evm-call.test.ts
+++ tests/functional/transaction-pool-api/source/evm-call.test.ts
@@ -4,7 +4,7 @@ import { EvmCalls } from "@mainsail/test-transaction-builders";
 // import { ContractAbis, Identifiers as EvmDevelopmentIdentifiers } from "@mainsail/evm-development";
 import { setup, shutdown } from "./setup.js";
 import { Snapshot, takeSnapshot } from "./snapshot.js";
-import { addTransactionsToPool, getWallets, isTransactionCommitted, waitBlock, waitForEvmResult } from "./utils.js";
+import { addTransactionsToPool, getWallets, isTransactionCommitted, waitBlock } from "./utils.js";
 
 describe<{
 	sandbox: Sandbox;
