494cdcda5f52cebb9d37efe7e47ca892130e0369
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-04-24 17:20:55 +0900

    fix mock

--- packages/evm/source/instances/mock.ts
+++ packages/evm/source/instances/mock.ts
@@ -1,18 +1,28 @@
 import { injectable } from "@mainsail/container";
-import { Contracts, Exceptions } from "@mainsail/contracts";
+import { Contracts } from "@mainsail/contracts";
 
 @injectable()
 export class MockInstance implements Contracts.Evm.Instance {
-	public async transact(txContext: Contracts.Evm.TransactionContext): Promise<Contracts.Evm.TransactionResult> {
+	private _pendingTransactions: Contracts.Evm.TransactionContext[] = [];
+
+	public async process(txContext: Contracts.Evm.TransactionContext): Promise<Contracts.Evm.ProcessResult> {
+		if (!txContext.readonly) {
+			this._pendingTransactions.push(txContext);
+		}
+
 		return {
-			gasRefunded: BigInt(0),
-			gasUsed: BigInt(0), // TODO: Return provided gas
-			logs: [],
-			success: true,
+			receipt: {
+				gasRefunded: BigInt(0),
+				gasUsed: BigInt(0), // TODO: Return provided gas
+				logs: [],
+				success: true,
+			},
 		};
 	}
 
-	public async view(txContext: Contracts.Evm.TransactionContext): Promise<Contracts.Evm.TransactionResult> {
-		throw new Exceptions.NotImplemented("view", this.constructor.name);
+	public async commit(): Promise<Contracts.Evm.CommitResult> {
+		this._pendingTransactions = [];
+
+		return {};
 	}
 }
