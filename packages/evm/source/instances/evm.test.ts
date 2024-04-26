8dd37166b20a84e8cf67144898125b736d63f0ea
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-04-26 17:16:28 +0900

    test auto commit

--- packages/evm/source/instances/evm.test.ts
+++ packages/evm/source/instances/evm.test.ts
@@ -91,6 +91,101 @@ describe<{
 		assert.equal(receipt.gasUsed, 21_070n);
 	});
 
+	it("should deploy, transfer and call balanceOf [no auto commit]", async ({ instance }) => {
+		await instance.setAutoCommit(false);
+
+		const roundKey = { height: BigInt(0), round: BigInt(0) };
+
+		const [sender, recipient] = wallets;
+
+		let { receipt } = await instance.process({
+			roundKey,
+			caller: sender.address,
+			data: Buffer.from(bytecode.slice(2), "hex"),
+		});
+
+		assert.true(receipt.success);
+		assert.equal(receipt.gasUsed, 964_156n);
+		assert.equal(receipt.deployedContractAddress, "0x0c2485e7d05894BC4f4413c52B080b6D1eca122a");
+
+		const contractAddress = receipt.deployedContractAddress;
+		assert.defined(contractAddress);
+
+		const iface = new ethers.Interface(abi);
+		const amount = ethers.parseEther("1337");
+
+		const transferEncodedCall = iface.encodeFunctionData("transfer", [recipient.address, amount]);
+		({ receipt } = await instance.process({
+			roundKey,
+			caller: sender.address,
+			data: Buffer.from(ethers.getBytes(transferEncodedCall)),
+			recipient: contractAddress,
+		}));
+
+		assert.true(receipt.success);
+		assert.equal(receipt.gasUsed, 52_222n);
+
+		const balanceOfEncodedCall = iface.encodeFunctionData("balanceOf", [recipient.address]);
+		({ receipt } = await instance.process({
+			roundKey,
+			caller: sender.address,
+			data: Buffer.from(ethers.getBytes(balanceOfEncodedCall)),
+			recipient: contractAddress,
+		}));
+
+		assert.true(receipt.success);
+		assert.equal(receipt.gasUsed, 24_295n);
+
+		const [balance] = iface.decodeFunctionResult("balanceOf", receipt.output!);
+		assert.equal(amount, balance);
+	});
+
+	it("should assert pending state is modified within same context", async ({ instance }) => {
+		await instance.setAutoCommit(false);
+
+		const [sender, recipient] = wallets;
+
+		const iface = new ethers.Interface(abi);
+		const transferEncodedCall = iface.encodeFunctionData("transfer", [recipient.address, ethers.parseEther("1")]);
+
+		const roundKey1 = { height: BigInt(0), round: BigInt(0) };
+
+		await assert.resolves(async () =>
+			instance.process({
+				roundKey: roundKey1,
+				caller: sender.address,
+				data: Buffer.from(ethers.getBytes(transferEncodedCall)),
+			}),
+		);
+
+		// Calling with same context again is fine
+		await assert.resolves(async () =>
+			instance.process({
+				roundKey: roundKey1,
+				caller: sender.address,
+				data: Buffer.from(ethers.getBytes(transferEncodedCall)),
+			}),
+		);
+
+		// Calling with different context is not fine
+		await assert.rejects(async () =>
+			instance.process({
+				roundKey: { ...roundKey1, round: BigInt(1) },
+				caller: sender.address,
+				data: Buffer.from(ethers.getBytes(transferEncodedCall)),
+			}),
+		);
+
+		// Still fine
+		await assert.resolves(async () =>
+			instance.process({
+				roundKey: roundKey1,
+				caller: sender.address,
+				data: Buffer.from(ethers.getBytes(transferEncodedCall)),
+			}),
+		);
+	});
+
 	it("should not throw when commit is empty", async ({ instance }) => {
 		await assert.resolves(async () => await instance.onCommit({ height: 0, round: 0 } as any));
 	});
