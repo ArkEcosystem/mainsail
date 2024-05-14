bc85638e4897d6b1067aaf497e2f02415e8c8df9
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-05-14 18:25:54 +0000

    Add defaults

--- packages/transaction-pool-server/source/defaults.ts
+++ packages/transaction-pool-server/source/defaults.ts
@@ -7,23 +7,23 @@ export const defaults = {
 			limit: 100,
 		},
 		socketTimeout: 5000,
-		trustProxy: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_TRANSACTION_POOL_TRUST_PROXY),
+		trustProxy: Environment.isTrue(Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_SERVER_TRUST_PROXY),
 		whitelist: ["*"],
 	},
 	server: {
 		http: {
-			enabled: !Environment.isTrue(Constants.EnvironmentVariables.CORE_API_TRANSACTION_POOL_DISABLED),
-			host: Environment.get(Constants.EnvironmentVariables.CORE_API_TRANSACTION_POOL_HOST, "0.0.0.0"),
-			port: Environment.get(Constants.EnvironmentVariables.CORE_API_TRANSACTION_POOL_PORT, 4007),
+			enabled: !Environment.isTrue(Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_SERVER_DISABLED),
+			host: Environment.get(Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_SERVER_HOST, "0.0.0.0"),
+			port: Environment.get(Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_SERVER_PORT, 4007),
 		},
 		// @see https://hapijs.com/api#-serveroptionstls
 		https: {
-			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_API_TRANSACTION_POOL_SSL),
-			host: Environment.get(Constants.EnvironmentVariables.CORE_API_TRANSACTION_POOL_SSL_HOST, "0.0.0.0"),
-			port: Environment.get(Constants.EnvironmentVariables.CORE_API_TRANSACTION_POOL_SSL_PORT, 8447),
+			enabled: Environment.isTrue(Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_SERVER_SSL),
+			host: Environment.get(Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_SERVER_SSL_HOST, "0.0.0.0"),
+			port: Environment.get(Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_SERVER_SSL_PORT, 8447),
 			tls: {
-				cert: Environment.get(Constants.EnvironmentVariables.CORE_API_TRANSACTION_POOL_SSL_CERT),
-				key: Environment.get(Constants.EnvironmentVariables.CORE_API_TRANSACTION_POOL_SSL_KEY),
+				cert: Environment.get(Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_SERVER_SSL_CERT),
+				key: Environment.get(Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_SERVER_SSL_KEY),
 			},
 		},
 	},
