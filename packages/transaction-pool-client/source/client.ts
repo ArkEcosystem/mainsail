04cbbd3f77c48a658836c06be4d995f4490ebab5
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-05-16 10:13:31 +0000

    Client getTx

--- packages/transaction-pool-client/source/client.ts
+++ packages/transaction-pool-client/source/client.ts
@@ -1,9 +1,36 @@
 import { injectable } from "@mainsail/container";
 import { Contracts } from "@mainsail/contracts";
+import { http } from "@mainsail/utils";
 
 @injectable()
 export class Client implements Contracts.TransactionPool.Client {
 	public async getTx(): Promise<Contracts.Crypto.Transaction[]> {
+		try {
+			const response = await this.#call("transactionPool.getTransactions", {});
+			console.log(response);
+		} catch (error) {
+			console.log(error);
+		}
+
 		return [];
+	}
+
+	// eslint-disable-next-line unicorn/no-null
+	async #call<T>(method: string, parameters: any, id: null | number = null): Promise<T> {
+		const response = await http.post("http://localhost:3000", {
+			body: {
+				id,
+				jsonrpc: "2.0",
+				method,
+				params: parameters,
+			},
+		});
+
+		if (response.statusCode === 200) {
+			// TODO: Validate response
+			return response.data;
+		}
+
+		throw new Error(`Failed to call method: ${method}`);
 	}
 }
