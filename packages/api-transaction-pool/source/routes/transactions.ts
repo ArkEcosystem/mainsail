3a3fae4e4bc7880393215f0a7ca6a87e56aa47ea
Sebastijan K <58827427+sebastijankuzner@users.noreply.github.com>
2024-05-20 18:44:03 +0200

    feat(transaction-pool-api): add transactions endpoints (#585)
    * Rename route
    
    * /unconfirmed
    
    * /unconfirmed/{id}
    
    * /types
    
    * /unconfirmed
    
    * Remove unused tag

--- packages/api-transaction-pool/source/routes/transactions.ts
+++ packages/api-transaction-pool/source/routes/transactions.ts
@@ -3,7 +3,8 @@ import { Contracts, Identifiers } from "@mainsail/contracts";
 import { Providers } from "@mainsail/kernel";
 import Joi from "joi";
 
-import { TransactionsController } from "../controllers/transaction-pool.js";
+import { TransactionsController } from "../controllers/transactions.js";
+import { pagination } from "../schemas.js";
 
 export const register = (server: Contracts.Api.ApiServer): void => {
 	const controller = server.app.app.resolve(TransactionsController);
@@ -46,6 +47,49 @@ export const register = (server: Contracts.Api.ApiServer): void => {
 				}),
 			},
 		},
-		path: "/transaction-pool",
+		path: "/transactions",
+	});
+
+	server.route({
+		handler: (request: Hapi.Request) => controller.unconfirmed(request),
+		method: "GET",
+		options: {
+			plugins: {
+				pagination: {
+					enabled: true,
+				},
+			},
+			validate: {
+				query: Joi.object({
+					transform: Joi.bool().default(true),
+				}).concat(pagination),
+			},
+		},
+		path: "/transactions/unconfirmed",
+	});
+
+	server.route({
+		handler: (request: Hapi.Request) => controller.showUnconfirmed(request),
+		method: "GET",
+		options: {
+			validate: {
+				params: Joi.object({
+					id: Joi.string().hex().length(64),
+				}),
+			},
+		},
+		path: "/transactions/unconfirmed/{id}",
+	});
+
+	server.route({
+		handler: (request: Hapi.Request) => controller.types(request),
+		method: "GET",
+		path: "/transactions/types",
+	});
+
+	server.route({
+		handler: (request: Hapi.Request) => controller.schemas(request),
+		method: "GET",
+		path: "/transactions/schemas",
 	});
 };
