784340d6a07f9629e12b14105b0794288b45c21e
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-06-03 09:15:38 +0000

    Improve handler

--- packages/kernel/source/ipc/handler.ts
+++ packages/kernel/source/ipc/handler.ts
@@ -6,27 +6,21 @@ export class Handler<T extends {}> implements Contracts.Kernel.IPC.Handler<T> {
 
 	public constructor(handler: T) {
 		this.handler = handler;
+
+		this.handleRequest();
 	}
 
-	public handleAction<K extends Contracts.Kernel.IPC.Actions<T>>(method: K): void {
-		parentPort?.on("message", (message) => {
-			if (message.method === method) {
-				// @ts-ignore
-				this.handler[method](...message.args);
-			}
-		});
-	}
-
-	public handleRequest<K extends Contracts.Kernel.IPC.Requests<T>>(method: K): void {
+	public handleRequest(): void {
 		parentPort?.on("message", async (message) => {
-			if (message.method === method) {
-				try {
-					// @ts-ignore
-					const result = await this.handler[method](...message.args);
-					parentPort?.postMessage({ id: message.id, result });
-				} catch (error) {
-					parentPort?.postMessage({ error: error.message, id: message.id });
-				}
+			if (this.handler[message.method] === undefined) {
+				throw new Error(`Method ${message.method} is not defined on the handler`);
+			}
+
+			try {
+				const result = await this.handler[message.method](...message.args);
+				parentPort?.postMessage({ id: message.id, result });
+			} catch (error) {
+				parentPort?.postMessage({ error: error.message, id: message.id });
 			}
 		});
 	}
--- packages/transaction-pool-worker/source/worker-script.ts
+++ packages/transaction-pool-worker/source/worker-script.ts
@@ -2,7 +2,4 @@ import { Ipc } from "@mainsail/kernel";
 
 import { WorkerScriptHandler } from "./worker-handler.js";
 
-const ipcHandler = new Ipc.Handler(new WorkerScriptHandler());
-
-ipcHandler.handleRequest("boot");
-ipcHandler.handleRequest("importSnapshot");
+new Ipc.Handler(new WorkerScriptHandler());
