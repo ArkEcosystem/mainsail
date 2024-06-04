6ffb5b353754732e49da4f6d6a615a282730b653
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-06-04 10:32:30 +0000

    Fix unit tests

--- packages/crypto-key-pair-ecdsa/source/index.ts
+++ packages/crypto-key-pair-ecdsa/source/index.ts
@@ -8,6 +8,9 @@ import { PublicKeyFactory } from "./public.js";
 import { schemas } from "./schemas.js";
 import { PublicKeySerializer } from "./serializer.js";
 
+export * from "./pair.js";
+export * from "./schemas.js";
+
 export class ServiceProvider extends Providers.ServiceProvider {
 	public async register(): Promise<void> {
 		this.app
