39e41d4e587b0f77f1167acd0ab7b0ba151e83e9
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-06-05 10:11:49 +0000

    Add env variables

--- packages/transaction-pool-broadcaster/source/defaults.ts
+++ packages/transaction-pool-broadcaster/source/defaults.ts
@@ -1,6 +1,12 @@
+import { Constants } from "@mainsail/contracts";
+import { Environment } from "@mainsail/kernel";
+
 export const defaults = {
 	blacklist: [],
-	maxPeersBroadcast: 3,
-	maxSequentialErrors: 3,
+	maxPeersBroadcast: Environment.get(Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_MAX_PEER_BROADCAST, 3),
+	maxSequentialErrors: Environment.get(
+		Constants.EnvironmentVariables.CORE_TRANSACTION_POOL_MAX_PEER_SEQUENTIAL_ERRORS,
+		2,
+	),
 	whitelist: ["*"],
 };
