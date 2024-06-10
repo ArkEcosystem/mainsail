f0f1b826cdd5ab7a55f04594344c2fc3d23ff39c
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-06-10 16:01:17 +0900

    fix pool mock

--- packages/transaction-pool-service/source/actions/apply-transaction.ts
+++ packages/transaction-pool-service/source/actions/apply-transaction.ts
@@ -1,19 +1,18 @@
-import { inject, tagged } from "@mainsail/container";
-import { Contracts, Identifiers } from "@mainsail/contracts";
-import { Services, Types } from "@mainsail/kernel";
+import { Contracts } from "@mainsail/contracts";
+import { Types } from "@mainsail/kernel";
+import { TransactionTriggerAction } from "./transaction-trigger-action.js";
 
-export class ApplyTransactionAction extends Services.Triggers.Action {
-	@inject(Identifiers.Evm.Instance)
-	@tagged("instance", "mock")
-	private readonly evm!: Contracts.Evm.Instance;
-
+export class ApplyTransactionAction extends TransactionTriggerAction {
 	public async execute(arguments_: Types.ActionArguments): Promise<void> {
 		const handler: Contracts.Transactions.TransactionHandler = arguments_.handler;
 		const transaction: Contracts.Crypto.Transaction = arguments_.transaction;
 		const walletRepository: Contracts.State.WalletRepository = arguments_.walletRepository;
 
 		await handler.apply(
-			{ evm: { commitKey: { height: BigInt(0), round: BigInt(0) }, instance: this.evm }, walletRepository },
+			{
+				evm: this.mockEvmContext(),
+				walletRepository,
+			},
 			transaction,
 		);
 	}
