884b85e665b81d56586a79a3bb44a1c37032c5fc
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-06-20 11:19:43 +0000

    Remove revert on sender state

--- packages/transaction-pool-service/source/sender-mempool.ts
+++ packages/transaction-pool-service/source/sender-mempool.ts
@@ -66,32 +66,30 @@ export class SenderMempool implements Contracts.TransactionPool.SenderMempool {
 	}
 
 	public async removeTransaction(id: string): Promise<Contracts.Crypto.Transaction[]> {
-		try {
-			this.#concurrency++;
-
-			return await this.#lock.runExclusive(async () => {
-				const index = this.#transactions.findIndex((t) => t.id === id);
-				if (index === -1) {
-					return [];
-				}
-
-				const removedTransactions: Contracts.Crypto.Transaction[] = this.#transactions
-					.splice(index, this.#transactions.length - index)
-					.reverse();
-
-				try {
-					for (const removedTransaction of removedTransactions) {
-						await this.senderState.revert(removedTransaction);
-					}
-					return removedTransactions;
-				} catch {
-					const otherRemovedTransactions = this.#transactions.splice(0, this.#transactions.length).reverse();
-					return [...removedTransactions, ...otherRemovedTransactions];
-				}
-			});
-		} finally {
-			this.#concurrency--;
-		}
+		return [];
+		// try {
+		// 	this.#concurrency++;
+		// 	return await this.#lock.runExclusive(async () => {
+		// 		const index = this.#transactions.findIndex((t) => t.id === id);
+		// 		if (index === -1) {
+		// 			return [];
+		// 		}
+		// 		const removedTransactions: Contracts.Crypto.Transaction[] = this.#transactions
+		// 			.splice(index, this.#transactions.length - index)
+		// 			.reverse();
+		// 		try {
+		// 			for (const removedTransaction of removedTransactions) {
+		// 				await this.senderState.revert(removedTransaction);
+		// 			}
+		// 			return removedTransactions;
+		// 		} catch {
+		// 			const otherRemovedTransactions = this.#transactions.splice(0, this.#transactions.length).reverse();
+		// 			return [...removedTransactions, ...otherRemovedTransactions];
+		// 		}
+		// 	});
+		// } finally {
+		// 	this.#concurrency--;
+		// }
 	}
 
 	public async removeForgedTransaction(id: string): Promise<Contracts.Crypto.Transaction[]> {
