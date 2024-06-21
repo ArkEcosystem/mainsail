3b39b0ffa7ccc2da4874af6327f1389dd00b0ee0
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-06-21 10:58:29 +0000

    Add events

--- packages/webhooks/source/events.ts
+++ packages/webhooks/source/events.ts
@@ -1,4 +1,7 @@
 export enum WebhookEvent {
 	Broadcasted = "webhooks.broadcasted",
 	Failed = "webhooks.failed",
+	Created = "webhooks.created",
+	Updated = "webhooks.updated",
+	Removed = "webhooks.removed",
 }
