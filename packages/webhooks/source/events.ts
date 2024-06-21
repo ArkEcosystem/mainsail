7c3e98d69f3c1da2bcb94f5cd7ba6f122ba2c70e
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-06-21 12:15:43 +0000

    Move webhook events

--- packages/webhooks/source/events.ts
+++
@@ -1,7 +0,0 @@
-export enum WebhookEvent {
-	Broadcasted = "webhooks.broadcasted",
-	Failed = "webhooks.failed",
-	Created = "webhooks.created",
-	Updated = "webhooks.updated",
-	Removed = "webhooks.removed",
-}
