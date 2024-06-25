9828db3c4f15d42d956d2e1037ba46c1835efac9
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-06-25 12:51:08 +0900

    add new keywords

--- packages/crypto-validation/source/keywords.ts
+++ packages/crypto-validation/source/keywords.ts
@@ -1,7 +1,8 @@
+import { Contracts } from "@mainsail/contracts";
 import { BigNumber } from "@mainsail/utils";
 import { AnySchemaObject, FuncKeywordDefinition } from "ajv";
 
-export const makeKeywords = () => {
+export const makeKeywords = (configuration: Contracts.Crypto.Configuration) => {
 	const maxBytes: FuncKeywordDefinition = {
 		compile: (schema) => (data) => Buffer.byteLength(data, "utf8") <= schema,
 		errors: false,
@@ -51,5 +52,68 @@ export const makeKeywords = () => {
 		},
 	};
 
-	return { bignum, maxBytes };
+	const transactionGasLimit: FuncKeywordDefinition = {
+		// @ts-ignore
+		compile(schema) {
+			return (data, parentSchema: AnySchemaObject) => {
+				const {
+					gas: { minimumGasLimit, maximumGasLimit },
+				} = configuration.getMilestone();
+
+				try {
+					const bignum = BigNumber.make(data);
+					if (bignum.isLessThan(minimumGasLimit)) {
+						return false;
+					}
+
+					if (bignum.isGreaterThan(maximumGasLimit)) {
+						return false;
+					}
+				} catch {
+					return false;
+				}
+
+				return true;
+			};
+		},
+		errors: false,
+		keyword: "transactionGasLimit",
+		metaSchema: {
+			properties: {},
+			type: "object",
+		},
+	};
+
+	const bytecode: FuncKeywordDefinition = {
+		// @ts-ignore
+		compile(schema) {
+			return (data, parentSchema: AnySchemaObject) => {
+				const {
+					gas: { maximumGasLimit },
+				} = configuration.getMilestone();
+
+				if (typeof data !== "string") {
+					return false;
+				}
+
+				// The allowed bytecode length is relative to the maximum transaction gas limit
+				const maxBytecodeLength = maximumGasLimit / 2;
+				const minBytecodeLength = 0;
+
+				const regex = new RegExp(`^(0x)?[0-9a-fA-F]{${minBytecodeLength},${maxBytecodeLength}}$`);
+				if (!regex.test(data)) {
+					return false;
+				}
+				return true;
+			};
+		},
+		errors: false,
+		keyword: "bytecode",
+		metaSchema: {
+			properties: {},
+			type: "object",
+		},
+	};
+
+	return { bignum, bytecode, maxBytes, transactionGasLimit };
 };
