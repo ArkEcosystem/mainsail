eb3b34e664c7149773da5c9b7b9c7d832fea7b26
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-07-01 14:31:03 +0000

    Update get status

--- packages/p2p/source/socket-server/codecs/get-status.ts
+++ packages/p2p/source/socket-server/codecs/get-status.ts
@@ -1,7 +1,3 @@
-import { Contracts } from "@mainsail/contracts";
-import { Utils } from "@mainsail/kernel";
-import { BigNumber } from "@mainsail/utils";
-
 import type * as types from "./proto/protos.d.ts";
 import * as _protos from "./proto/protos.js";
 
@@ -14,35 +10,11 @@ export const getStatus = {
 			Buffer.from(proto.GetStatusRequest.encode(object).finish()),
 	},
 	response: {
-		deserialize: (payload: Buffer): Contracts.P2P.PeerPingResponse => {
-			const decoded = proto.GetStatusResponse.decode(payload).toJSON();
-
-			Utils.assert.defined<string>(decoded.state?.header?.totalAmount);
-			Utils.assert.defined<string>(decoded.state?.header?.totalFee);
-			Utils.assert.defined<string>(decoded.state?.header?.reward);
-
-			const totalAmount = new BigNumber(decoded.state.header.totalAmount);
-			const totalFee = new BigNumber(decoded.state.header.totalFee);
-			const reward = new BigNumber(decoded.state.header.reward);
-
-			return {
-				...decoded,
-				state: {
-					...decoded.state,
-					header: {
-						...decoded.state?.header,
-						reward,
-						totalAmount,
-						totalFee,
-					},
-				},
-			} as Contracts.P2P.PeerPingResponse;
-		},
-		serialize: (object: Contracts.P2P.PeerPingResponse): Buffer => {
-			object.state.header.totalAmount = object.state.header.totalAmount.toString();
-			object.state.header.totalFee = object.state.header.totalFee.toString();
-			object.state.header.reward = object.state.header.reward.toString();
-			return Buffer.from(proto.GetStatusResponse.encode(object).finish());
-		},
+		deserialize: (payload: Buffer): types.getStatus.IGetStatusResponse =>
+			proto.GetStatusResponse.toObject(proto.GetStatusResponse.decode(payload), {
+				defaults: true,
+			}),
+		serialize: (object: types.getStatus.IGetStatusResponse): Buffer =>
+			Buffer.from(proto.GetStatusResponse.encode(object).finish()),
 	},
 };
