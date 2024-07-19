a5db358f862d6ba1b909f99f0cb9cd2182c2be0d
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-07-19 17:37:39 +0000

    Add web3ClientVersion

---
+++ packages/api-evm/source/actions/web3-client-version.ts
@@ -0,0 +1,20 @@
+import { inject, injectable } from "@mainsail/container";
+import { Contracts, Identifiers } from "@mainsail/contracts";
+
+@injectable()
+export class Web3ClientVersionAction implements Contracts.Api.RPC.Action {
+	@inject(Identifiers.Application.Version)
+	private readonly version!: string;
+
+	public readonly name: string = "web3_clientVersion";
+
+	public readonly schema = {
+		$id: `jsonRpc_${this.name}`,
+		maxItems: 0,
+		type: "array",
+	};
+
+	public async handle(parameters: []): Promise<string> {
+		return `@mainsail/core/${this.version}`;
+	}
+}
