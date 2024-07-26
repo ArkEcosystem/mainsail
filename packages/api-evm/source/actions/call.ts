4b5256e9b357bb8752baf55e853266dbddc69ca6
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-07-26 15:51:54 +0000

    Update tags

--- packages/api-evm/source/actions/call.ts
+++ packages/api-evm/source/actions/call.ts
@@ -2,7 +2,7 @@ import { inject, injectable, tagged } from "@mainsail/container";
 import { Contracts, Exceptions, Identifiers } from "@mainsail/contracts";
 import { ethers } from "ethers";
 
-type BlockTag = "latest" | "earliest" | "pending";
+type BlockTag = "latest" | "finalized" | "safe";
 
 type TxData = {
 	from: string;
@@ -35,7 +35,7 @@ export class CallAction implements Contracts.Api.RPC.Action {
 				required: ["from", "to", "data"],
 				type: "object",
 			},
-			{ enum: ["latest", "earliest", "pending"], type: "string" },
+			{ enum: ["latest", "finalized", "safe"], type: "string" },
 		],
 
 		type: "array",
