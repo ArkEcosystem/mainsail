8aa2e984988dc9c28fefe25d3113cb93a411bcbd
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-09-05 14:45:52 +0900

    consensus: try exclusive lock

--- packages/consensus/source/processors/commit-processor.ts
+++ packages/consensus/source/processors/commit-processor.ts
@@ -27,7 +27,7 @@ export class CommitProcessor extends AbstractProcessor implements Contracts.Cons
 	async process(commit: Contracts.Crypto.Commit): Promise<Contracts.Consensus.ProcessorResult> {
 		let promise: Promise<void> | undefined;
 
-		const result = await this.commitLock.runNonExclusive(async (): Promise<Contracts.Consensus.ProcessorResult> => {
+		const result = await this.commitLock.runExclusive(async (): Promise<Contracts.Consensus.ProcessorResult> => {
 			if (!this.#hasValidHeight(commit)) {
 				return Contracts.Consensus.ProcessorResult.Skipped;
 			}
