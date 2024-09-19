2010eba7bf9e0ed2566006c3d5a5d0529c74ef09
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-09-19 16:50:57 +0200

    Fix crypto-commit

--- packages/crypto-commit/test/helpers/prepare-sandbox.ts
+++ packages/crypto-commit/test/helpers/prepare-sandbox.ts
@@ -1,5 +1,5 @@
 import { Identifiers } from "@mainsail/contracts";
-import { ServiceProvider as CoreCryptoAddressBech32m } from "@mainsail/crypto-address-bech32m";
+import { ServiceProvider as CoreCryptoAddressKeccak256 } from "@mainsail/crypto-address-keccak256";
 import { ServiceProvider as CoreCryptoBlock } from "@mainsail/crypto-block";
 import { ServiceProvider as CoreCryptoConfig } from "@mainsail/crypto-config";
 import { ServiceProvider as CoreCryptoConsensus } from "@mainsail/crypto-consensus-bls12-381";
@@ -35,7 +35,7 @@ export const prepareSandbox = async (context) => {
 	await context.sandbox.app.resolve(CoreCryptoSignatureSchnorr).register();
 	await context.sandbox.app.resolve(CoreCryptoConsensus).register();
 	await context.sandbox.app.resolve(CoreCryptoKeyPairSchnorr).register();
-	await context.sandbox.app.resolve(CoreCryptoAddressBech32m).register();
+	await context.sandbox.app.resolve(CoreCryptoAddressKeccak256).register();
 	await context.sandbox.app.resolve(CoreCryptoWif).register();
 	await context.sandbox.app.resolve(CoreFees).register();
 	await context.sandbox.app.resolve(CoreFeesStatic).register();
