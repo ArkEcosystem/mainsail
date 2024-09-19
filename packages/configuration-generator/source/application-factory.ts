a0630f6f41c888f9e297ec45e23f1433a348c112
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-09-19 16:28:33 +0200

    Fix build

--- packages/configuration-generator/source/application-factory.ts
+++ packages/configuration-generator/source/application-factory.ts
@@ -1,7 +1,5 @@
 import { Container } from "@mainsail/container";
 import { Contracts, Exceptions, Identifiers } from "@mainsail/contracts";
-import { ServiceProvider as CoreCryptoAddressBase58 } from "@mainsail/crypto-address-base58";
-import { ServiceProvider as CoreCryptoAddressBech32m } from "@mainsail/crypto-address-bech32m";
 import { ServiceProvider as CoreCryptoAddressKeccak256 } from "@mainsail/crypto-address-keccak256";
 import { ServiceProvider as CoreCryptoBlock } from "@mainsail/crypto-block";
 import { ServiceProvider as CryptoCommit } from "@mainsail/crypto-commit";
@@ -55,16 +53,6 @@ export const makeApplication = async (configurationPath: string, options: Record
 	let addressMilestone;
 
 	switch (options.address) {
-		case "base58": {
-			await app.resolve(CoreCryptoAddressBase58).register();
-			addressMilestone = { base58: options.base58Prefix };
-			break;
-		}
-		case "bech32m": {
-			await app.resolve(CoreCryptoAddressBech32m).register();
-			addressMilestone = { bech32m: options.bech32mPrefix };
-			break;
-		}
 		case "keccak256": {
 			await app.resolve(CoreCryptoAddressKeccak256).register();
 			addressMilestone = { keccak256: true };
