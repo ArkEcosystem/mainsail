64f835f556db5ee76710401cb5433cab163badfa
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-09-24 12:15:25 +0200

    Remove defaults

--- packages/state/source/service-provider.ts
+++ packages/state/source/service-provider.ts
@@ -1,6 +1,5 @@
 import { Identifiers } from "@mainsail/contracts";
 import { Providers } from "@mainsail/kernel";
-import Joi from "joi";
 
 import { Service } from "./service.js";
 import { State } from "./state.js";
@@ -16,18 +15,5 @@ export class ServiceProvider extends Providers.ServiceProvider {
 
 		this.app.bind(Identifiers.State.Service).to(Service).inSingletonScope();
 		this.app.bind(Identifiers.State.State).to(State).inSingletonScope();
-	}
-
-	public configSchema(): Joi.AnySchema {
-		return Joi.object({
-			snapshots: Joi.object({
-				enabled: Joi.bool().required(),
-				interval: Joi.number().integer().min(1).required(),
-				retainFiles: Joi.number().integer().min(1).required(),
-				skipUnknownAttributes: Joi.bool().required(),
-			}).required(),
-		})
-			.required()
-			.unknown(true);
 	}
 }
