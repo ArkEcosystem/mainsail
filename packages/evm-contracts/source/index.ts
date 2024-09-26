870240a1248eed8bdfd8db732d380d4e440ee139
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-09-26 11:33:48 +0900

    add json abis

---
+++ packages/evm-contracts/source/index.ts
@@ -0,0 +1,4 @@
+import ConsensusAbi from "./abis/Consensus.json" with { type: "json" };
+import DirectTransferAbi from "./abis/DirectTransfer.json" with { type: "json" };
+
+export { ConsensusAbi, DirectTransferAbi };
