a68d68d7a891706078e2b2ac1952563c92d91b8c
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-10-16 15:25:35 +0200

    Get all validators

--- packages/evm-consensus/source/validator-set.ts
+++ packages/evm-consensus/source/validator-set.ts
@@ -21,14 +21,31 @@ export class ValidatorSet implements Contracts.ValidatorSet.Service {
 	#topValidators: Contracts.State.ValidatorWallet[] = [];
 	#indexByAddress: Map<string, number> = new Map();
 
+	#allValidators:  Map<string, Contracts.State.ValidatorWallet> = new Map();
+	#dirtyValidators: Contracts.State.ValidatorWallet[] = [];
+
+
 	public async restore(): Promise<void> {
 		await this.#buildActiveValidators();
+
+		const validators = await this.#getAllValidators();
+		this.#allValidators = new Map(validators.map((validator) => [validator.address, validator]));
 	}
 
 	public async onCommit(unit: Contracts.Processor.ProcessableUnit): Promise<void> {
 		if (Utils.roundCalculator.isNewRound(unit.height + 1, this.configuration)) {
 			await this.#buildActiveValidators();
 		}
+
+		await this.#calculateChangedValidators();
+	}
+
+	public getAllValidators(): Contracts.State.ValidatorWallet[] {
+		return [...this.#allValidators.values()];
+	}
+
+	public getDirtyValidators(): Contracts.State.ValidatorWallet[] {
+		return this.#dirtyValidators;
 	}
 
 	public getActiveValidators(): Contracts.State.ValidatorWallet[] {
@@ -66,6 +83,20 @@ export class ValidatorSet implements Contracts.ValidatorSet.Service {
 		this.#indexByAddress = new Map(this.#topValidators.map((validator, index) => [validator.address, index]));
 	}
 
+	async #calculateChangedValidators(): Promise<void> {
+		this.#dirtyValidators = [];
+
+		const validators = await this.#getAllValidators();
+		for (const validator of validators) {
+			const currentValidator = this.#allValidators.get(validator.address);
+			if (!currentValidator || !currentValidator.voteBalance.isEqualTo(validator.voteBalance) || currentValidator.isResigned !== validator.isResigned || currentValidator.votersCount !== validator.votersCount || currentValidator.blsPublicKey !== validator.blsPublicKey) {
+				this.#dirtyValidators.push(validator);
+			}
+		}
+
+		this.#allValidators = new Map(validators.map((validator) => [validator.address, validator]));
+	}
+
 	async #getActiveValidators(): Promise<Contracts.State.ValidatorWallet[]> {
 		const consensusContractAddress = this.app.get<string>(EvmConsensusIdentifiers.Contracts.Addresses.Consensus);
 		const deployerAddress = this.app.get<string>(EvmConsensusIdentifiers.Internal.Addresses.Deployer);
@@ -96,7 +127,7 @@ export class ValidatorSet implements Contracts.ValidatorSet.Service {
 				blsPublicKey: blsPublicKey.slice(2),
 				isResigned,
 				voteBalance: Utils.BigNumber.make(voteBalance),
-				votersCount,
+				votersCount: Number(votersCount),
 			};
 
 			validatorWallets.push(validatorWallet);
@@ -105,40 +136,42 @@ export class ValidatorSet implements Contracts.ValidatorSet.Service {
 		return validatorWallets;
 	}
 
-	// async #getAllValidators(): Promise<Contracts.State.ValidatorWallet[]> {
-	// 	const consensusContractAddress = this.app.get<string>(EvmConsensusIdentifiers.Contracts.Addresses.Consensus);
-	// 	const deployerAddress = this.app.get<string>(EvmConsensusIdentifiers.Internal.Addresses.Deployer);
-	// 	const { evmSpec } = this.configuration.getMilestone();
+	async #getAllValidators(): Promise<Contracts.State.ValidatorWallet[]> {
+		const consensusContractAddress = this.app.get<string>(EvmConsensusIdentifiers.Contracts.Addresses.Consensus);
+		const deployerAddress = this.app.get<string>(EvmConsensusIdentifiers.Internal.Addresses.Deployer);
+		const { evmSpec } = this.configuration.getMilestone();
 
-	// 	const iface = new ethers.Interface(ConsensusAbi.abi);
-	// 	const data = iface.encodeFunctionData("getAllValidators").slice(2);
+		const iface = new ethers.Interface(ConsensusAbi.abi);
+		const data = iface.encodeFunctionData("getAllValidators").slice(2);
 
-	// 	const result = await this.evm.view({
-	// 		caller: deployerAddress,
-	// 		data: Buffer.from(data, "hex"),
-	// 		recipient: consensusContractAddress,
-	// 		specId: evmSpec,
-	// 	});
+		const result = await this.evm.view({
+			caller: deployerAddress,
+			data: Buffer.from(data, "hex"),
+			recipient: consensusContractAddress,
+			specId: evmSpec,
+		});
 
-	// 	if (!result.success) {
-	// 		this.app.terminate("getAllValidators failed");
-	// 	}
+		if (!result.success) {
+			this.app.terminate("getAllValidators failed");
+		}
 
-	// 	const [validators] = iface.decodeFunctionResult("getAllValidators", result.output!);
+		const [validators] = iface.decodeFunctionResult("getAllValidators", result.output!);
 
-	// 	const validatorWallets: Contracts.State.ValidatorWallet[] = [];
-	// 	for (const [, validator] of validators.entries()) {
-	// 		const [address, [votersCount, voteBalance, isResigned, blsPublicKey]] = validator;
+		const validatorWallets: Contracts.State.ValidatorWallet[] = [];
+		for (const [, validator] of validators.entries()) {
+			const [address, [votersCount, voteBalance, isResigned, blsPublicKey]] = validator;
 
-	// 		const validatorWallet: Contracts.State.ValidatorWallet = {
-	// 			address,
-	// 			blsPublicKey: blsPublicKey.slice(2),
-	// 			voteBalance: Utils.BigNumber.make(voteBalance),
-	// 		};
+			const validatorWallet: Contracts.State.ValidatorWallet = {
+				address,
+				blsPublicKey: blsPublicKey.slice(2),
+				isResigned,
+				voteBalance: Utils.BigNumber.make(voteBalance),
+				votersCount: Number(votersCount),
+			};
 
-	// 		validatorWallets.push(validatorWallet);
-	// 	}
+			validatorWallets.push(validatorWallet);
+		}
 
-	// 	return validatorWallets;
-	// }
+		return validatorWallets;
+	}
 }
