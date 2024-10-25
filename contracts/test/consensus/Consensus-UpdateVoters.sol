a0f7a6090d389c502d348d7ca2be28ff8bb2b9ea
oXtxNt9U <oXtxNt9U@users.noreply.github.com>
2024-10-25 03:14:36 +0000

    style: resolve style guide violations

--- contracts/test/consensus/Consensus-UpdateVoters.sol
+++ contracts/test/consensus/Consensus-UpdateVoters.sol
@@ -5,17 +5,17 @@ import {Test, console} from "@forge-std/Test.sol";
 import {Consensus, ValidatorData, Validator, Unvoted, Voted} from "@contracts/consensus/Consensus.sol";
 
 contract ConsensusTest is Test {
-	Consensus public consensus;
+    Consensus public consensus;
 
-	function setUp() public {
-		consensus = new Consensus();
-	}
+    function setUp() public {
+        consensus = new Consensus();
+    }
 
-	function test_updateVoters_should_allow_only_caller() public {
-		address addr = address(1);
-		vm.startPrank(addr);
-		address[] memory voters = new address[](0);
-		vm.expectRevert("Caller is not the contract owner");
-		consensus.updateVoters(voters);
-	}
+    function test_updateVoters_should_allow_only_caller() public {
+        address addr = address(1);
+        vm.startPrank(addr);
+        address[] memory voters = new address[](0);
+        vm.expectRevert("Caller is not the contract owner");
+        consensus.updateVoters(voters);
+    }
 }
