e9a227b158742b3ff717d53648352cc5625c9209
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-11-15 15:08:50 +0100

    Add ERC1967Proxy

--- packages/evm-contracts/source/index.ts
+++ packages/evm-contracts/source/index.ts
@@ -1,3 +1,4 @@
 export { default as ConsensusAbi } from "./abis/ConsensusV1.json" with { type: "json" };
 export { default as DirectTransferAbi } from "./abis/DirectTransfer.json" with { type: "json" };
+export { default as ERC1967ProxyAbi } from "./abis/ERC1967Proxy.json" with { type: "json" };
 export * from "./function-sigs.js";
