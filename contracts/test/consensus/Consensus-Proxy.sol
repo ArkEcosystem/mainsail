31635ab59aa1f651c9f502764d003af9ddd473fb
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-11-15 13:27:25 +0100

    Test data after upgrade

--- contracts/test/consensus/Consensus-Proxy.sol
+++ contracts/test/consensus/Consensus-Proxy.sol
@@ -39,4 +39,61 @@ contract ConsensusTest is Base {
         vm.expectRevert(Initializable.InvalidInitialization.selector);
         consensusNew.initialize();
     }
+
+    function test_proxy_should_update_and_perserve_variables() public {
+        assertEq(consensus.version(), 1);
+
+        // Register valdiators
+        vm.startPrank(address(1));
+        consensus.registerValidator(prepareBLSKey(address(1)));
+        vm.startPrank(address(2));
+        consensus.registerValidator(prepareBLSKey(address(2)));
+        vm.startPrank(address(3));
+        consensus.registerValidator(prepareBLSKey(address(3)));
+
+        // Resign valdiator
+        consensus.resignValidator();
+
+        // Vote
+        address voterAddr = address(4);
+        vm.deal(voterAddr, 100 ether);
+        vm.startPrank(voterAddr);
+        consensus.vote(address(1));
+
+        vm.stopPrank();
+        consensus.calculateActiveValidators(2);
+
+        assertEq(consensus.version(), 1);
+        assertEq(consensus.registeredValidatorsCount(), 3);
+        assertEq(consensus.resignedValidatorsCount(), 1);
+        assertEq(consensus.activeValidatorsCount(), 2);
+        assertEq(consensus.getVotesCount(), 1);
+        assertEq(consensus.getActiveValidators().length, 2);
+        Validator[] memory validatorsBefore = consensus.getAllValidators();
+        assertEq(validatorsBefore.length, 3);
+
+        // Upgrade
+        consensus.upgradeToAndCall(address(new ConsensusVTest()), bytes(""));
+
+        // Cast proxy to new contract
+        ConsensusVTest consensusNew = ConsensusVTest(address(consensus));
+        assertEq(consensusNew.versionv2(), 99);
+        assertEq(consensusNew.version(), 1);
+        assertEq(consensusNew.registeredValidatorsCount(), 3);
+        assertEq(consensusNew.resignedValidatorsCount(), 1);
+        assertEq(consensusNew.activeValidatorsCount(), 2);
+        assertEq(consensusNew.getVotesCount(), 1);
+        assertEq(consensus.getActiveValidators().length, 2);
+        Validator[] memory validatorsAfter = consensusNew.getAllValidators();
+        assertEq(validatorsAfter.length, 3);
+
+        // Compare valdiators
+        for (uint256 i = 0; i < validatorsBefore.length; i++) {
+            assertEq(validatorsBefore[i].addr, validatorsAfter[i].addr);
+            assertEq(validatorsBefore[i].data.votersCount, validatorsAfter[i].data.votersCount);
+            assertEq(validatorsBefore[i].data.voteBalance, validatorsAfter[i].data.voteBalance);
+            assertEq(validatorsBefore[i].data.isResigned, validatorsAfter[i].data.isResigned);
+            assertEq(validatorsBefore[i].data.blsPublicKey, validatorsAfter[i].data.blsPublicKey);
+        }
+    }
 }
