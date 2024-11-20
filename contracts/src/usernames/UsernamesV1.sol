a267dc864e2b8f23e50935211c6090aac698c7a3
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-11-20 17:01:53 +0100

    Add username conditions

--- contracts/src/usernames/UsernamesV1.sol
+++ contracts/src/usernames/UsernamesV1.sol
@@ -9,8 +9,8 @@ error CallerIsOwner();
 
 contract UsernamesV1 is Initializable, UUPSUpgradeable {
     address private _owner;
-    // Modifiers
 
+    // Modifiers
     modifier onlyOwner() {
         if (msg.sender != _owner) {
             revert CallerIsNotOwner();
@@ -32,4 +32,38 @@ contract UsernamesV1 is Initializable, UUPSUpgradeable {
 
     // Overrides
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    // External functions
+
+    // RULES:
+    // minimum length of 1 character
+    // maximum length of 20 characters
+    // only lowercase letters, numbers and underscores are allowed
+    // cannot start or end with underscore
+    // cannot contain two or more consecutive underscores
+    function registerUsername(string memory username) external preventOwner {
+        // Register username
+        bytes memory b = bytes(username);
+
+        // Check username length
+        require(b.length >= 1 && b.length <= 20, "Invalid username length");
+
+        if (b[0] == 0x5F || b[b.length - 1] == 0x5F) {
+            revert("Username cannot start or end with underscore");
+        }
+
+        for (uint256 i = 0; i < b.length; i++) {
+            if (
+                !(b[i] >= 0x30 && b[i] <= 0x39) // 0-9
+                    && !(b[i] >= 0x61 && b[i] <= 0x7A) // a-z
+                    && !(b[i] == 0x5F) // _
+            ) {
+                revert("Invalid character in username");
+            }
+
+            if (b[i] == 0x5F && b[i + 1] == 0x5F) {
+                revert("Username cannot contain two or more consecutive underscores");
+            }
+        }
+    }
 }
---
+++ contracts/test/usernames/UsernamesV1.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: GNU GENERAL PUBLIC LICENSE
+pragma solidity ^0.8.13;
+
+import {Test, console} from "@forge-std/Test.sol";
+import {UsernamesV1} from "@contracts/usernames/UsernamesV1.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+
+contract UsernamesTest is Test {
+    UsernamesV1 public usernames;
+
+    function setUp() public {
+        bytes memory data = abi.encode(UsernamesV1.initialize.selector);
+        address proxy = address(new ERC1967Proxy(address(new UsernamesV1()), data));
+        usernames = UsernamesV1(proxy);
+    }
+
+    function test_register_username_should_pass() public {
+        vm.startPrank(address(1));
+        usernames.registerUsername("test");
+
+        vm.startPrank(address(2));
+        usernames.registerUsername("te_st");
+
+        vm.startPrank(address(3));
+        usernames.registerUsername("t_e_s_t");
+
+        vm.startPrank(address(4));
+        usernames.registerUsername("0123456789");
+
+        vm.startPrank(address(5));
+        usernames.registerUsername("abcdefghijeklmnopqrs");
+
+        vm.startPrank(address(6));
+        usernames.registerUsername("tuvwxyz");
+    }
+
+    function test_register_username_rever_if_owner() public {
+        vm.expectRevert();
+        usernames.registerUsername("test");
+    }
+
+    function test_register_username_rever_if_empty() public {
+        vm.startPrank(address(1));
+        vm.expectRevert();
+        usernames.registerUsername("");
+    }
+
+    function test_register_username_rever_if_greater_than_20() public {
+        vm.startPrank(address(1));
+        vm.expectRevert();
+        usernames.registerUsername("000000000000000000000"); // 20 chars
+        vm.expectRevert();
+        usernames.registerUsername("0000000000000000000000"); // 21 chars
+        vm.expectRevert();
+        usernames.registerUsername("00000000000000000000000"); // 22 chars
+    }
+
+    function test_register_username_rever_if_starts_or_end_with_underscore() public {
+        vm.startPrank(address(1));
+        vm.expectRevert();
+        usernames.registerUsername("_test");
+        vm.expectRevert();
+        usernames.registerUsername("test_");
+    }
+
+    function test_register_username_rever_if_contains_2_consecutive_underscores() public {
+        vm.startPrank(address(1));
+        vm.expectRevert();
+        usernames.registerUsername("te__st");
+    }
+
+    function test_register_username_rever_if_contains_uppercase_charactes() public {
+        string memory characters = "ABCDEDGHIJKLMNOPQRSTUVWXYZ";
+        bytes memory b = bytes(characters);
+
+        for (uint256 i = 0; i < b.length; i++) {
+            bytes memory c = new bytes(3);
+            c[0] = 0x61; // a
+            c[1] = b[i];
+            c[2] = 0x61; // a
+
+            vm.startPrank(address(uint160(i)));
+            vm.expectRevert();
+            usernames.registerUsername(string(c));
+        }
+    }
+
+    function test_register_username_rever_if_contains_special_characters() public {
+        string memory characters = "!@#$%^&*()+{}|:\"<>?`-=[]\\;',./";
+        bytes memory b = bytes(characters);
+
+        for (uint256 i = 0; i < b.length; i++) {
+            bytes memory c = new bytes(3);
+            c[0] = 0x61; // a
+            c[1] = b[i];
+            c[2] = 0x61; // a
+
+            vm.startPrank(address(uint160(i)));
+            vm.expectRevert();
+            usernames.registerUsername(string(c));
+        }
+    }
+}
