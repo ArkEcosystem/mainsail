fdb1e93da597896dd0272289b61657dcea2dbe23
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-11-21 11:44:20 +0100

    Add usernames

--- packages/evm-contracts/source/index.ts
+++ packages/evm-contracts/source/index.ts
@@ -1,4 +1,5 @@
 export { default as ConsensusAbi } from "./abis/ConsensusV1.json" with { type: "json" };
 export { default as DirectTransferAbi } from "./abis/DirectTransfer.json" with { type: "json" };
 export { default as ERC1967ProxyAbi } from "./abis/ERC1967Proxy.json" with { type: "json" };
+export { default as UsernamesAbi } from "./abis/UsernamesV1.json" with { type: "json" };
 export * from "./function-sigs.js";
