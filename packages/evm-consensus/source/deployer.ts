5b85b3f745dcbdf63cf37b8bae6ccbe6e9b246ff
sebastijankuzner <sebastijankuzner@users.noreply.github.com>
2024-11-22 07:48:11 +0000

    style: resolve style guide violations

--- packages/evm-consensus/source/deployer.ts
+++ packages/evm-consensus/source/deployer.ts
@@ -1,6 +1,6 @@
 import { inject, injectable, tagged } from "@mainsail/container";
 import { Contracts, Identifiers } from "@mainsail/contracts";
-import { ConsensusAbi, ERC1967ProxyAbi, UsernamesAbi, MultiPaymentAbi } from "@mainsail/evm-contracts";
+import { ConsensusAbi, ERC1967ProxyAbi, MultiPaymentAbi, UsernamesAbi } from "@mainsail/evm-contracts";
 import { Utils } from "@mainsail/kernel";
 import { ethers, sha256 } from "ethers";
 
