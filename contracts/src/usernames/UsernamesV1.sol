022974df1418d3d52b7fbaeaaefdb32d322ebaf9
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-11-22 10:26:15 +0100

    Switch to upgradable contracts

--- contracts/src/usernames/UsernamesV1.sol
+++ contracts/src/usernames/UsernamesV1.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GNU GENERAL PUBLIC LICENSE
 pragma solidity ^0.8.27;
 
-import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 error CallerIsNotOwner();
 error CallerIsOwner();
