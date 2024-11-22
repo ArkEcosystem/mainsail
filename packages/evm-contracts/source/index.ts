fed25de71ca5f79e0cbda02aee95fe7c83b309ee
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-11-22 11:06:58 +0100

    Remove directTransfer abi

--- packages/evm-contracts/source/index.ts
+++ packages/evm-contracts/source/index.ts
@@ -1,5 +1,4 @@
 export { default as ConsensusAbi } from "./abis/ConsensusV1.json" with { type: "json" };
-export { default as DirectTransferAbi } from "./abis/DirectTransfer.json" with { type: "json" };
 export { default as ERC1967ProxyAbi } from "./abis/ERC1967Proxy.json" with { type: "json" };
 export { default as MultiPaymentAbi } from "./abis/MultiPayment.json" with { type: "json" };
 export { default as UsernamesAbi } from "./abis/UsernamesV1.json" with { type: "json" };
