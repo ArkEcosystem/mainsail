a0cf029b5e11bed473087290f84bc7f32950aa1e
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-11-28 08:55:54 +0100

    Add eth_getBlockByHash

---
+++ packages/api-evm/source/actions/eth-get-block-by-hash.ts
@@ -0,0 +1,41 @@
+import { inject, injectable } from "@mainsail/container";
+import { Contracts, Identifiers } from "@mainsail/contracts";
+
+import { BlockResource } from "../resources/index.js";
+
+@injectable()
+export class EthGetBlockByHashAction implements Contracts.Api.RPC.Action {
+	public readonly name: string = "eth_getBlockByHash";
+
+	@inject(Identifiers.Application.Instance)
+	private readonly app!: Contracts.Kernel.Application;
+
+	@inject(Identifiers.State.Store)
+	private readonly stateStore!: Contracts.State.Store;
+
+	@inject(Identifiers.Database.Service)
+	private readonly databaseService!: Contracts.Database.DatabaseService;
+
+	public readonly schema = {
+		$id: `jsonRpc_${this.name}`,
+
+		maxItems: 2,
+		minItems: 2,
+
+		prefixItems: [{ $ref: "prefixedHex" }, { type: "boolean" }],
+		type: "array",
+	};
+
+	public async handle(parameters: [string, boolean]): Promise<object | null> {
+		const transactionObject = parameters[1];
+
+		const commit = await this.databaseService.getCommitById(parameters[0].slice(2));
+
+		if (!commit) {
+			// eslint-disable-next-line unicorn/no-null
+			return null;
+		}
+
+		return this.app.resolve(BlockResource).transform(commit.block, transactionObject);
+	}
+}
