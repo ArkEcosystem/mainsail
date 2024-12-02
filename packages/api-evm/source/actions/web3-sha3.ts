993794b2632abceefce70da5f61595b374239bbe
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-12-02 10:58:58 +0100

    Add web3_sha3

---
+++ packages/api-evm/source/actions/web3-sha3.ts
@@ -0,0 +1,17 @@
+import { injectable } from "@mainsail/container";
+import { Contracts } from "@mainsail/contracts";
+
+@injectable()
+export class Web3Sha3 implements Contracts.Api.RPC.Action {
+	public readonly name: string = "web3_sha3";
+
+	public readonly schema = {
+		$id: `jsonRpc_${this.name}`,
+		maxItems: 0,
+		type: "array",
+	};
+
+	public async handle(parameters: []): Promise<string> {
+		return `0x${0}`;
+	}
+}
