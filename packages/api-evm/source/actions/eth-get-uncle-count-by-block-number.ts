719b592393fd010854552d6b6154d1becdb905de
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-12-02 09:42:36 +0100

    Add eth_getUncleCountByBlockNumber

---
+++ packages/api-evm/source/actions/eth-get-uncle-count-by-block-number.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from "@mainsail/container";
+import { Contracts, Exceptions, Identifiers } from "@mainsail/contracts";
+
+@injectable()
+export class EthGetUncleCountByBlockNumber implements Contracts.Api.RPC.Action {
+	@inject(Identifiers.State.Store)
+	private readonly stateStore!: Contracts.State.Store;
+
+	public readonly name: string = "eth_getUncleCountByBlockNumber";
+
+	public readonly schema = {
+		$id: `jsonRpc_${this.name}`,
+		maxItems: 1,
+		minItems: 1,
+
+		prefixItems: [{ $ref: "prefixedHex" }],
+		type: "array",
+	};
+
+	public async handle(parameters: [string]): Promise<string> {
+		if (this.stateStore.getHeight() < Number(parameters[0])) {
+			throw new Exceptions.RpcError("Block not found");
+		}
+
+		return `0x0`;
+	}
+}
