d94795f88eac797c6d79faeda0de94f25cb316c2
sebastijankuzner <sebastijan.kuzner@outlook.com>
2024-12-02 10:30:44 +0100

    Add eth_getUncleByBlockNumberAndIndex

---
+++ packages/api-evm/source/actions/eth_get-uncle-by-block-number-and-index.ts
@@ -0,0 +1,28 @@
+import { inject, injectable } from "@mainsail/container";
+import { Contracts, Exceptions, Identifiers } from "@mainsail/contracts";
+
+@injectable()
+export class EthGetUncleByBlockNumberAndIndex implements Contracts.Api.RPC.Action {
+	@inject(Identifiers.State.Store)
+	private readonly stateStore!: Contracts.State.Store;
+
+	public readonly name: string = "eth_getUncleByBlockNumberAndIndex";
+
+	public readonly schema = {
+		$id: `jsonRpc_${this.name}`,
+		maxItems: 2,
+		minItems: 2,
+
+		prefixItems: [{ $ref: "prefixedHex" }, { $ref: "prefixedHex" }],
+		type: "array",
+	};
+
+	public async handle(parameters: [string]): Promise<null> {
+		if (this.stateStore.getHeight() < Number(parameters[0])) {
+			throw new Exceptions.RpcError("Block not found");
+		}
+
+		// eslint-disable-next-line unicorn/no-null
+		return null;
+	}
+}
