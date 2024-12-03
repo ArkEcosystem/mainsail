63bd7bc2f89c56aae7b5ce59c3d57ce42d701e36
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-12-03 16:57:10 +0900

    set temporary path

--- packages/configuration-generator/bin/create-genesis-block.js
+++ packages/configuration-generator/bin/create-genesis-block.js
@@ -16,6 +16,10 @@ async function run() {
 		symbol: "TѦ",
 		token: "ARK",
 		distribute: true,
+		premine: "0",
+		snapshot: {
+			path: "../../snapshot-19a87c96dbe8ad1be06d33e97cd17f5662eb952c29efd3d8bb00c9c75e7582bc.json",
+		}
 	});
 }
 
