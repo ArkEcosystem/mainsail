bba3ebe29f4ee367faaf25ae9ffb9afa8d3935df
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-12-16 11:25:17 +0900

    revert genesis block generator defaults

--- packages/configuration-generator/source/configuration-generator.ts
+++ packages/configuration-generator/source/configuration-generator.ts
@@ -103,7 +103,7 @@ export class ConfigurationGenerator {
 			{
 				task: async () => {
 					if (!internalOptions.overwriteConfig && pathExistsSync(this.configurationPath)) {
-						//	throw new Error(`${this.configurationPath} already exists.`);
+						throw new Error(`${this.configurationPath} already exists.`);
 					}
 
 					ensureDirSync(this.configurationPath);
