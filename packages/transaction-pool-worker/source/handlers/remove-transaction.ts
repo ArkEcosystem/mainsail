872a1220089cec0263876396b1a0cec456e103d2
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2024-12-20 10:44:05 +0900

    also remove from storage

--- packages/transaction-pool-worker/source/handlers/remove-transaction.ts
+++ packages/transaction-pool-worker/source/handlers/remove-transaction.ts
@@ -6,7 +6,11 @@ export class RemoveTransactionHandler {
 	@inject(Identifiers.TransactionPool.Mempool)
 	private readonly mempool!: Contracts.TransactionPool.Mempool;
 
+	@inject(Identifiers.TransactionPool.Storage)
+	private readonly storage!: Contracts.TransactionPool.Storage;
+
 	public async handle(address: string, id: string): Promise<void> {
 		await this.mempool.removeTransaction(address, id);
+		this.storage.removeTransaction(id);
 	}
 }
