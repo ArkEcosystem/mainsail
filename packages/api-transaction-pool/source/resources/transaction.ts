88bbfc94bc2d5339603ce6a88355cb20dc38e08d
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-01-08 13:41:07 +0900

    remove obsolete tx pool api routes

--- packages/api-transaction-pool/source/resources/transaction.ts
+++ packages/api-transaction-pool/source/resources/transaction.ts
@@ -8,28 +8,6 @@ export class TransactionResource implements Contracts.Api.Resource {
 	}
 
 	public async transform(resource: Contracts.Crypto.TransactionData): Promise<object> {
-		//AppUtils.assert.defined<string>(resource.senderPublicKey);
-
-		// const wallet = await this.stateService.getStore().walletRepository.findByPublicKey(resource.senderPublicKey);
-		// const sender: string = wallet.getAddress();
-
-		return {
-			// amount: resource.amount.toFixed(),
-			// asset: resource.asset,
-			// blockId: resource.blockId,
-			// fee: resource.fee.toFixed(),
-			// id: resource.id,
-			// nonce: resource.nonce?.toFixed(),
-			// recipient: resource.recipientId,
-			// // sender,
-			// // senderPublicKey: resource.senderPublicKey,
-			// signature: resource.signature,
-			// signatures: resource.signatures,
-			// timestamp: resource.timestamp,
-			// type: resource.type,
-			// typeGroup: resource.typeGroup,
-			// vendorField: resource.vendorField,
-			// version: resource.version,
-		};
+		return this.raw(resource);
 	}
 }
