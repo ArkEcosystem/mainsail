189a502e38db20a4b4a99843e18c0797c8ff27ab
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-01-13 12:51:34 +0100

    Get transaction

--- packages/api-evm/source/actions/eth-get-transaction-receipt.ts
+++ packages/api-evm/source/actions/eth-get-transaction-receipt.ts
@@ -1,9 +1,16 @@
-import { injectable } from "@mainsail/container";
-import { Contracts } from "@mainsail/contracts";
+import { inject, injectable } from "@mainsail/container";
+import { Contracts, Identifiers } from "@mainsail/contracts";
 
 @injectable()
 export class EthGetTransactionReceipt implements Contracts.Api.RPC.Action {
 	public readonly name: string = "eth_getTransactionReceipt";
+
+	@inject(Identifiers.Database.Service)
+	private readonly databaseService!: Contracts.Database.DatabaseService;
+
+	// @inject(Identifiers.Evm.Instance)
+	// @tagged("instance", "evm") // TODO: Check ephemeral
+	// private readonly evm!: Contracts.Evm.Instance;
 
 	public readonly schema = {
 		$id: `jsonRpc_${this.name}`,
@@ -18,6 +25,12 @@ export class EthGetTransactionReceipt implements Contracts.Api.RPC.Action {
 	};
 
 	public async handle(parameters: [string, boolean]): Promise<object | null> {
+		const transaction = await this.databaseService.getTransactionById(parameters[0].slice(2));
+
+		if (!transaction) {
+			return null;
+		}
+
 		return null;
 	}
 }
