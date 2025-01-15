a3f260c0eb5e8b31f07fddf087127793f34768ac
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-01-15 11:13:00 +0100

    Sort

--- packages/api-evm/source/resources/receipt.ts
+++ packages/api-evm/source/resources/receipt.ts
@@ -9,21 +9,20 @@ export class ReceiptResource {
 	): Promise<object> {
 		/* eslint-disable sort-keys-fix/sort-keys-fix */
 		return {
-			blockHash: transaction.blockId,
-			blockNumber: `0x${transaction.blockHeight?.toString(16)}`,
-			contractAddress: receipt.deployedContractAddress,
-			cumulativeGasUsed: "", // The sum of the base fee and tip paid per unit of gas.
-			effectiveGasUsed: "", // The total amount of gas used when this transaction was executed in the block.
-
-			from: transaction.senderAddress,
-			gasUsed: `0x${receipt.gasUsed.toString(16)}`,
-			logs: receipt.logs,
-			logsBloom: "",
-			status: receipt.success ? "0x1" : "0x0",
-			to: transaction.recipientAddress,
 			transactionHash: transaction.id,
 			transactionIndex: `0x${transaction.sequence?.toString(16)}`,
+			blockHash: transaction.blockId,
+			blockNumber: `0x${transaction.blockHeight?.toString(16)}`,
+			from: transaction.senderAddress,
+			to: transaction.recipientAddress,
+			cumulativeGasUsed: "", // The sum of the base fee and tip paid per unit of gas.
+			effectiveGasUsed: "", // The total amount of gas used when this transaction was executed in the block.
+			gasUsed: `0x${receipt.gasUsed.toString(16)}`,
+			contractAddress: receipt.deployedContractAddress,
+			logs: receipt.logs,
+			logsBloom: "",
 			type: "0x2",
+			status: receipt.success ? "0x1" : "0x0",
 		};
 		/* eslint-enable sort-keys-fix/sort-keys-fix */
 	}
