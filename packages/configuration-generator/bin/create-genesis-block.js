261c79591a316cbc7e6fd43db5d8e1c571308cfc
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-01-15 13:45:05 +0100

    Fix typo

--- packages/configuration-generator/bin/create-genesis-block.js
+++ packages/configuration-generator/bin/create-genesis-block.js
@@ -23,7 +23,7 @@ async function run() {
 		// },
 	});
 
-	for (const tag of ["evm", "valdiator", "transaction-pool"]) {
+	for (const tag of ["evm", "validator", "transaction-pool"]) {
 		if (app.isBoundTagged(AppIdentifiers.Evm.Instance, "instance", tag)) {
 			await app.getTagged(AppIdentifiers.Evm.Instance, "instance", tag).dispose();
 		}
