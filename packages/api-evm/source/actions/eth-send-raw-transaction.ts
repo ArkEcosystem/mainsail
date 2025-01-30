b84a6fb6236bece96a6d4a056bfd1e12847b7bbc
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-01-30 18:17:21 +0100

    Implement action

--- packages/api-evm/source/actions/eth-send-raw-transaction.ts
+++ packages/api-evm/source/actions/eth-send-raw-transaction.ts
@@ -1,10 +1,11 @@
 import { inject, injectable } from "@mainsail/container";
-import { Contracts, Identifiers } from "@mainsail/contracts";
+import { Contracts, Exceptions, Identifiers } from "@mainsail/contracts";
+import { http } from "@mainsail/utils";
 
 @injectable()
 export class EthSendRawTransactionAction implements Contracts.Api.RPC.Action {
-	@inject(Identifiers.State.Store)
-	private readonly stateStore!: Contracts.State.Store;
+	@inject(Identifiers.Cryptography.Transaction.Factory)
+	private readonly transactionFactory!: Contracts.Crypto.TransactionFactory;
 
 	public readonly name: string = "eth_sendRawTransaction";
 
@@ -17,7 +18,21 @@ export class EthSendRawTransactionAction implements Contracts.Api.RPC.Action {
 		type: "array",
 	};
 
-	public async handle(parameters: []): Promise<string> {
-		return `0x${this.stateStore.getHeight().toString(16)}`;
+	public async handle(parameters: [string]): Promise<string> {
+		const response = await http.post("http://localhost:4007/api/transactions", {
+			body: { transactions: [parameters[0].slice(2)] },
+		});
+
+		if (response.statusCode === 200) {
+			if (response.data.data.accept.length > 0) {
+				const tx = await this.transactionFactory.fromHex(parameters[0].slice(2));
+				return `0x${tx.id}`;
+			} else {
+				throw new Exceptions.RpcError(response.data.errors[0].message);
+			}
+		}
+
+		// TODO Improve error handling
+		throw new Exceptions.RpcError("Error sending transaction");
 	}
 }
