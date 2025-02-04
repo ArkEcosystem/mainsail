28d7ded7bab74493e94a1a936dc5058831193ea6
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-02-04 02:41:23 +0000

    style: resolve style guide violations

--- packages/test-transaction-builders/source/evm-calls.ts
+++ packages/test-transaction-builders/source/evm-calls.ts
@@ -2,10 +2,9 @@ import { Contracts, Identifiers } from "@mainsail/contracts";
 import { EvmCallBuilder } from "@mainsail/crypto-transaction-evm-call";
 import { BigNumberish, ethers } from "ethers";
 
+import { default as DARK20 } from "./abis/DARK20.json" assert { type: "json" };
 import { Context, EvmCallOptions } from "./types.js";
 import { buildSignedTransaction, getAddressByPublicKey } from "./utils.js";
-
-import { default as DARK20 } from "./abis/DARK20.json" assert { type: "json" };
 
 export const makeEvmCall = async (
 	{ sandbox, wallets }: Context,
