4b5eb89cd7c1776337fe37f2c6f90c6b5df24602
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-02-25 16:51:58 +0100

    Remove FeeMatcher

--- packages/contracts/source/contracts/transaction-pool/dynamic-fee-matcher.ts
+++
@@ -1,6 +0,0 @@
-import { Transaction } from "../crypto/transactions.js";
-
-export interface FeeMatcher {
-	throwIfCannotEnterPool(transaction: Transaction): Promise<void>;
-	throwIfCannotBroadcast(transaction: Transaction): Promise<void>;
-}
