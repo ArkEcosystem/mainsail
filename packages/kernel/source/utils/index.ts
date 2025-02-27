6ea561ea7437a3dfbc46c80f2b56bb6859dd54c1
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-02-27 18:05:32 +0100

    Fix index

--- packages/kernel/source/utils/index.ts
+++ packages/kernel/source/utils/index.ts
@@ -1,20 +1,6 @@
-import { calculateApproval } from "./calculate-forging-info.js";
-import { calculateRound, calculateRoundInfoByRound, isNewRound } from "./round-calculator.js";
-import { calculateSupply } from "./supply-calculator.js";
-import { calculateMinimalTimestamp } from "./timestamp-calculator.js";
 export * from "./assert.js";
 export * as IpAddress from "./ip-address.js";
+export { isBlacklisted } from "./is-blacklisted.js";
+export { isWhitelisted } from "./is-whitelisted.js";
 export * from "./lock.js";
 export * from "@mainsail/utils";
-
-export const roundCalculator = { calculateRound, calculateRoundInfoByRound, isNewRound };
-export const supplyCalculator = { calculateSupply };
-export const validatorCalculator = { calculateApproval };
-export const timestampCalculator = { calculateMinimalTimestamp };
-
-export { formatCurrency } from "./format-currency.js";
-export { isBlacklisted } from "./is-blacklisted.js";
-export { getBlockNotChainedErrorMessage, isBlockChained } from "./is-block-chained.js";
-export { isMajority } from "./is-majority.js";
-export { isMinority } from "./is-minority.js";
-export { isWhitelisted } from "./is-whitelisted.js";
