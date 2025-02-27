9a2ff942b20cd3ec824148a4d520a4876460cdbf
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-02-27 15:23:58 +0100

    Set gas used

--- packages/validator/source/validator.ts
+++ packages/validator/source/validator.ts
@@ -235,16 +235,15 @@ export class Validator implements Contracts.Validator.Validator {
 		transactions: Contracts.Crypto.Transaction[],
 		timestamp: number,
 	): Promise<Contracts.Crypto.Block> {
+		const previousBlock = this.stateStore.getLastBlock();
+		const height = previousBlock.header.height + 1;
+		const milestone = this.cryptoConfiguration.getMilestone(height);
+
 		const totals: { amount: BigNumber; fee: BigNumber; gasUsed: number } = {
 			amount: BigNumber.ZERO,
 			fee: BigNumber.ZERO,
 			gasUsed: 0,
 		};
-
-		const previousBlock = this.stateStore.getLastBlock();
-		const height = previousBlock.header.height + 1;
-		const milestone = this.cryptoConfiguration.getMilestone(height);
-
 		const payloadBuffers: Buffer[] = [];
 		const transactionData: Contracts.Crypto.TransactionData[] = [];
 
@@ -258,7 +257,8 @@ export class Validator implements Contracts.Validator.Validator {
 			Utils.assert.defined<number>(data.gasUsed);
 
 			totals.amount = totals.amount.plus(data.value);
-			totals.fee = totals.fee.plus(data.gasPrice);
+			Utils.assert.defined<number>(data.gasUsed);
+			totals.fee = totals.fee.plus(data.gasUsed);
 			totals.gasUsed += data.gasUsed;
 
 			payloadBuffers.push(Buffer.from(data.id, "hex"));
