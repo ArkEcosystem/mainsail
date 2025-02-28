95923075166efc6227e6674fae001bf183530ae5
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-02-28 14:21:07 +0100

    Cleanup

--- packages/kernel/source/utils/assert.ts
+++ packages/kernel/source/utils/assert.ts
@@ -1,5 +1,3 @@
-// import { IBlock, ITransaction } from "@mainsail/contracts";
-
 import { Exceptions } from "@mainsail/contracts";
 
 const assertType = (condition: boolean, description: string): asserts condition => {
@@ -8,27 +6,9 @@ const assertType = (condition: boolean, description: string): asserts condition 
 	}
 };
 
-interface Assert {
-	boolean: (value: unknown) => asserts value is boolean;
-	buffer: (value: unknown) => asserts value is Buffer;
-	number: (value: unknown) => asserts value is number;
-	object: (value: unknown) => asserts value is Record<string, any>;
-	string: (value: unknown) => asserts value is string;
-	symbol: (value: unknown) => asserts value is symbol;
-	undefined: (value: unknown) => asserts value is undefined;
-
-	array: <T>(value: unknown) => asserts value is Array<T>;
-	bigint: (value: unknown) => asserts value is bigint;
-	// block(value: unknown): asserts value is IBlock;
-	defined<T>(value: unknown): asserts value is NonNullable<T>;
-	// transaction(value: unknown): asserts value is ITransaction;
-}
-
-export const assert: Assert = {
+export const assert = {
 	array: <T>(value: unknown): asserts value is Array<T> => assertType(Array.isArray(value), "array"),
 	bigint: (value: unknown): asserts value is bigint => assertType(typeof value === "bigint", "bigint"),
-	// block: (value: unknown): asserts value is IBlock =>
-	// 	assertType(value instanceof IBlock, "Crypto.Blocks.Block"),
 	boolean: (value: unknown): asserts value is boolean => assertType(typeof value === "boolean", "boolean"),
 	buffer: (value: unknown): asserts value is Buffer => assertType(value instanceof Buffer, "buffer"),
 	defined: <T>(value: unknown): asserts value is NonNullable<T> =>
@@ -37,7 +17,5 @@ export const assert: Assert = {
 	object: (value: unknown): asserts value is Record<string, any> => assertType(typeof value === "object", "object"),
 	string: (value: unknown): asserts value is string => assertType(typeof value === "string", "string"),
 	symbol: (value: unknown): asserts value is symbol => assertType(typeof value === "symbol", "symbol"),
-	// transaction: (value: unknown): asserts value is ITransaction =>
-	// 	assertType(value instanceof ITransaction, "Crypto.Transactions.Transaction"),
 	undefined: (value: unknown): asserts value is undefined => assertType(value === undefined, "undefined"),
 };
