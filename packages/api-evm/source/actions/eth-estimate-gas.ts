b243ed98eb4ae2eece4b8d7378a86cff93d2f4ea
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-03-13 15:47:14 +0900

    improve gas estimation using binary search

--- packages/api-evm/source/actions/eth-estimate-gas.ts
+++ packages/api-evm/source/actions/eth-estimate-gas.ts
@@ -1,5 +1,6 @@
 import { inject, injectable, tagged } from "@mainsail/container";
 import { Contracts, Exceptions, Identifiers } from "@mainsail/contracts";
+import { assert } from "@mainsail/utils";
 import dayjs from "dayjs";
 
 type TxData = {
@@ -47,48 +48,131 @@ export class EthEstimateGasAction implements Contracts.Api.RPC.Action {
 		type: "array",
 	};
 
+	// Loosely based on https://github.com/ethereum/go-ethereum/blob/5606cbc710ffcf327740e4db54776eb8a3c1a2fc/eth/gasestimator/gasestimator.go#L54
 	public async handle(parameters: [TxData]): Promise<any> {
-		try {
-			const [data] = parameters;
+		const defaultGas = 21000;
 
-			const { evmSpec, block, gas } = this.configuration.getMilestone();
+		const [data] = parameters;
 
-			const accountInfo = await this.evm.getAccountInfo(data.from);
+		const { evmSpec, block, gas } = this.configuration.getMilestone();
 
-			const commitKey = { height: BigInt(this.configuration.getHeight()), round: BigInt(0) };
+		const accountInfo = await this.evm.getAccountInfo(data.from);
 
-			const dataToProcess = {
-				blockContext: {
-					commitKey,
-					gasLimit: BigInt(block.maxGasLimit),
-					timestamp: BigInt(dayjs().valueOf()),
-					validatorAddress: "0x0000000000000000000000000000000000000001",
-				},
-				caller: data.from,
-				data: data.data ? Buffer.from(data.data.slice(2), "hex") : Buffer.alloc(0),
-				gasLimit: data.gas ? BigInt(data.gas) : BigInt(block.maxGasLimit),
-				gasPrice: data.gasPrice ? BigInt(data.gasPrice) : BigInt(gas.minimumGasPrice),
-				nonce: accountInfo.nonce,
-				recipient: data.to,
-				specId: evmSpec,
-				txHash: "0".repeat(64),
-				value: data.value ? BigInt(data.value) : BigInt(0),
-			};
-
-			await this.evm.prepareNextCommit({
-				commitKey,
-			});
-
-			const { receipt } = await this.evm.process(dataToProcess);
-			const { success, gasUsed } = receipt;
-
-			if (success) {
-				return `0x${gasUsed.toString(16)}`;
+		// Skip estimation if it's a vanilla transfer and recipient is not a contract
+		if (!data.data || data.data === "0x") {
+			const targetCode = await this.evm.codeAt(data.to);
+			if (targetCode === "0x") {
+				return `0x${defaultGas.toString(16)}`;
 			}
-		} catch (error) {
-			throw new Exceptions.RpcError(`execution reverted: ${error.message}`);
 		}
 
-		throw new Exceptions.RpcError("execution reverted");
+		const commitKey = { height: BigInt(this.configuration.getHeight()), round: BigInt(0) };
+
+		let maxGasLimit = data.gas ? BigInt(data.gas) : BigInt(block.maxGasLimit);
+		let minGasLimit = maxGasLimit;
+
+		const context = {
+			blockContext: {
+				commitKey,
+				gasLimit: BigInt(block.maxGasLimit),
+				timestamp: BigInt(dayjs().valueOf()),
+				validatorAddress: "0x0000000000000000000000000000000000000001",
+			},
+			caller: data.from,
+			data: data.data ? Buffer.from(data.data.slice(2), "hex") : Buffer.alloc(0),
+			gasLimit: maxGasLimit,
+			gasPrice: data.gasPrice ? BigInt(data.gasPrice) : BigInt(gas.minimumGasPrice),
+			nonce: accountInfo.nonce,
+			recipient: data.to,
+			specId: evmSpec,
+			txHash: "0".repeat(64),
+			value: data.value ? BigInt(data.value) : BigInt(0),
+		};
+
+		// Execute with max allowed gas limit
+		let { success, receipt, executionError } = await this.#execute(context);
+		if (executionError) {
+			throw new Exceptions.RpcError(`execution reverted: ${executionError}`);
+		}
+
+		// First execution failed, so no point in trying further
+		if (!success) {
+			throw new Exceptions.RpcError("execution reverted");
+		}
+
+		assert.defined(receipt);
+
+		// For almost any transaction, the gas consumed by the unconstrained execution
+		// above lower-bounds the gas limit required for it to succeed
+		minGasLimit = receipt.gasUsed - 1n;
+
+		// There's a fairly high chance for the transaction to execute successfully
+		// with gasLimit set to the first execution's usedGas + gasRefund. Explicitly
+		// check that gas amount and use as a limit for the binary search.
+		const optimisticGasLimit = (receipt.gasUsed + receipt.gasRefunded + 2300n) * (64n / 63n);
+		if (optimisticGasLimit < maxGasLimit) {
+			let { success, executionError } = await this.#execute({ ...context, gasLimit: optimisticGasLimit });
+			if (executionError) {
+				// This should not happen under normal conditions since if we make it this far the
+				// transaction had run without error at least once before.
+				throw new Exceptions.RpcError(`execution reverted: ${executionError}`);
+			}
+
+			if (success) {
+				maxGasLimit = optimisticGasLimit;
+			} else {
+				minGasLimit = optimisticGasLimit;
+			}
+		}
+
+		// Try to find the smallest gas limit using binary search.
+		while (minGasLimit + 1n < maxGasLimit) {
+			// Since it's a best effort we don't care about making a perfect estimation and instead bail out
+			// early once close enough.
+			const errorRatio = 0.015;
+			if (Number(maxGasLimit - minGasLimit) / Number(maxGasLimit) < errorRatio) {
+				break;
+			}
+
+			let midGasLimit = (maxGasLimit + minGasLimit) / 2n;
+			if (midGasLimit > minGasLimit * 2n) {
+				// Most txs don't need much higher gas limit than their gas used, and most txs don't
+				// require near the full block limit of gas, so the selection of where to bisect the
+				// range here is skewed to favor the low side.
+				midGasLimit = minGasLimit * 2n;
+			}
+
+			({ success, executionError } = await this.#execute({ ...context, gasLimit: midGasLimit }));
+			if (executionError) {
+				throw new Exceptions.RpcError(`execution reverted: ${executionError}`);
+			}
+
+			if (success) {
+				maxGasLimit = midGasLimit;
+			} else {
+				minGasLimit = midGasLimit;
+			}
+		}
+
+		return `0x${maxGasLimit.toString(16)}`;
 	}
+
+	async #execute(context: Contracts.Evm.TransactionContext): Promise<EstimateOutcome> {
+		await this.evm.prepareNextCommit({
+			commitKey: context.blockContext.commitKey,
+		});
+
+		try {
+			const { receipt } = await this.evm.process(context);
+			return { success: receipt.success, receipt };
+		} catch (error) {
+			return { success: false, executionError: error.message };
+		}
+	}
+}
+
+interface EstimateOutcome {
+	receipt?: Contracts.Evm.TransactionReceipt;
+	success: boolean;
+	executionError?: string;
 }
