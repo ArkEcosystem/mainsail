516e551f5c2d5b3476eefc1e7cfb4d209181dcf1
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-04-16 13:09:48 +0900

    use `@multiInject` for block verifier

--- packages/processor/source/index.ts
+++ packages/processor/source/index.ts
@@ -5,10 +5,30 @@ import { ProcessBlockAction } from "./actions/process-block.js";
 import { BlockProcessor } from "./block-processor.js";
 import { BlockVerifier } from "./block-verifier.js";
 import { TransactionProcessor } from "./transaction-processor.js";
+import { ChainedVerifier } from "./verifiers/chained-verifier.js";
+import { TimestampVerifier } from "./verifiers/timestamp-verifier.js";
+import { GeneratorVerifier } from "./verifiers/generator-verifier.js";
+import { VersionVerifier } from "./verifiers/version-verifier.js";
+import { RewardVerifier } from "./verifiers/reward-verifier.js";
+import { TransactionLengthVerifier } from "./verifiers/transaction-length-verifier.js";
+import { GasLimitVerifier } from "./verifiers/gas-limit-verifier.js";
 
 export class ServiceProvider extends Providers.ServiceProvider {
 	public async register(): Promise<void> {
 		this.app.bind(Identifiers.Processor.BlockVerifier).to(BlockVerifier).inSingletonScope();
+
+		for (const handler of [
+			ChainedVerifier,
+			TimestampVerifier,
+			GeneratorVerifier,
+			VersionVerifier,
+			RewardVerifier,
+			TransactionLengthVerifier,
+			GasLimitVerifier,
+		]) {
+			this.app.bind(Identifiers.Processor.BlockVerifierHandlers).to(handler);
+		}
+
 		this.app.bind(Identifiers.Processor.BlockProcessor).to(BlockProcessor).inSingletonScope();
 		this.app.bind(Identifiers.Processor.TransactionProcessor).to(TransactionProcessor).inSingletonScope();
 
