044875b64675162b74d0e6d20d48a6615037357e
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-05-07 11:15:30 +0200

    Simplify logic

--- packages/blockchain-utils/source/proposer-calculator.ts
+++ packages/blockchain-utils/source/proposer-calculator.ts
@@ -1,6 +1,5 @@
 import { inject, injectable } from "@mainsail/container";
 import { Contracts, Identifiers } from "@mainsail/contracts";
-import { assert } from "@mainsail/utils";
 
 @injectable()
 export class ProposerCalculator implements Contracts.BlockchainUtils.ProposerCalculator {
@@ -10,15 +9,9 @@ export class ProposerCalculator implements Contracts.BlockchainUtils.ProposerCal
 	@inject(Identifiers.State.Store)
 	private readonly stateStore!: Contracts.State.Store;
 
-	// TODO: Support validator changes
-	private validatorMatrix: number[] = [...Array.from({ length: 53 }).keys()];
-
 	public getValidatorIndex(round: number): number {
 		const { activeValidators } = this.configuration.getMilestone();
 
-		const offset = (this.stateStore.getTotalRound() + round) % activeValidators;
-		const result = this.validatorMatrix[offset % activeValidators];
-		assert.number(result);
-		return result;
+		return (this.stateStore.getTotalRound() + round) % activeValidators;
 	}
 }
