c47cbb632ba62a21ec41b50345f6a7ada522cafd
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-05-13 16:05:59 +0200

    Lint fix

--- packages/container/source/decorators.ts
+++ packages/container/source/decorators.ts
@@ -3,6 +3,7 @@ import "reflect-metadata";
 import { injectable as importedInjectable, injectFromBase } from "inversify";
 
 export function injectable(): ClassDecorator {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 	return function (target: Function): void {
 		importedInjectable()(target);
 		const basePrototype = Object.getPrototypeOf(target.prototype);
