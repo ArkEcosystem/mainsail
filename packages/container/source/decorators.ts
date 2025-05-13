75c92652fe1d832d440c94af6fb8e4ac7b915f60
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-05-13 15:48:33 +0200

    Create custom decorator

---
+++ packages/container/source/decorators.ts
@@ -0,0 +1,20 @@
+import "reflect-metadata";
+
+import { injectable as importedInjectable, injectFromBase } from "inversify";
+
+export function injectable(): ClassDecorator {
+	return function (target: Function): void {
+		importedInjectable()(target);
+		const basePrototype = Object.getPrototypeOf(target.prototype);
+		const hasBaseClass = basePrototype && basePrototype !== Object.prototype;
+
+		if (
+			hasBaseClass &&
+			Reflect.getMetadataKeys(basePrototype.constructor).includes(
+				"@inversifyjs/core/classIsInjectableFlagReflectKey",
+			)
+		) {
+			injectFromBase()(target);
+		}
+	};
+}
--- packages/container/source/index.ts
+++ packages/container/source/index.ts
@@ -1,2 +1,3 @@
 export * from "./container.js";
+export * from "./decorators.js";
 export * from "./ioc.js";
