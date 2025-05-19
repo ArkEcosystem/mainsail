45192a0beee1d8766db85ed999c7c320b5a32761
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-05-19 13:11:20 +0900

    fix with latest foundry

--- contracts/test/consensus/Consensus-Internal.sol
+++ contracts/test/consensus/Consensus-Internal.sol
@@ -5,18 +5,34 @@ import {Test, console} from "@forge-std/Test.sol";
 import {ConsensusV1} from "@contracts/consensus/ConsensusV1.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
-contract ConsensusInternalTest is Test, ConsensusV1 {
+contract ConsensusInternalWrapper is ConsensusV1 {
+    function clamp(uint256 value, uint256 min, uint256 max) external pure returns (uint256) {
+        return _clamp(value, min, max);
+    }
+
+    function shuffleMem(address[] memory array) external view {
+        return _shuffleMem(array);
+    }
+}
+
+contract ConsensusInternalTest is Test {
+    ConsensusInternalWrapper consensus;
+
+    function setUp() public {
+        consensus = new ConsensusInternalWrapper();
+    }
+
     function test_clamp() public {
-        assertEq(_clamp(0, 0, 0), 0);
-        assertEq(_clamp(0, 5, 10), 5);
-        assertEq(_clamp(10, 0, 5), 5);
+        assertEq(consensus.clamp(0, 0, 0), 0);
+        assertEq(consensus.clamp(0, 5, 10), 5);
+        assertEq(consensus.clamp(10, 0, 5), 5);
 
         vm.expectRevert(abi.encodeWithSelector(ConsensusV1.InvalidRange.selector, 5, 0));
-        _clamp(0, 5, 0);
+        consensus.clamp(0, 5, 0);
     }
 
     function test_shuffleMemEmpty() public view {
         address[] memory addresses = new address[](0);
-        _shuffleMem(addresses);
+        consensus.shuffleMem(addresses);
     }
 }
