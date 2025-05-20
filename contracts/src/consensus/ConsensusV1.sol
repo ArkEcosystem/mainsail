2e7e8d800099159ac0e4a20ac7859d617d3d187e
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-05-20 22:57:57 +0200

    Remove unnecessary check

--- contracts/src/consensus/ConsensusV1.sol
+++ contracts/src/consensus/ConsensusV1.sol
@@ -376,10 +376,6 @@ contract ConsensusV1 is UUPSUpgradeable, OwnableUpgradeable {
     }
 
     function isValidatorRegistered(address addr) public view returns (bool) {
-        if (addr == address(0)) {
-            return false;
-        }
-
         return _hasValidator[addr];
     }
 
