02a7029432e78bc9bf2741a7de888b89f9e0e4e4
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-05-23 16:13:01 +0200

    Simplify regex

--- packages/utils/source/words.ts
+++ packages/utils/source/words.ts
@@ -1,4 +1,1 @@
-export const words = (value: string): string[] | null =>
-	value.match(
-		/[A-Z\u00C0-\u00D6\u00D8-\u00DE]?[a-z\u00DF-\u00F6\u00F8-\u00FF]+|[A-Z\u00C0-\u00D6\u00D8-\u00DE]+(?![a-z\u00DF-\u00F6\u00F8-\u00FF])|\d+/g,
-	);
+export const words = (value: string): string[] | null => value.match(/\p{Lu}+(?!\p{Ll})|\p{Lu}?\p{Ll}+|\d+/gu);
