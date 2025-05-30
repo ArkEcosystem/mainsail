44b4f45a865749bba763a32f5246ecdd50abceaf
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-05-30 13:18:55 +0900

    use address instead of publickey

--- tests/functional/consensus/source/utilities.ts
+++ tests/functional/consensus/source/utilities.ts
@@ -66,7 +66,7 @@ export const makeProposal = async (
 
 	await sleep(1); // Sleep to avoid same timestamp
 
-	const block = await proposer.prepareBlock(validator.publicKey, round, timestamp);
+	const block = await proposer.prepareBlock(validator.address, round, timestamp);
 	const proposal = await proposer.propose(
 		node.app
 			.get<Contracts.ValidatorSet.Service>(Identifiers.ValidatorSet.Service)
