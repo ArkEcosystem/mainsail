71e40145293804ba0f16b44b8e1344b65949b9e2
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-06-05 20:34:25 +0200

    Check refund

--- contracts/test/consensus/Consensus-ValidatorResignation.sol
+++ contracts/test/consensus/Consensus-ValidatorResignation.sol
@@ -5,6 +5,16 @@ import {ConsensusV1} from "@contracts/consensus/ConsensusV1.sol";
 import {Base} from "./Base.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
+contract RejectingRefund {
+    receive() external payable {
+        revert("I reject Ether");
+    }
+
+    function callResignValidator(ConsensusV1 target) external {
+        target.resignValidator(); // this will fail
+    }
+}
+
 contract ConsensusTest is Base {
     function test_validator_resignation_pass_with_default_fee() public {
         assertEq(consensus.validatorsCount(), 0);
@@ -88,6 +98,42 @@ contract ConsensusTest is Base {
         assertEq(addr.balance, 100 ether);
     }
 
+    function test_validator_resignation_revert_if_receiver_rejected_fee_return() public {
+        assertEq(consensus.validatorsCount(), 0);
+
+        // Set a custom fee for validator registration
+        uint256 customFee = 40 ether;
+        consensus.setFee(customFee);
+
+        address payable addr = payable(new RejectingRefund());
+        vm.deal(addr, 100 ether);
+
+        address payable addr2 = payable(address(2));
+        vm.deal(addr2, 100 ether);
+
+        // Act
+        vm.startPrank(addr2);
+        consensus.registerValidator{value: customFee}(prepareBLSKey(addr2)); // Add another validator to allow resign
+        vm.startPrank(addr);
+        consensus.registerValidator{value: customFee}(prepareBLSKey(addr));
+        vm.stopPrank();
+
+        // Assert
+        assertEq(consensus.validatorsCount(), 2);
+        ConsensusV1.Validator memory validator = consensus.getValidator(addr);
+        assertEq(validator.addr, addr);
+        assertEq(validator.data.blsPublicKey, prepareBLSKey(addr));
+        assertEq(validator.data.voteBalance, 0);
+        assertEq(validator.data.votersCount, 0);
+        assertEq(validator.data.fee, customFee);
+        assertEq(validator.data.isResigned, false);
+        assertEq(addr.balance, 60 ether);
+
+        vm.startPrank(addr);
+        vm.expectRevert(ConsensusV1.RefundFailed.selector);
+        consensus.resignValidator();
+    }
+
     function test_validator_resignation_revert_if_caller_is_not_validator() public {
         vm.expectRevert(ConsensusV1.CallerIsNotValidator.selector);
         consensus.resignValidator();
@@ -130,7 +176,7 @@ contract ConsensusTest is Base {
         consensus.resignValidator();
     }
 
-    function test_calculate_active_valitors_should_reset_min_validators() public {
+    function test_calculate_active_validators_should_reset_min_validators() public {
         assertEq(consensus.validatorsCount(), 0);
         address addr = address(1);
 
