ccca8ede7623e27dca84cf6927e42dea2375a10d
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-06-05 16:45:02 +0200

    Test registration

--- contracts/test/consensus/Consensus-ValidatorRegistration.sol
+++ contracts/test/consensus/Consensus-ValidatorRegistration.sol
@@ -6,7 +6,7 @@ import {Base} from "./Base.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract ConsensusTest is Base {
-    function test_validator_registration_pass() public {
+    function test_validator_registration_pass_with_default_fee() public {
         assertEq(consensus.validatorsCount(), 0);
         address addr = address(1);
 
@@ -24,7 +24,45 @@ contract ConsensusTest is Base {
         assertEq(validator.data.blsPublicKey, prepareBLSKey(addr));
         assertEq(validator.data.voteBalance, 0);
         assertEq(validator.data.votersCount, 0);
+        assertEq(validator.data.fee, 0);
         assertEq(validator.data.isResigned, false);
+    }
+
+    function test_validator_registration_pass_with_adjusted_fee() public {
+        assertEq(consensus.validatorsCount(), 0);
+
+        // Set a custom fee for validator registration
+        uint256 customFee = 50 ether;
+        consensus.setFee(customFee);
+
+        address payable addr = payable(address(1));
+        vm.deal(addr, 100 ether);
+
+        // Act
+        vm.startPrank(addr);
+        vm.expectEmit(address(consensus));
+        emit ConsensusV1.ValidatorRegistered(addr, prepareBLSKey(addr));
+        consensus.registerValidator{value: customFee}(prepareBLSKey(addr));
+        vm.stopPrank();
+
+        // Assert
+        assertEq(consensus.validatorsCount(), 1);
+        ConsensusV1.Validator memory validator = consensus.getValidator(addr);
+        assertEq(validator.addr, addr);
+        assertEq(validator.data.blsPublicKey, prepareBLSKey(addr));
+        assertEq(validator.data.voteBalance, 0);
+        assertEq(validator.data.votersCount, 0);
+        assertEq(validator.data.fee, customFee);
+        assertEq(validator.data.isResigned, false);
+    }
+
+    function test_validator_registration_revert_if_fee_is_invalid() public {
+        address payable addr = payable(address(1));
+        vm.deal(addr, 100 ether);
+        vm.startPrank(addr);
+
+        vm.expectRevert(ConsensusV1.InvalidFee.selector);
+        consensus.registerValidator{value: 40 ether}(prepareBLSKey(address(2)));
     }
 
     function test_validator_registration_revert_if_validator_is_already_registered() public {
