5312e9636e9364de6125cc3c6801da16d13e61a5
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-06-30 13:34:30 +0900

    tx hasher

--- packages/crypto-transaction/source/utilities.ts
+++ packages/crypto-transaction/source/utilities.ts
@@ -19,31 +19,68 @@ export class Utils implements Contracts.Crypto.TransactionUtilities {
 		transaction: Contracts.Crypto.TransactionData,
 		options?: Contracts.Crypto.SerializeOptions,
 	): Promise<Buffer> {
+		let fields: Array<any> = [];
+
+		const { rlpPrefix } = transaction;
+
 		// based on EIP1559 encoding
-		const fields = [
-			toBeArray(transaction.network),
-			toBeArray(transaction.nonce.toBigInt()),
-			toBeArray(0), // maxPriorityFeePerGas
-			toBeArray(transaction.gasPrice), // maxFeePerGas
-			toBeArray(transaction.gas),
-			transaction.to || "0x",
-			toBeArray(transaction.value.toBigInt()),
-			transaction.data.startsWith("0x") ? transaction.data : `0x${transaction.data}`,
-			[], // accessList is unused
-		];
+		if (rlpPrefix) {
+			if (rlpPrefix !== 0x02) {
+				throw new Error("expected EIP1559 transaction");
+			}
+
+			fields = [
+				toBeArray(transaction.network),
+				toBeArray(transaction.nonce.toBigInt()),
+				toBeArray(0), // maxPriorityFeePerGas
+				toBeArray(transaction.gasPrice), // maxFeePerGas
+				toBeArray(transaction.gas),
+				transaction.to || "0x",
+				toBeArray(transaction.value.toBigInt()),
+				transaction.data.startsWith("0x") ? transaction.data : `0x${transaction.data}`,
+				[], // accessList is unused
+			];
+		} else {
+			// Legacy encoding
+			fields = [
+				toBeArray(transaction.nonce.toBigInt()),
+				toBeArray(transaction.gasPrice),
+				toBeArray(transaction.gas),
+				transaction.to || "0x",
+				toBeArray(transaction.value.toBigInt()),
+				transaction.data.startsWith("0x") ? transaction.data : `0x${transaction.data}`,
+
+				// EIP-155 also requires chainId (`v`)
+				toBeArray(transaction.network), // v
+				toBeArray(0), // r
+				toBeArray(0), // s
+			];
+		}
 
 		if (options && !options.excludeSignature) {
 			assert.number(transaction.v);
 			assert.string(transaction.r);
 			assert.string(transaction.s);
 
-			fields.push(toBeArray(transaction.v), `0x${transaction.r}`, `0x${transaction.s}`);
+			if (rlpPrefix) {
+				fields.push(toBeArray(transaction.v), `0x${transaction.r}`, `0x${transaction.s}`);
+			} else {
+				// Legacy with EIP-155
+				const normalizedV = transaction.v;
+				const v = transaction.network * 2 + 35 + normalizedV;
+
+				fields[6] = toBeArray(v);
+				fields[7] = `0x${transaction.r}`;
+				fields[8] = `0x${transaction.s}`;
+			}
 		}
 
-		const eip1559Prefix = "02"; // marker for Type 2 (EIP1559) transaction which is the standard nowadays
-		const encoded = encodeRlp(fields).slice(2); // remove 0x prefix
+		let encoded = Buffer.from(encodeRlp(fields).slice(2), "hex");
+		if (rlpPrefix) {
+			encoded = Buffer.concat([Buffer.from([rlpPrefix]), encoded]);
+		}
 
-		return Buffer.from(keccak256(Buffer.from(`${eip1559Prefix}${encoded}`, "hex")).slice(2), "hex");
+		return Buffer.from(keccak256(encoded).slice(2), "hex");
 	}
 
 	public async getHash(transaction: Contracts.Crypto.Transaction): Promise<string> {
