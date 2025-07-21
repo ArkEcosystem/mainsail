e043bc756f30959e2095fc2bad8cae35184e5879
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-07-21 10:18:33 +0200

    Remove crypto worker count

--- scripts/install-testnet.sh
+++ scripts/install-testnet.sh
@@ -187,7 +187,7 @@ addCore() {
 }
 
 heading "Configuring for custom TestNet ..."
-# get snapshot URL    
+# get snapshot URL
 SNAP=$(curl -s -L -H "Accept: application/vnd.github+json" "https://api.github.com/repos/ArkEcosystem/mainsail-network-config/contents/testnet/mainsail/"  | grep  compressed | grep download_url | awk '{ print $2 }' | tr -d ",")
 #
     channel=evm addCore ${channel} && rm -rf ~/.config/mainsail/core/ &&  rm -rf ~/.local/state/mainsail/core/ &&  rm -rf ~/.local/share/mainsail/core/
@@ -198,7 +198,6 @@ SNAP=$(curl -s -L -H "Accept: application/vnd.github+json" "https://api.github.c
 	mainsail env:set --key=MAINSAIL_API_TRANSACTION_POOL_PORT --value=4007
 	mainsail env:set --key=MAINSAIL_API_EVM_HOST --value=0.0.0.0
 	mainsail env:set --key=MAINSAIL_API_EVM_PORT --value=4008
-	mainsail env:set --key=MAINSAIL_CRYPTO_WORKER_COUNT --value=2
 	mainsail env:set --key=MAINSAIL_API_SYNC_ENABLED --value=false
 
 warning "Cleaning up Pnpm cache .."
