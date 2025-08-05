46964172855abed3f3c1ee4ab157f1859fe2e25b
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-08-05 15:37:28 +0200

    Add skipPeerStateVerification to defaults

--- packages/p2p/source/service-provider.ts
+++ packages/p2p/source/service-provider.ts
@@ -92,6 +92,7 @@ export class ServiceProvider extends Providers.ServiceProvider {
 				port: Joi.number().integer().min(1).max(65_535).required(), // TODO: Check
 			}).required(),
 			skipDiscovery: Joi.bool(),
+			skipPeerStateVerification: Joi.bool(),
 			txPoolPort: Joi.number().integer().min(0).required(),
 			verifyTimeout: Joi.number().integer().min(0).required(),
 			whitelist: Joi.array().items(Joi.string()).required(),
