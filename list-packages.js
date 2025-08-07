eb8cfba0d7a7c5ff6f6cbc0ce801d281213b94ae
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-08-07 18:34:21 +0200

    Add list packages

---
+++ list-packages.js
@@ -0,0 +1,17 @@
+import { resolve, join } from "path";
+import { lstatSync, readdirSync, readFileSync, writeFileSync} from "fs";
+
+const source = resolve(join(process.cwd(), "packages"));
+
+const main = async () => {
+		const pkgs = readdirSync(source)
+		.filter((name) => lstatSync(`${source}/${name}`).isDirectory())
+		.filter((name) => name !== "evm")
+		.sort();
+
+		for (const pkg of pkgs) {
+			console.log(`{ "path": "packages/${pkg}" }`);
+		}
+}
+
+main();
