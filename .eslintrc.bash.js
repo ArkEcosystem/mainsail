61b4bcfd6d482fa57b7d66932dc18f24a41e8a9d
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-09-17 14:01:47 +0200

    Remove .eslintrc.bash.js

--- .eslintrc.bash.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	...require("./.eslintrc"),
-	parserOptions: {
-		project: "../../tsconfig.eslint.json",
-		extraFileExtensions: [".json"],
-		projectFolderIgnoreList: ["build", "coverage", "node_modules", "public"],
-	},
-};
