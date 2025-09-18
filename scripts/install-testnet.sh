a2bdb376505734d6c3b0742ba16791a1f8e72100
Adrian Kerchev <{ID}+{username}@users.noreply.github.com>
2025-09-18 06:15:17 -0400

    remove unnecessary apt-update step

--- scripts/install-testnet.sh
+++ scripts/install-testnet.sh
@@ -97,7 +97,6 @@ heading "Installing system dependencies..."
 success "Installed system dependencies!"
 
 heading "Make sure NTP exists systemwide and it's enabled by default..."
-    sudo apt-get update
     sudo $APT_ENV apt-get install systemd-timesyncd -yq
     sudo timedatectl set-ntp on > /dev/null 2>&1 || true
 
