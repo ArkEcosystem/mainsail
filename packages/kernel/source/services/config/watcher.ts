ddd1195275501a10ffb05ded2ee050efe1711415
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-10-01 14:40:44 +0200

    Make function async

--- packages/kernel/source/services/config/watcher.ts
+++ packages/kernel/source/services/config/watcher.ts
@@ -12,7 +12,7 @@ export class Watcher {
 	public async boot(): Promise<void> {
 		const configFiles = new Set([".env", "validators.json", "peers.json", "plugins.js", "plugins.json"]);
 
-		this.#watcher = await nsfw(this.app.configPath(), (events) => {
+		this.#watcher = await nsfw(this.app.configPath(), async (events) => {
 			for (const event of events) {
 				if (event.action === nsfw.ActionType.MODIFIED && configFiles.has(event.file)) {
 					await this.app.reboot();
