1a255d4e3f5a8b4ae16919ab239364d68bca6653
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-10-09 12:27:21 +0900

    transaction tests

---
+++ packages/crypto-transaction/source/deserializer.test.ts
@@ -0,0 +1,44 @@
+import { Contracts, Identifiers } from "@mainsail/contracts";
+import { describe, Sandbox } from "../../test-framework/source";
+import {
+	serializedTransactionContractCall,
+	serializedTransactionContractCallWithSecondSignature,
+	serializedTransactionDeploy,
+	serializedTransactionTransfer,
+	serializedTransactionTransferEqualGreater11Fields,
+	serializedTransactionTransferLessThan9Fields,
+} from "../test/fixtures/transaction";
+import { prepareSandbox } from "../test/helpers/prepare-sandbox";
+
+describe<{
+	sandbox: Sandbox;
+	deserializer: Contracts.Crypto.TransactionDeserializer;
+}>("Deserializer", ({ it, beforeEach, assert }) => {
+	beforeEach(async (context) => {
+		await prepareSandbox(context);
+
+		context.deserializer = context.sandbox.app.get<Contracts.Crypto.TransactionDeserializer>(
+			Identifiers.Cryptography.Transaction.Deserializer,
+		);
+	});
+
+	it("should be ok", async ({ deserializer }) => {
+		for (const serialized of [
+			serializedTransactionContractCall,
+			serializedTransactionContractCallWithSecondSignature,
+			serializedTransactionDeploy,
+			serializedTransactionTransfer,
+		]) {
+			await assert.resolves(async () => deserializer.deserialize(Buffer.from(serialized, "hex")));
+		}
+	});
+
+	it("should not deserialize transaction with invalid number of fields", async ({ deserializer }) => {
+		for (const [serialized, error] of [
+			[serializedTransactionTransferLessThan9Fields, "decoded RLP contains too few fields"],
+			[serializedTransactionTransferEqualGreater11Fields, "decoded RLP contains too many fields"],
+		]) {
+			await assert.rejects(async () => deserializer.deserialize(Buffer.from(serialized, "hex")), error);
+		}
+	});
+});
