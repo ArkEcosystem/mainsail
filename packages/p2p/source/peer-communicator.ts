35e162f74c734049894e1c0166ad903f52dba067
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-10-16 14:16:59 +0000

    Mark catch empty

--- packages/p2p/source/peer-communicator.ts
+++ packages/p2p/source/peer-communicator.ts
@@ -76,7 +76,9 @@ export class PeerCommunicator implements Contracts.P2P.PeerCommunicator {
 					if (statusCode === 200) {
 						peer.ports[name] = plugin.port;
 					}
-				} catch { }
+				} catch {
+					// empty
+				}
 			}),
 		);
 	}
