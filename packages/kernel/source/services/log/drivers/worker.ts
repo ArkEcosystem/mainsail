181b0cb0ec50ad6287f18b9fcf2bdda83cce1ccb
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-10-21 11:22:41 +0200

    Fix worker class

--- packages/kernel/source/services/log/drivers/worker.ts
+++ packages/kernel/source/services/log/drivers/worker.ts
@@ -1,8 +1,7 @@
 import { injectable } from "@mainsail/container";
 import { Contracts } from "@mainsail/contracts";
-import { isEmpty, prettyTime } from "@mainsail/utils";
+import { isEmpty } from "@mainsail/utils";
 import chalk, { ChalkInstance } from "chalk";
-import { differenceInMilliseconds, format } from "date-fns";
 import { inspect } from "util";
 
 @injectable()
@@ -19,8 +18,6 @@ export class WorkerLogger implements Contracts.Kernel.Logger {
 	};
 
 	protected silentConsole = false;
-
-	#lastTimestamp: Date = new Date();
 
 	public async make(options: unknown): Promise<Contracts.Kernel.Logger> {
 		return this;
@@ -81,21 +78,6 @@ export class WorkerLogger implements Contracts.Kernel.Logger {
 			message = inspect(message, { depth: 1 });
 		}
 
-		level = level ? this.levelStyles[level](`[${level.toUpperCase()}] `) : "";
-
-		const timestamp: string = format(new Date(), "yyyy-MM-dd HH:MM:ss.SSS");
-		const timestampDiff: string = this.getTimestampDiff();
-
-		process.stdout.write(`[${timestamp}] ${level}${message}${timestampDiff}\n`);
-	}
-
-	protected getTimestampDiff(): string {
-		const now = new Date();
-
-		const diff: number = differenceInMilliseconds(now, this.#lastTimestamp);
-
-		this.#lastTimestamp = new Date();
-
-		return chalk.yellow(` +${diff ? prettyTime(diff) : "0ms"}`);
+		process.stdout.write(`[${level}] (threadId) ${message}\n`);
 	}
 }
