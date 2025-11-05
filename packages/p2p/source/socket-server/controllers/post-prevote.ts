176b33b4ec1bc7ecb7039f00d6d5b828d81992e7
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-11-05 13:12:44 +0000

    Collect times

--- packages/p2p/source/socket-server/controllers/post-prevote.ts
+++ packages/p2p/source/socket-server/controllers/post-prevote.ts
@@ -1,6 +1,7 @@
 import Hapi from "@hapi/hapi";
-import { inject, injectable } from "@mainsail/container";
-import { Contracts, Identifiers } from "@mainsail/contracts";
+import { inject, injectable, postConstruct } from "@mainsail/container";
+import { Contracts, Events, Identifiers } from "@mainsail/contracts";
+import { performance } from "perf_hooks";
 
 import { getPeerIp } from "../../utils/index.js";
 
@@ -18,10 +19,33 @@ export class PostPrevoteController implements Contracts.P2P.Controller {
 	@inject(Identifiers.P2P.State)
 	private readonly state!: Contracts.P2P.State;
 
+	@inject(Identifiers.Services.EventDispatcher.Service)
+	private readonly eventDispatcher!: Contracts.Kernel.EventDispatcher;
+
+	#times: number[] = [];
+
+	@postConstruct()
+	public init(): void {
+		console.log("PostPrevoteController initialized");
+
+		this.eventDispatcher.listen(Events.ConsensusEvent.RoundStarted, {
+			handle: (payload: { name: string }) => {
+				console.log("Event received:", payload.name);
+
+				this.#times.sort((a, b) => a - b);
+				console.log("Prevote processing times (ms):", this.#times.map((time) => Math.floor(time)).join(", "));
+
+				this.#times = [];
+			}
+		});
+	}
+
 	public async handle(
 		request: Contracts.P2P.PostPrevoteRequest,
 		h: Hapi.ResponseToolkit,
 	): Promise<Contracts.P2P.PostPrevoteResponse> {
+		const start = performance.now();
+
 		try {
 			const prevote = await this.factory.makePrevoteFromBytes(request.payload.prevote);
 
@@ -35,6 +59,9 @@ export class PostPrevoteController implements Contracts.P2P.Controller {
 		} catch (error) {
 			this.peerDisposer.banPeer(getPeerIp(request), error.message);
 		}
+
+		const end = performance.now();
+		this.#times.push(end - start);
 
 		return {};
 	}
--- packages/p2p/source/socket-server/plugins/codec.ts
+++ packages/p2p/source/socket-server/plugins/codec.ts
@@ -1,9 +1,11 @@
-import { inject, injectable, multiInject, tagged } from "@mainsail/container";
-import { Contracts, Identifiers } from "@mainsail/contracts";
+import { inject, injectable, multiInject, postConstruct, tagged } from "@mainsail/container";
+import { Contracts, Events, Identifiers } from "@mainsail/contracts";
 import { Providers } from "@mainsail/kernel";
 
 import { Route } from "../routes/route.js";
 import { BasePlugin } from "./base-plugin.js";
+
+let times: number[] = [];
 
 @injectable()
 export class CodecPlugin extends BasePlugin {
@@ -20,6 +22,26 @@ export class CodecPlugin extends BasePlugin {
 	@multiInject(Identifiers.P2P.Routes)
 	private readonly routes!: Route[];
 
+	@inject(Identifiers.Services.EventDispatcher.Service)
+	private readonly eventDispatcher!: Contracts.Kernel.EventDispatcher;
+
+
+	@postConstruct()
+	public init(): void {
+		console.log("CodecPlugin initialized");
+
+		this.eventDispatcher.listen(Events.ConsensusEvent.RoundStarted, {
+			handle: (payload: { name: string }) => {
+				console.log("Event received:", payload.name);
+
+				times.sort((a, b) => a - b);
+				console.log("Codec prevote processing times (ms):", times.map((time) => time.toFixed(3)).join(", "));
+
+				times = [];
+			}
+		});
+	}
+
 	public register(server) {
 		if (this.configuration.getRequired("developmentMode.enabled")) {
 			return;
@@ -33,8 +55,20 @@ export class CodecPlugin extends BasePlugin {
 		server.ext({
 			async method(request, h) {
 				try {
-					request.payload = allRoutesConfigByPath[request.path].codec.request.deserialize(request.payload);
+					if (request.path === "/postPrevote") {
+						const start = performance.now();
+
+						request.payload = allRoutesConfigByPath[request.path].codec.request.deserialize(request.payload);
+
+						const end = performance.now();
+						times.push(end - start);
+					} else {
+						request.payload = allRoutesConfigByPath[request.path].codec.request.deserialize(request.payload);
+
+					}
 				} catch (error) {
+					console.error(`Payload deserializing on ${request.path} failed: ${error}`);
+
 					return this.disposeAndReturnBadRequest(request, h, `Payload deserializing failed: ${error}`);
 				}
 				return h.continue;
