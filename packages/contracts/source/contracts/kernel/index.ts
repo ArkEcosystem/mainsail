2ff95667d4ea597065250b4ad1caabce18272600
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-11-13 15:02:43 +0100

    Move types

--- packages/contracts/source/contracts/kernel/index.ts
+++ packages/contracts/source/contracts/kernel/index.ts
@@ -1,5 +1,6 @@
 export * from "./application.js";
 export * from "./cache.js";
+export * from "./common.js";
 export * from "./config.js";
 export * as Container from "./container.js";
 export * from "./events.js";
