3919c61db825469d2b5f944f99a03b2a3e0c1d2e
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-11-13 15:12:41 +0100

    Update PipelineFactory imports

--- packages/contracts/source/contracts/types/index.ts
+++ packages/contracts/source/contracts/types/index.ts
@@ -1,5 +1,4 @@
 import type { CacheStore } from "../kernel/cache.js";
-import type { Pipeline } from "../kernel/pipeline.js";
 
 export type { Class, JsonObject, JsonValue, PackageJson, Primitive } from "type-fest";
 
@@ -8,5 +7,3 @@ export type KeyValuePair<T = any> = Record<string, T>;
 export type ActionArguments = Record<string, any>;
 
 export type CacheFactory<K, T> = <K, T>() => CacheStore<K, T>;
-
-export type PipelineFactory<T> = () => Pipeline<T>;
--- packages/kernel/source/types/index.ts
+++ packages/kernel/source/types/index.ts
@@ -5,5 +5,3 @@ export type KeyValuePair<T = any> = Record<string, T>;
 export type ActionArguments = Record<string, any>;
 
 export type CacheFactory<K, T> = <K, T>() => Promise<Contracts.Kernel.CacheStore<K, T>>;
-
-export type PipelineFactory<T> = () => Contracts.Kernel.Pipeline<T>;
