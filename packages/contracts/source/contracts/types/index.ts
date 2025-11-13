556659fe05bc3be02e4a705ae8422e5466be9879
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-11-13 15:14:36 +0100

    Update CacheFactory imports

--- packages/contracts/source/contracts/types/index.ts
+++ packages/contracts/source/contracts/types/index.ts
@@ -1,9 +1,5 @@
-import type { CacheStore } from "../kernel/cache.js";
-
 export type { Class, JsonObject, JsonValue, PackageJson, Primitive } from "type-fest";
 
 export type KeyValuePair<T = any> = Record<string, T>;
 
 export type ActionArguments = Record<string, any>;
-
-export type CacheFactory<K, T> = <K, T>() => CacheStore<K, T>;
--- packages/kernel/source/types/index.ts
+++ packages/kernel/source/types/index.ts
@@ -1,7 +1,3 @@
-import type { Contracts } from "@mainsail/contracts";
-
 export type KeyValuePair<T = any> = Record<string, T>;
 
 export type ActionArguments = Record<string, any>;
-
-export type CacheFactory<K, T> = <K, T>() => Promise<Contracts.Kernel.CacheStore<K, T>>;
