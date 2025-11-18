7d195a8ce528d895ee8bbb968dd49877670de747
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-11-18 14:41:02 +0100

    Fix p2p tests

--- packages/p2p/source/socket-server/codecs/get-status.test.ts
+++ packages/p2p/source/socket-server/codecs/get-status.test.ts
@@ -2,9 +2,7 @@ import { describe } from "../../../../test-framework/source";
 import { request, response } from "../../../test/fixtures/get-status";
 import { getStatus } from "./get-status.js";
 import type * as types from "./proto/protos.d.ts";
-import * as _protos from "./proto/protos.js";
-
-const proto = (_protos as any).default.getStatus as typeof types.getStatus;
+import { getStatus as proto } from "./proto/protos.js";
 
 describe("PeerCodec", ({ it, assert }) => {
 	it.only("#getStatus should serde request", () => {
