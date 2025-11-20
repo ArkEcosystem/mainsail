3a894a6832ebaa5b66e2d20f78885e63f29f9c92
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-11-20 12:58:07 +0900

    partially fix `@mainsail/api-common`

--- packages/api-common/source/plugins/whitelist.ts
+++ packages/api-common/source/plugins/whitelist.ts
@@ -1,11 +1,12 @@
 import Boom from "@hapi/boom";
+import type Hapi from "@hapi/hapi";
 import { isWhitelisted } from "@mainsail/utils";
 
 import { getIp } from "../utils/index.js";
 
 export const whitelist = {
 	name: "whitelist",
-	register(server, options) {
+	register(server: Hapi.Server, options: { whitelist: string[]; trustProxy: boolean }): void {
 		server.ext({
 			async method(request, h) {
 				if (!options.whitelist) {
