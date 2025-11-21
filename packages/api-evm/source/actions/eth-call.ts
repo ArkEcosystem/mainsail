71fd47aa4cdc3d88fa810e2ceccfd99bb42c7110
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-11-21 11:11:27 +0100

    Update eth_call

--- packages/api-evm/source/actions/eth-call.ts
+++ packages/api-evm/source/actions/eth-call.ts
@@ -1,8 +1,8 @@
-import { Enums, Identifiers } from "@mainsail/constants";
+import { Identifiers } from "@mainsail/constants";
 import { inject, injectable, tagged } from "@mainsail/container";
 import type { Contracts } from "@mainsail/contracts";
 import { RpcError } from "@mainsail/exceptions";
-import { toBytes } from "viem";
+import dayjs from "dayjs";
 
 type TxData = {
 	from?: string;
@@ -55,6 +55,8 @@ export class CallAction implements Contracts.Api.RPC.Action {
 
 		const {
 			block: { maxGasLimit },
+			gas: { minimumGasPrice },
+			evmSpec,
 		} = this.configuration.getMilestone();
 
 		// Cap gas limit to block gas limit
@@ -64,6 +66,7 @@ export class CallAction implements Contracts.Api.RPC.Action {
 			gasLimit = userGasLimit < gasLimit ? userGasLimit : gasLimit;
 		}
 
+		/*
 		const { success, output } = await this.evm.view({
 			// default to zero address
 			data: Buffer.from(toBytes(data.data)),
@@ -72,9 +75,32 @@ export class CallAction implements Contracts.Api.RPC.Action {
 			specId: Enums.Evm.SpecId.LATEST,
 			to: data.to,
 		});
+		*/
 
-		if (success) {
-			return `0x${output?.toString("hex")}`;
+		const commitKey = { blockNumber: BigInt(this.configuration.getHeight()), round: BigInt(0) };
+
+		const nonce = data.from ? (await this.evm.getAccountInfo(data.from)).nonce : 0;
+
+		const { receipt } = await this.evm.simulate({
+			blockContext: {
+				commitKey,
+				gasLimit: BigInt(maxGasLimit),
+				timestamp: BigInt(dayjs().valueOf()),
+				validatorAddress: "0x0000000000000000000000000000000000000001",
+			},
+			data: data.data ? Buffer.from(data.data.slice(2), "hex") : Buffer.alloc(0),
+			from: data.from ?? "0x" + "0".repeat(40),
+			gasLimit,
+			// gasPrice: data.gasPrice ? BigInt(data.gasPrice) : BigInt(gas.minimumGasPrice),
+			gasPrice: BigInt(minimumGasPrice),
+			nonce: BigInt(nonce),
+			specId: evmSpec,
+			to: data.to,
+			value: data.value ? BigInt(data.value) : BigInt(0),
+		});
+
+		if (receipt.status === 1) {
+			return `0x${receipt.output?.toString("hex")}`;
 		}
 
 		throw new RpcError("execution reverted");
