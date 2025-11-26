a00d7d2feca66945064d22d65fc129221824e102
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-11-26 15:10:24 +0900

    fix `@mainsail/test-runner`

--- packages/test-runner/source/contracts.ts
+++ packages/test-runner/source/contracts.ts
@@ -1,25 +1,32 @@
-export interface Fake {
-	call<U>(...arguments_: any[]): U;
+export interface Fake<TArguments extends unknown[], TResult> {
+	call(...arguments_: TArguments): TResult;
+
 	called(): void;
-	calledWith(...arguments_: any[]): void;
-	notCalledWith(...arguments_: any[]): void;
-	calledNthWith(index: number, ...arguments_: any[]): void;
+	calledWith(...arguments_: TArguments): void;
+	notCalledWith(...arguments_: TArguments): void;
+	calledNthWith(index: number, ...arguments_: TArguments): void;
 	calledOnce(): void;
 	calledTimes(times: number): void;
 	neverCalled(): void;
-	getCallArgs(index: number): any[];
+
+	getCallArgs(index: number): TArguments;
+
 	restore(): void;
 	reset(): void;
+
+	toFunction(): (...arguments_: TArguments) => TResult;
 }
 
-export interface Stub extends Fake {
-	returnValue(value: unknown): Stub;
-	returnValueOnce(value: unknown): Stub;
-	returnValueNth(nth: number, value: unknown): Stub;
-	returnValueSequence(sequence: unknown[]): Stub;
-	resolvedValue(value: unknown): Stub;
-	resolvedValueNth(nth: number, value: unknown): Stub;
-	resolvedValueSequence(sequence: unknown[]): Stub;
-	rejectedValue(value: unknown): Stub;
-	rejectedValueNth(nth: number, value: unknown): Stub;
+export interface Stub<TArguments extends unknown[], TResult> extends Fake<TArguments, TResult> {
+	returnValue(value: TResult): this;
+	returnValueOnce(value: TResult): this;
+	returnValueNth(nth: number, value: TResult): this;
+	returnValueSequence(sequence: TResult[]): this;
+
+	resolvedValue(value: unknown): this;
+	resolvedValueNth(nth: number, value: unknown): this;
+	resolvedValueSequence(sequence: unknown[]): this;
+
+	rejectedValue(value: unknown): this;
+	rejectedValueNth(nth: number, value: unknown): this;
 }
--- packages/test-runner/source/spy.ts
+++ packages/test-runner/source/spy.ts
@@ -1,8 +1,6 @@
-import type { SinonSpy } from "sinon";
-
 import type { Fake as IFake } from "./contracts.js";
 import { Fake } from "./fake.js";
 
-export class Spy extends Fake<SinonSpy> implements IFake {
-	//
-}
+export class Spy<TArguments extends unknown[] = unknown[], TResult = unknown>
+	extends Fake<TArguments, TResult>
+	implements IFake<TArguments, TResult> {}
