d1614c4b65630b7b27db39753929d907a4b26334
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-11-26 15:01:50 +0900

    fix `@mainsail/transaction-pool-service`

--- packages/transaction-pool-service/source/actions/verify-transaction.ts
+++ packages/transaction-pool-service/source/actions/verify-transaction.ts
@@ -4,9 +4,14 @@ import { Services } from "@mainsail/kernel";
 
 @injectable()
 export class VerifyTransactionAction extends Services.Triggers.Action {
-	public async execute(arguments_: Contracts.Kernel.ActionArguments): Promise<boolean> {
-		const handler: Contracts.Transactions.TransactionHandler = arguments_.handler;
-		const transaction: Contracts.Crypto.Transaction = arguments_.transaction;
+	public async execute(
+		arguments_: Contracts.Kernel.ActionArguments<{
+			handler: Contracts.Transactions.TransactionHandler;
+			transaction: Contracts.Crypto.Transaction;
+		}>,
+	): Promise<boolean> {
+		const handler = arguments_.handler;
+		const transaction = arguments_.transaction;
 
 		return handler.verify(transaction);
 	}
