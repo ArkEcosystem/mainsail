f2611b9da1f22b1fab501d9ce1dffa67a18414cf
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2025-11-27 09:42:15 +0900

    fix `@mainsail/api-http`

--- packages/api-http/source/controllers/node.ts
+++ packages/api-http/source/controllers/node.ts
@@ -21,7 +21,7 @@ export class NodeController extends Controller {
 	@inject(ApiDatabaseIdentifiers.PeerRepositoryFactory)
 	private readonly peerRepositoryFactory!: ApiDatabaseContracts.PeerRepositoryFactory;
 
-	public async status(request: Hapi.Request) {
+	public async status(request: Hapi.Request): Promise<object> {
 		const state = await this.getState();
 		const peerBlockNumber = await this.peerRepositoryFactory().getPeerBlockNumberP90();
 		const ownBlockNumber = Number(state?.blockNumber ?? 0);
@@ -36,7 +36,7 @@ export class NodeController extends Controller {
 		};
 	}
 
-	public async syncing(request: Hapi.Request) {
+	public async syncing(request: Hapi.Request): Promise<object> {
 		const state = await this.getState();
 		const peerBlockNumber = await this.peerRepositoryFactory().getPeerBlockNumberP90();
 		const ownBlockNumber = Number(state?.blockNumber ?? 0);
@@ -51,7 +51,7 @@ export class NodeController extends Controller {
 		};
 	}
 
-	public async fees(request: Hapi.Request) {
+	public async fees(request: Hapi.Request): Promise<object> {
 		const configuration = await this.getConfiguration();
 		const cryptoConfiguration = configuration.cryptoConfiguration as Contracts.Crypto.NetworkConfig;
 		const genesisTimestamp = cryptoConfiguration.genesisBlock.block.timestamp;
@@ -70,7 +70,7 @@ export class NodeController extends Controller {
 		return { data: grouped, meta: { days: request.query.days } };
 	}
 
-	public async configuration(request: Hapi.Request) {
+	public async configuration(request: Hapi.Request): Promise<object> {
 		const configuration = await this.getConfiguration();
 		const plugins = await this.getPlugins();
 
@@ -95,7 +95,7 @@ export class NodeController extends Controller {
 		};
 	}
 
-	public async configurationCrypto(request: Hapi.Request) {
+	public async configurationCrypto(request: Hapi.Request): Promise<object> {
 		const configuration = await this.getConfiguration();
 		return {
 			data: configuration?.cryptoConfiguration ?? {},
--- packages/api-http/source/controllers/transactions.ts
+++ packages/api-http/source/controllers/transactions.ts
@@ -16,7 +16,7 @@ export class TransactionsController extends Controller {
 	@inject(ApiDatabaseIdentifiers.TransactionRepositoryFactory)
 	private readonly transactionRepositoryFactory!: ApiDatabaseContracts.TransactionRepositoryFactory;
 
-	public async index(request: Hapi.Request) {
+	public async index(request: Hapi.Request): Promise<object> {
 		const criteria: Search.Criteria.TransactionCriteria = request.query;
 		const pagination = this.getListingPage(request);
 		const sorting = this.getListingOrder(request);
@@ -37,7 +37,7 @@ export class TransactionsController extends Controller {
 		);
 	}
 
-	public async show(request: Hapi.Request) {
+	public async show(request: Hapi.Request): Promise<object> {
 		const transaction = await this.transactionRepositoryFactory()
 			.createQueryBuilder()
 			.select()
