81ffd43938cbcb1f5ac30df6058c7ada6866cb61
sebastijankuzner <sebastijan.kuzner@outlook.com>
2025-12-12 13:55:05 +0100

    Fix schemas

--- packages/crypto-messages/source/schemas.ts
+++ packages/crypto-messages/source/schemas.ts
@@ -1,22 +1,9 @@
 import { Enums } from "@mainsail/constants";
 import type { AnySchemaObject } from "ajv";
 
-export const schemas: Record<"prevote" | "precommit", AnySchemaObject> = {
-	precommit: {
-		$id: "precommit",
-		properties: {
-			blockHash: { $ref: "blockHash" },
-			blockNumber: { minimum: 1, type: "integer" },
-			round: { minimum: 0, type: "integer" },
-			signature: { $ref: "consensusSignature" },
-			type: { enum: [Enums.Crypto.MessageType.Prevote, Enums.Crypto.MessageType.Precommit] },
-			validatorIndex: { isValidatorIndex: {} },
-		},
-		required: ["type", "blockNumber", "round", "validatorIndex", "signature"],
-		type: "object",
-	},
-	prevote: {
-		$id: "prevote",
+export const schemas: Record<"message", AnySchemaObject> = {
+	message: {
+		$id: "message",
 		properties: {
 			blockHash: { $ref: "blockHash" },
 			blockNumber: { minimum: 1, type: "integer" },
