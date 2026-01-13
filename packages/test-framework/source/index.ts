c14083f3ad690d29a9500c4dd9678a82a25f0365
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-01-13 13:41:55 +0100

    Remove passphrases

--- packages/test-framework/source/index.ts
+++ packages/test-framework/source/index.ts
@@ -1,4 +1,3 @@
 export * from "./app/index.js";
 export * from "./cli/index.js"; // CLI Helpers
-export { default as passphrases } from "./internal/passphrases.json"; // mnemonics
 export { assert, describe, describeEach, describeSkip, describeWithContext, loader } from "@mainsail/test-runner"; // Entity Factories for commonly used entities like blocks and wallets
