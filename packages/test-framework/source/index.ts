fe405c3d12ec0edf8dfda80c79418d4883531281
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-01-13 13:17:24 +0100

    Don't export signer

--- packages/test-framework/source/index.ts
+++ packages/test-framework/source/index.ts
@@ -1,7 +1,6 @@
 export * from "./app/index.js";
 export * from "./cli/index.js"; // CLI Helpers
 export * as Factories from "./factories/index.js"; // Entity Factories for commonly used entities like blocks and wallets
-export * from "./internal/index.js"; // internals are also useful for bridgechains
 export { default as passphrases } from "./internal/passphrases.json"; // mnemonics
 export * as Utils from "./utils/index.js"; // Utilities for common tasks like sending HTTP requests or altering wallets
 export { assert, describe, describeEach, describeSkip, describeWithContext, loader } from "@mainsail/test-runner"; // Entity Factories for commonly used entities like blocks and wallets
