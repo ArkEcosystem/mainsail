606d9762bb4c86e2d7f54e8e9e25e98b97bd2b80
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-01-13 13:13:09 +0100

    Imporve exports

--- packages/test-framework/source/index.ts
+++ packages/test-framework/source/index.ts
@@ -1,15 +1,7 @@
-// uvu
-export { assert, describe, describeEach, describeSkip, describeWithContext, loader } from "@mainsail/test-runner";
-// Entity Factories for commonly used entities like blocks and wallets
 export * from "./app/index.js";
-// CLI Helpers
-export * from "./cli/index.js";
-// Entity Factories for commonly used entities like blocks and wallets
-// Generators for commonly used entities like blocks and wallets
-export * as Factories from "./factories/index.js";
-// Utilities for common tasks like sending HTTP requests or altering wallets
-export * from "./utils/index.js"; // @TODO export as Utils
-// internals are also useful for bridgechains
-export * from "./internal/index.js";
-// mnemonics
-export { default as passphrases } from "./internal/passphrases.json";
+export * from "./cli/index.js"; // CLI Helpers
+export * as Factories from "./factories/index.js"; // Entity Factories for commonly used entities like blocks and wallets
+export * from "./internal/index.js"; // internals are also useful for bridgechains
+export { default as passphrases } from "./internal/passphrases.json"; // mnemonics
+export * as Utils from "./utils/index.js"; // Utilities for common tasks like sending HTTP requests or altering wallets
+export { assert, describe, describeEach, describeSkip, describeWithContext, loader } from "@mainsail/test-runner"; // Entity Factories for commonly used entities like blocks and wallets
