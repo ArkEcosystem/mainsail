b526e679e8284c673bd1f56fbb4721377c8d94f7
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-01-13 13:07:06 +0100

    Remove state store

--- packages/test-framework/source/mocks/index.ts
+++ packages/test-framework/source/mocks/index.ts
@@ -1,4 +1,3 @@
 export * as TransactionPoolQuery from "./query.js";
 export * as ServiceProviderRepository from "./service-provider-repository.js";
-export * as store from "./state-store.js";
 export * as TransactionPoolProcessor from "./transaction-pool-processor.js";
