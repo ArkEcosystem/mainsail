5760a5cc0f216fb4ba0890c8818d4cd4d3012ccc
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-01-13 13:19:21 +0100

    Remove utils

--- packages/test-framework/source/index.ts
+++ packages/test-framework/source/index.ts
@@ -2,5 +2,4 @@ export * from "./app/index.js";
 export * from "./cli/index.js"; // CLI Helpers
 export * as Factories from "./factories/index.js"; // Entity Factories for commonly used entities like blocks and wallets
 export { default as passphrases } from "./internal/passphrases.json"; // mnemonics
-export * as Utils from "./utils/index.js"; // Utilities for common tasks like sending HTTP requests or altering wallets
 export { assert, describe, describeEach, describeSkip, describeWithContext, loader } from "@mainsail/test-runner"; // Entity Factories for commonly used entities like blocks and wallets
