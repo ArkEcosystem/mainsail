d5e97587986c99ca4522ae88fa831a869dc4c603
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-01-14 17:02:02 +0100

    logger-pino

--- packages/logger-pino/source/driver.test.ts
+++ packages/logger-pino/source/driver.test.ts
@@ -7,7 +7,7 @@ import { readdirSync } from "fs";
 import { Writable } from "stream";
 import { dirSync, setGracefulCleanup } from "tmp";
 
-import { describe } from "../../test-framework/source";
+import { describe } from "@mainsail/test-framework";
 import { PinoLogger } from "./driver";
 
 describe("Logger", ({ assert, afterAll, afterEach, beforeAll, beforeEach, it }) => {
