a02be3db86aa9075c1d880a49ca7471002e05a03
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-01-20 17:38:13 +0100

    Add Command contracts

--- packages/cli/source/commands/discover-commands.ts
+++ packages/cli/source/commands/discover-commands.ts
@@ -3,20 +3,20 @@ import { inject, injectable } from "@mainsail/container";
 import type { Contracts } from "@mainsail/contracts";
 import { lstatSync, readdirSync } from "fs";
 
-import { Command, CommandList } from "./command.js";
+import { Command } from "./command.js";
 
 @injectable()
-export class DiscoverCommands {
+export class DiscoverCommands implements Contracts.Cli.DiscoverCommands {
 	@inject(Identifiers.Cli.Application.Instance)
 	private readonly app!: Contracts.Cli.Application;
 
-	public async within(path: string): Promise<CommandList> {
+	public async within(path: string): Promise<Contracts.Cli.CommandList> {
 		const commandFiles: string[] = readdirSync(path)
 			.map((item: string) => `${path}/${item}`)
 			.filter((item: string) => lstatSync(item).isFile())
 			.filter((item: string) => item.endsWith(".js"));
 
-		const commands: CommandList = {};
+		const commands: Contracts.Cli.CommandList = {};
 
 		for (const file of commandFiles) {
 			const { Command } = await import(file);
@@ -30,8 +30,8 @@ export class DiscoverCommands {
 		return commands;
 	}
 
-	public async from(packages: string[]): Promise<CommandList> {
-		const commands: CommandList = {};
+	public async from(packages: string[]): Promise<Contracts.Cli.CommandList> {
+		const commands: Contracts.Cli.CommandList = {};
 
 		if (!Array.isArray(packages) || packages.length <= 0) {
 			return commands;
---
+++ packages/contracts/source/contracts/cli/commands.ts
@@ -0,0 +1,25 @@
+import type { InputValue, InputValues } from "./cli.js"
+
+export interface Command {
+	register(argv: string[]): void;
+	initialize(): Promise<void>;
+	interact(): Promise<void>;
+	run(): Promise<void>;
+	showHelp(): void;
+	getArguments(): InputValues;
+	getArgument(name: string): string;
+	setArgument(name: string, value: InputValue): void;
+	hasArgument(name: string): boolean;
+	getFlags<T = InputValues>(): T;
+	getFlag<T = InputValue>(name: string): T;
+	setFlag(name: string, value: InputValue): void;
+	hasFlag(name: string): boolean;
+	execute(): Promise<void>;
+}
+
+export type CommandList = Record<string, Command>;
+
+export interface DiscoverCommands {
+	within(path: string): Promise<CommandList>;
+	from(packages: string[]): Promise<CommandList>;
+}
