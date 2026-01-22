fef61eceac474e1ee988ead87219551f36af6cdd
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-01-22 11:53:46 +0100

    Move console to cli

--- packages/cli/source/index.ts
+++ packages/cli/source/index.ts
@@ -9,4 +9,5 @@ export * from "./input/index.js";
 export * from "./output/index.js";
 export * as Plugins from "./plugins/index.js";
 export * as Services from "./services/index.js";
+export * as Test from "./test/index.js";
 export * as Utils from "./utils/index.js";
--- packages/test-framework/source/index.ts
+++ packages/test-framework/source/index.ts
@@ -1,3 +1,2 @@
 export * from "./app/index.js";
-export * from "./cli/index.js"; // CLI Helpers
 export { assert, describe, describeEach, describeSkip, describeWithContext, loader } from "@mainsail/test-runner"; // Entity Factories for commonly used entities like blocks and wallets
