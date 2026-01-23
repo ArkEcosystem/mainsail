4b34a324a52626d639893427f475f46de1d42685
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-01-23 12:37:55 +0900

    delete `@mainsail/test-framework`

--- packages/test-framework/source/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./app/index.js";
-export { assert, describe, describeEach, describeSkip, describeWithContext, loader } from "@mainsail/test-runner"; // Entity Factories for commonly used entities like blocks and wallets
