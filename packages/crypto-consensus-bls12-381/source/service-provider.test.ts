46c9b7f58de241dbfe65ef97f89379a13b375193
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-01-23 12:30:32 +0900

    refactor `@mainsail/crypto-consensus-bls12-381`

--- packages/crypto-consensus-bls12-381/source/service-provider.test.ts
+++ packages/crypto-consensus-bls12-381/source/service-provider.test.ts
@@ -1,24 +1,26 @@
 import { Identifiers } from "@mainsail/constants";
 
-import { describe, Sandbox } from "@mainsail/test-framework";
+import { Application } from "@mainsail/kernel";
+import { Container } from "@mainsail/container";
+import { describe } from "@mainsail/test-runner";
 import { ServiceProvider } from "./service-provider.js";
 
 describe<{
-	sandbox: Sandbox;
+	app: Application;
 	serviceProvider: ServiceProvider;
 }>("ServiceProvider", ({ beforeEach, it, assert, spy }) => {
 	const validator = {
-		addSchema: () => {},
+		addSchema: () => { },
 	};
 
 	beforeEach((context) => {
-		context.sandbox = new Sandbox();
-		context.sandbox.app.bind(Identifiers.Cryptography.Validator).toConstantValue(validator);
+		context.app = new Application(new Container());
+		context.app.bind(Identifiers.Cryptography.Validator).toConstantValue(validator);
 
-		context.serviceProvider = context.sandbox.app.resolve(ServiceProvider);
+		context.serviceProvider = context.app.resolve(ServiceProvider);
 	});
 
-	it("#register - should bind tagged and register schema", async ({ sandbox, serviceProvider }) => {
+	it("#register - should bind tagged and register schema", async ({ app, serviceProvider }) => {
 		const spyAddSchema = spy(validator, "addSchema");
 
 		await serviceProvider.register();
@@ -31,7 +33,7 @@ describe<{
 			Identifiers.Cryptography.Identity.PublicKey.Factory,
 			Identifiers.Cryptography.Identity.PublicKey.Serializer,
 			Identifiers.Cryptography.Signature.Instance,
-		].forEach((identifier) => assert.true(sandbox.app.isBoundTagged(identifier, "type", "consensus")));
+		].forEach((identifier) => assert.true(app.isBoundTagged(identifier, "type", "consensus")));
 
 		spyAddSchema.calledTimes(2);
 	});
