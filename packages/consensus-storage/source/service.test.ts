f4e9062e5d79b969a3fe4d67a964f0aebce3521b
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-01-28 17:47:34 +0100

    Test get state

--- packages/consensus-storage/source/service.test.ts
+++ packages/consensus-storage/source/service.test.ts
@@ -1,4 +1,4 @@
-import { Identifiers } from "@mainsail/constants";
+import { Identifiers, Enums } from "@mainsail/constants";
 
 import { Application } from "@mainsail/kernel";
 import { dirSync, setGracefulCleanup } from "tmp";
@@ -8,11 +8,28 @@ import { Service } from "./service";
 
 import { open } from "lmdb";
 import { join } from "path";
+import { Contracts } from "@mainsail/contracts";
 
 describe<{
 	app: Application;
 	service: Service;
 }>("ServiceProvider", ({ beforeEach, afterEach, it, assert, spy }) => {
+	const state0: Contracts.Consensus.StateData = {
+		blockNumber: 0,
+		round: 0,
+		step: Enums.Consensus.Step.Precommit,
+		validRound: undefined,
+		lockedRound: undefined,
+	}
+
+	const state1: Contracts.Consensus.StateData = {
+		blockNumber: 1,
+		round: 1,
+		step: Enums.Consensus.Step.Prevote,
+		validRound: 0,
+		lockedRound: 0,
+	}
+
 	beforeEach((context) => {
 		const app = new Application();
 
@@ -45,6 +62,24 @@ describe<{
 		assert.undefined(await service.getState());
 	});
 
+	it("#getState - should return latest stored state", async ({ service }) => {
+		assert.undefined(await service.getState());
+
+		await service.persist({
+			state: state0,
+			proposals: [],
+			messages: [],
+		});
+		assert.equal(await service.getState(), state0);
+
+		await service.persist({
+			state: state1,
+			proposals: [],
+			messages: [],
+		});
+		assert.equal(await service.getState(), state1);
+	});
+
 	it("#getProposals - should return empty array", async ({ service }) => {
 		assert.equal(await service.getProposals(), []);
 	});
@@ -53,4 +88,6 @@ describe<{
 		assert.equal(await service.getMessages(), []);
 	});
 
+	it("#persist - should persist", async ({ service }) => {
+	});
 });
