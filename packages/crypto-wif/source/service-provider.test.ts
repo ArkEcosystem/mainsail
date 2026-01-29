480be76273a2a2acfdd1a0354dfd13f74faeb84d
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-01-29 14:45:10 +0100

    Add service provider

---
+++ packages/crypto-wif/source/service-provider.test.ts
@@ -0,0 +1,25 @@
+import { Identifiers } from "@mainsail/constants";
+
+import { Application } from "@mainsail/kernel";
+
+import { describe } from "@mainsail/test-runner";
+import { ServiceProvider } from "./service-provider";
+
+describe<{
+	app: Application;
+	serviceProvider: ServiceProvider;
+}>("ServiceProvider", ({ beforeEach, it, assert }) => {
+	beforeEach((context) => {
+		const app = new Application();
+
+		context.serviceProvider = app.resolve(ServiceProvider);
+		context.app = app;
+	});
+
+	it("#register - should be ok", async ({ serviceProvider, app }) => {
+		await serviceProvider.register();
+
+		assert.true(app.isBound(Identifiers.Cryptography.Identity.Wif.Factory));
+	});
+
+});
