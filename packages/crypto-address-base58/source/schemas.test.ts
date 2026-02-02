c0fbc8e17307dc85ebe447b99f94494794a0e866
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-02-02 13:17:05 +0100

    Fix schemas tests

--- packages/crypto-address-base58/source/schemas.test.ts
+++ packages/crypto-address-base58/source/schemas.test.ts
@@ -1,11 +1,12 @@
 import type { Contracts } from "@mainsail/contracts";
 import { Identifiers } from "@mainsail/constants";
 import { Configuration } from "@mainsail/crypto-config";
-import { ServiceProvider as Schnorr } from "@mainsail/crypto-key-pair-schnorr";
 import { schemas as baseSchemas } from "@mainsail/crypto-validation";
+import { ServiceProvider as ECDSA } from "@mainsail/crypto-key-pair-ecdsa";
 import { ServiceProvider as CoreValidation } from "@mainsail/validation";
 import { Validator } from "@mainsail/validation/source/validator";
 import { generateMnemonic } from "bip39";
+
 
 import cryptoJson from "../../core/bin/config/devnet/core/crypto.json";
 import { Application } from "@mainsail/kernel";
@@ -64,7 +65,7 @@ describe<{
 	});
 
 	it("address - should be ok for factory", async (context) => {
-		await context.app.resolve<Schnorr>(Schnorr).register();
+		await context.app.resolve<ECDSA>(ECDSA).register();
 
 		assert.undefined(
 			context.validator.validate(
