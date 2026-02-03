d4ee5fcb2fcf6b7a20e3928108bbd560d1bb75b3
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-02-03 15:37:57 +0100

    Fix schemas

--- packages/crypto-signature-bls12-381/source/index.ts
+++ packages/crypto-signature-bls12-381/source/index.ts
@@ -1,3 +1,4 @@
+export * from "./schemas.js";
 export * from "./serializer.js";
 export * from "./service-provider.js";
 export * from "./signature.js";
--- packages/database/test/helpers/prepare-sandbox.ts
+++ packages/database/test/helpers/prepare-sandbox.ts
@@ -1,26 +1,27 @@
-import type { Contracts } from "@mainsail/contracts";
 import { Identifiers } from "@mainsail/constants";
-import { Application } from "@mainsail/kernel";
-
-import crypto from "../../../core/bin/config/devnet/core/crypto.json";
-import { ServiceProvider as CoreCryptoAddressKeccak256 } from "@mainsail/crypto-address-keccak256";
+import type { Contracts } from "@mainsail/contracts";
 import { ServiceProvider as CoreCryptoAddressBase58 } from "@mainsail/crypto-address-base58";
+import { ServiceProvider as CoreCryptoAddressKeccak256 } from "@mainsail/crypto-address-keccak256";
 import { ServiceProvider as CoreCryptoBlock } from "@mainsail/crypto-block";
-import { ServiceProvider as CoreCryptoConfig } from "@mainsail/crypto-config";
 import { ServiceProvider as CoreCryptoCommit } from "@mainsail/crypto-commit";
+import { ServiceProvider as CoreCryptoConfig } from "@mainsail/crypto-config";
 import { ServiceProvider as CoreCryptoHashBcrypto } from "@mainsail/crypto-hash-bcrypto";
+import { ServiceProvider as CoreCryptoKeyPairBls } from "@mainsail/crypto-key-pair-bls12-381";
 import { ServiceProvider as CoreCryptoKeyPairEcdsa } from "@mainsail/crypto-key-pair-ecdsa";
+import { ServiceProvider as CoreCryptoSignatureBls} from "@mainsail/crypto-signature-bls12-381";
 import { ServiceProvider as CoreCryptoSignatureEcdsa } from "@mainsail/crypto-signature-ecdsa";
-import { ServiceProvider as CoreCryptoConsensusBls12381 } from "@mainsail/crypto-consensus-bls12-381";
 import { ServiceProvider as CoreCryptoTransaction } from "@mainsail/crypto-transaction";
 import { ServiceProvider as CoreCryptoValidation } from "@mainsail/crypto-validation";
 import { ServiceProvider as CoreCryptoWif } from "@mainsail/crypto-wif";
+import { Application } from "@mainsail/kernel";
 import { ServiceProvider as CoreEvents } from "@mainsail/kernel/source/services/events";
 import { ServiceProvider as CoreTriggers } from "@mainsail/kernel/source/services/triggers";
 import { ServiceProvider as CoreSerializer } from "@mainsail/serializer";
 import { ServiceProvider as CoreTransactions } from "@mainsail/transactions";
 import { ServiceProvider as CoreValidation } from "@mainsail/validation";
 import { dirSync } from "tmp";
+
+import crypto from "../../../core/bin/config/devnet/core/crypto.json";
 
 export const prepareSandbox = async (context: { app?: Application }) => {
 	context.app = new Application();
@@ -42,14 +43,15 @@ export const prepareSandbox = async (context: { app?: Application }) => {
 
 	await context.app.resolve(CoreCryptoAddressKeccak256).register();
 	await context.app.resolve(CoreCryptoAddressBase58).register();
-	await context.app.resolve(CoreCryptoConsensusBls12381).register();
+	await context.app.resolve(CoreCryptoSignatureBls).register();
+	await context.app.resolve(CoreCryptoKeyPairBls).register();
 
 	await context.app.resolve(CoreCryptoValidation).register();
 	await context.app.resolve(CoreCryptoWif).register();
 
 	context.app.bind(Identifiers.Services.Log.Service).toConstantValue({
-		info: (msg) => console.log(msg),
-		debug: (msg) => console.log(msg),
+		debug: (message) => console.log(message),
+		info: (message) => console.log(message),
 	});
 	context.app.get<Contracts.Crypto.Configuration>(Identifiers.Cryptography.Configuration).setConfig(crypto);
 
@@ -64,8 +66,8 @@ export const prepareSandbox = async (context: { app?: Application }) => {
 	context.app.bind(Identifiers.State.Store).toConstantValue({
 		getLastBlock: () => ({
 			data: {
-				number: 1,
 				id: "0000000000000000000000000000000000000000000000000000000000000000",
+				number: 1,
 			},
 		}),
 	});
