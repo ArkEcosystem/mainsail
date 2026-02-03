21c7728a189092a6c3d3542ae17c644a7cc7d8a4
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-02-03 15:20:35 +0100

    Remove package

--- packages/crypto-consensus-bls12-381/source/schemas.ts
+++
@@ -1,8 +0,0 @@
-
-export const schemas = {
-	consensusSignature: {
-		$id: "consensusSignature",
-		allOf: [{ $ref: "alphanumeric" }, { maxLength: 192, minLength: 192 }],
-		type: "string",
-	},
-};
