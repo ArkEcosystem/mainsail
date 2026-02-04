a643df059094c561c7ecbacb8cf3fa6ee483ac9d
sebastijankuzner <58827427+sebastijankuzner@users.noreply.github.com>
2026-02-04 15:38:44 +0000

    style: resolve style guide violations

--- packages/crypto-signature-bls12-381/source/serializer.test.ts
+++ packages/crypto-signature-bls12-381/source/serializer.test.ts
@@ -16,7 +16,8 @@ describe<{
 	});
 
 	it("should serialize and deserialize signature", async ({ serializer }) => {
-		const signature = "9529fb1b3001aa735a2d3a70ac8568c9e5757c7112d43de6a0463b3d4354b54a706dc3ab9ca49d32f2307059fe93c5b017949f54427e257af38f72cff36b041ce30fb5ebbac636b2f84ced80a16e0150b059771dae40a5baf86f5805baf061b0";
+		const signature =
+			"9529fb1b3001aa735a2d3a70ac8568c9e5757c7112d43de6a0463b3d4354b54a706dc3ab9ca49d32f2307059fe93c5b017949f54427e257af38f72cff36b041ce30fb5ebbac636b2f84ced80a16e0150b059771dae40a5baf86f5805baf061b0";
 
 		const byteBuffer = ByteBuffer.fromBuffer(Buffer.alloc(500));
 
