f3b4e8a4b2b65c3c4b695a7e488357074e751b55
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-02-05 12:42:43 +0900

    extract app and add all validator secrets to config

--- tests/e2e/consensus/checks/config.mjs
+++ tests/e2e/consensus/checks/config.mjs
@@ -1,13 +1,25 @@
 import crypto from "/mainsail/tests/e2e/consensus/nodes/node0/core/crypto.json" with { type: "json" }
-import validatorSecrets from "/mainsail/tests/e2e/consensus/nodes/node0/core/validators.json" with { type: "json" }
+import validatorSecrets1 from "/mainsail/tests/e2e/consensus/nodes/node0/core/validators.json" with { type: "json" }
+import validatorSecrets2 from "/mainsail/tests/e2e/consensus/nodes/node1/core/validators.json" with { type: "json" }
+import validatorSecrets3 from "/mainsail/tests/e2e/consensus/nodes/node2/core/validators.json" with { type: "json" }
+import validatorSecrets4 from "/mainsail/tests/e2e/consensus/nodes/node3/core/validators.json" with { type: "json" }
+import validatorSecrets5 from "/mainsail/tests/e2e/consensus/nodes/node4/core/validators.json" with { type: "json" }
 
 export const config = {
     to: "0x1000000000000000000000000000000000000000",
-    senderPassphrase: validatorSecrets.secrets[0],
+    tokenBeneficiary: "0x9000000000000000000000000000000000000009",
+    senderPassphrase: validatorSecrets1.secrets[0],
     peer: {
         apiEvmUrl: "http://api-node:4008",
         apiTxPoolUrl: "http://api-node:4007",        
         apiHttpUrl: "http://api-http:4003"
     },
+    validatorSecrets: [
+        validatorSecrets1.secrets[0],
+        validatorSecrets2.secrets[0],
+        validatorSecrets3.secrets[0],
+        validatorSecrets4.secrets[0],
+        validatorSecrets5.secrets[0],
+    ],
     crypto,
 }
