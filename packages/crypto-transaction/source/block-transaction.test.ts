0101797bf76ef36edf364f7bc6bc18bd32bfab41
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-02-18 10:38:48 +0100

    Replace transactions

--- packages/crypto-transaction/source/block-transaction.test.ts
+++ packages/crypto-transaction/source/block-transaction.test.ts
@@ -1,12 +1,12 @@
 import { describe } from "@mainsail/test-runner";
-import { transactionTransferData, serializedTransactionTransfer } from "../test/fixtures/transaction.js";
+import { Transactions, Serialized } from "../test/fixtures/index.js";
 import { BlockTransaction } from "./block-transaction.js";
 
 describe("BlockTransaction", ({ it, assert }) => {
 	it("should create transaction and convert toData", () => {
-		const transaction = new BlockTransaction(
-			transactionTransferData,
-			Buffer.from(serializedTransactionTransfer, "hex"),
+		const tx = new BlockTransaction(
+			Transactions.transactionTransfer,
+			Buffer.from(Serialized.transactionTransfer, "hex"),
 			{
 				transactionIndex: 0,
 				blockNumber: 1,
@@ -14,33 +14,32 @@ describe("BlockTransaction", ({ it, assert }) => {
 			},
 		);
 
-		assert.equal(transaction.toData(), transactionTransferData);
-		assert.undefined(transaction.toData().legacySecondSignature);
-		assert.equal(transaction.serialized.toString("hex"), serializedTransactionTransfer);
+		const { serialized: _, ...transactionData } = Transactions.transactionTransfer;
 
-		assert.equal(transaction.transactionIndex, 0);
-		assert.equal(transaction.blockNumber, 1);
-		assert.equal(transaction.blockHash, "0000000000000000000000000000000000000000000000000000000000000000");
+		assert.equal(tx.toData(), transactionData);
+		assert.undefined(tx.toData().legacySecondSignature);
+		assert.equal(tx.serialized.toString("hex"), Serialized.transactionTransfer);
+
+		assert.equal(tx.transactionIndex, 0);
+		assert.equal(tx.blockNumber, 1);
+		assert.equal(tx.blockHash, "0000000000000000000000000000000000000000000000000000000000000000");
 	});
 
 	it("should create transaction and convert toData with legacySecondSignature", () => {
-		const tx = {
-			...transactionTransferData,
-			legacySecondSignature: "022a6f404dbd49c9c74a3d88d65b967a9b51d3465c92833e8e2ede11e7242f014",
-		};
-
-		const transaction = new BlockTransaction(tx, Buffer.from(serializedTransactionTransfer, "hex"), {
+		const tx = new BlockTransaction(Transactions.transactionContractCallWithSecondSignature, Buffer.from(Serialized.transactionContractCallWithSecondSignature, "hex"), {
 			transactionIndex: 1,
 			blockNumber: 1,
 			blockHash: "0000000000000000000000000000000000000000000000000000000000000000",
 		});
 
-		assert.equal(transaction.toData(), tx);
-		assert.equal(transaction.toData().legacySecondSignature, tx.legacySecondSignature);
-		assert.equal(transaction.serialized.toString("hex"), serializedTransactionTransfer);
+		const { serialized: _, ...transactionData } = Transactions.transactionContractCallWithSecondSignature;
 
-		assert.equal(transaction.transactionIndex, 1);
-		assert.equal(transaction.blockNumber, 1);
-		assert.equal(transaction.blockHash, "0000000000000000000000000000000000000000000000000000000000000000");
+		assert.equal(tx.toData(), transactionData);
+		assert.equal(tx.toData().legacySecondSignature, transactionData.legacySecondSignature);
+		assert.equal(tx.serialized.toString("hex"), Serialized.transactionContractCallWithSecondSignature);
+
+		assert.equal(tx.transactionIndex, 1);
+		assert.equal(tx.blockNumber, 1);
+		assert.equal(tx.blockHash, "0000000000000000000000000000000000000000000000000000000000000000");
 	});
 });
--- packages/crypto-transaction/source/transaction.test.ts
+++ packages/crypto-transaction/source/transaction.test.ts
@@ -1,26 +1,23 @@
 import { describe } from "@mainsail/test-runner";
-import { transactionTransferData, serializedTransactionTransfer } from "../test/fixtures/transaction.js";
+import { Transactions, Serialized } from "../test/fixtures/index.js";
 import { Transaction } from "./transaction.js";
 
 describe("Transaction", ({ it, assert }) => {
 	it("should create transaction and convert toData", () => {
-		const transaction = new Transaction(transactionTransferData, Buffer.from(serializedTransactionTransfer, "hex"));
+		const tx = new Transaction(Transactions.transactionTransfer, Buffer.from(Serialized.transactionTransfer, "hex"));
 
-		assert.equal(transaction.toData(), transactionTransferData);
-		assert.undefined(transaction.toData().legacySecondSignature);
-		assert.equal(transaction.serialized.toString("hex"), serializedTransactionTransfer);
+		const { serialized: _, ...transactionData } = Transactions.transactionTransfer;
+		assert.equal(tx.toData(), transactionData);
+		assert.undefined(tx.toData().legacySecondSignature);
+		assert.equal(tx.serialized.toString("hex"), Serialized.transactionTransfer);
 	});
 
 	it("should create transaction and convert toData with legacySecondSignature", () => {
-		const tx = {
-			...transactionTransferData,
-			legacySecondSignature: "022a6f404dbd49c9c74a3d88d65b967a9b51d3465c92833e8e2ede11e7242f014",
-		};
+		const tx = new Transaction(Transactions.transactionContractCallWithSecondSignature, Buffer.from(Serialized.transactionContractCallWithSecondSignature, "hex"));
 
-		const transaction = new Transaction(tx, Buffer.from(serializedTransactionTransfer, "hex"));
-
-		assert.equal(transaction.toData(), tx);
-		assert.equal(transaction.toData().legacySecondSignature, tx.legacySecondSignature);
-		assert.equal(transaction.serialized.toString("hex"), serializedTransactionTransfer);
+		const { serialized: _, ...transactionData } = Transactions.transactionContractCallWithSecondSignature;
+		assert.equal(tx.toData(), transactionData);
+		assert.equal(tx.toData().legacySecondSignature, Transactions.transactionContractCallWithSecondSignature.legacySecondSignature);
+		assert.equal(tx.serialized.toString("hex"), Serialized.transactionContractCallWithSecondSignature);
 	});
 });
