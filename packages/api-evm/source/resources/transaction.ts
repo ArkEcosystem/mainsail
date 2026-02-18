0729c4d2d7937a0eb1c552312a98019458631fdb
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-02-18 14:15:20 +0000

    Fix transaction resource

--- packages/api-evm/source/resources/transaction.ts
+++ packages/api-evm/source/resources/transaction.ts
@@ -13,7 +13,7 @@ export class TransactionResource {
 			gas: `0x${transaction.gasLimit.toString(16)}`,
 			gasPrice: `0x${transaction.gasPrice.toString(16)}`,
 			hash: `0x${transaction.hash}`,
-			input: `0x${transaction.data}`,
+			input: transaction.data,
 			nonce: `0x${transaction.nonce.toString(16)}`,
 			// eslint-disable-next-line unicorn/no-null
 			to: transaction.to || null,
