03a35018976e510a722eae6c540c83b3a3b2924d
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-03-03 15:43:54 +0100

    Test serializeProposedData

--- packages/crypto-proposal/source/serializer.test.ts
+++ packages/crypto-proposal/source/serializer.test.ts
@@ -1,12 +1,14 @@
-import type { Contracts } from "@mainsail/contracts";
-
 import { Application } from "@mainsail/kernel";
 import { describe } from "@mainsail/test-runner";
 import {
 	proposalData,
 	proposalDataWithValidRound,
 	serializedBlock,
+	proposalLockProof,
 	serializedProposal,
+	serializedProposalLockProof,
+	serializedProposalData,
+	serializedProposalDataWithLockProof,
 	serializedProposalDataWithValidRound,
 	serializedProposalDataWithValidRoundForSignature,
 	serializedProposalForSignature,
@@ -83,20 +85,35 @@ describe<{
 	});
 
 	it("#serializeLockProof - should serialize and deserialize lock proof", async ({ deserializer, serializer }) => {
-		const proposalLockProof: Contracts.Crypto.AggregatedSignature = {
-			signature:
-				"927628d67c385fe216aa800def9cce0c09f5f9fbf836583d7c07ab6a98e1b5681802c92f81ad54984236a07fa389dbab1519f3c91ad39a505a61c3624a88c65da71fe721d7af0ed452516771b94d027be713dba68e14fa2c9680e35b63f0e038",
-			validators: [true, true, true, false, false, true, true, true, true, false],
-		};
-
-		const serializedProposalLockProof =
-			"927628d67c385fe216aa800def9cce0c09f5f9fbf836583d7c07ab6a98e1b5681802c92f81ad54984236a07fa389dbab1519f3c91ad39a505a61c3624a88c65da71fe721d7af0ed452516771b94d027be713dba68e14fa2c9680e35b63f0e0380ae701000000000000";
-
 		const serialized = (await serializer.serializeLockProof(proposalLockProof)).toString("hex");
 		assert.equal(serialized, serializedProposalLockProof);
 
 		const deserialized = await deserializer.deserializeLockProof(Buffer.from(serialized, "hex"));
 		assert.equal(proposalLockProof.signature, deserialized.signature);
 		assert.equal(proposalLockProof.validators, deserialized.validators);
+	});
+
+	it("#serializeProposedData - should correctly serialize block", async ({ serializer }) => {
+		const serialized =
+			await serializer.serializeProposedData(
+				{
+					block: { serialized: serializedBlock },
+					lockProof: undefined
+				},
+			);
+
+		assert.equal(serialized.toString("hex"), serializedProposalData);
+	});
+
+	it("#serializeProposedData - should correctly serialize block, with lock proof", async ({ serializer }) => {
+		const serialized =
+			await serializer.serializeProposedData(
+				{
+					block: { serialized: serializedBlock },
+					lockProof: proposalLockProof
+				},
+			);
+
+		assert.equal(serialized.toString("hex"), serializedProposalDataWithLockProof);
 	});
 });
