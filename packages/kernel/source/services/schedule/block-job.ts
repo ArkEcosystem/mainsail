77667964f447ec4b98b547b629425aa1ab97e5d6
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-03-03 08:34:55 +0100

    FIx block job

--- packages/kernel/source/services/schedule/block-job.ts
+++ packages/kernel/source/services/schedule/block-job.ts
@@ -9,10 +9,7 @@ import { ExecuteCallbackWhenReady } from "./listeners.js";
 @injectable()
 export class BlockJob implements Job {
 	@inject(Identifiers.Services.EventDispatcher.Service)
-	private readonly events!: Contracts.Kernel.EventDispatcher<Contracts.Crypto.BlockData>;
-
-	@inject(Identifiers.Cryptography.Configuration)
-	private readonly configuration!: Contracts.Crypto.Configuration;
+	private readonly eventDispatcher!: Contracts.Kernel.EventDispatcher<Contracts.Crypto.BlockData>;
 
 	protected blockCount = 1;
 
@@ -22,43 +19,18 @@ export class BlockJob implements Job {
 
 			callback();
 
-			await this.events.dispatch(Events.ScheduleEvent.BlockJobFinished, {
+			await this.eventDispatcher.dispatch(Events.ScheduleEvent.BlockJobFinished, {
 				blockCount: this.blockCount,
 				executionTime: performance.now() - start,
 			});
 		};
 
-		this.events.listen(Events.BlockEvent.Received, new ExecuteCallbackWhenReady(onCallback, this.blockCount));
+		this.eventDispatcher.listen(Events.BlockEvent.Received, new ExecuteCallbackWhenReady(onCallback, this.blockCount));
 	}
 
 	public cron(blockCount: number): this {
 		this.blockCount = blockCount;
 
 		return this;
-	}
-
-	public everyBlock(): this {
-		return this.cron(1);
-	}
-
-	public everyFiveBlocks(): this {
-		return this.cron(5);
-	}
-
-	public everyTenBlocks(): this {
-		return this.cron(10);
-	}
-
-	public everyFifteenBlocks(): this {
-		return this.cron(15);
-	}
-
-	public everyThirtyBlocks(): this {
-		return this.cron(30);
-	}
-
-	public everyRound(): this {
-		// TODO: rebuild on milestone change
-		return this.cron(this.configuration.getMilestone(1).roundValidators);
 	}
 }
