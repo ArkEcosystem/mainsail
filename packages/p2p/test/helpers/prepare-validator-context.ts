bcb6e7c44dce49e26b1de63987b3066cc6802d6f
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-03-13 15:58:02 +0100

    Fix p2p tests

--- packages/p2p/test/helpers/prepare-validator-context.ts
+++ packages/p2p/test/helpers/prepare-validator-context.ts
@@ -1,7 +1,6 @@
 import { Identifiers } from "@mainsail/constants";
 import { schemas as cryptoBlockSchemas } from "@mainsail/crypto-block/distribution/index.js";
 import { Configuration } from "@mainsail/crypto-config/distribution/index.js";
-import { makeKeywords as makeProposalKeywords } from "@mainsail/crypto-proposal/distribution/keywords.js";
 import { schemas as cryptoTransactionSchemas } from "@mainsail/crypto-transaction/distribution/index.js";
 import { schemas as cryptoValidationSchemas } from "@mainsail/crypto-validation/distribution/index.js";
 import { makeKeywords as makeCryptoValidationKeywords } from "@mainsail/crypto-validation/distribution/keywords.js";
@@ -25,10 +24,6 @@ export const prepareValidatorContext = (context: Context) => {
 	for (const keyword of Object.values(makeCryptoValidationKeywords(configuration))) {
 		context.validator.addKeyword(keyword);
 	}
-
-	const messageKeywords = makeProposalKeywords(configuration);
-	context.validator.addKeyword(messageKeywords.limitToRoundValidators);
-	context.validator.addKeyword(messageKeywords.isValidatorIndex);
 
 	context.validator.addSchema(cryptoValidationSchemas.hex);
 	context.validator.addSchema(cryptoBlockSchemas.blockHash);
