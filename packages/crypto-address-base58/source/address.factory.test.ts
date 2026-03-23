f628cf145411a79b5376822985926918a110e3a0
sebastijankuzner <58827427+sebastijankuzner@users.noreply.github.com>
2026-03-23 18:57:13 +0000

    style: resolve style guide violations [ci-lint-fix]

--- packages/crypto-address-base58/source/address.factory.test.ts
+++ packages/crypto-address-base58/source/address.factory.test.ts
@@ -17,12 +17,8 @@ describe<{ app: Application }>("AddressFactory", ({ assert, beforeEach, it }) =>
 	beforeEach(async (context) => {
 		context.app = new Application();
 		context.app.bind(Identifiers.Cryptography.Configuration).to(Configuration).inSingletonScope();
-		context.app
-			.get<Contracts.Crypto.Configuration>(Identifiers.Cryptography.Configuration)
-			.setConfig(cryptoConfig);
-		context.app
-			.get<Contracts.Crypto.Configuration>(Identifiers.Cryptography.Configuration)
-			.set("network.wif", 170);
+		context.app.get<Contracts.Crypto.Configuration>(Identifiers.Cryptography.Configuration).setConfig(cryptoConfig);
+		context.app.get<Contracts.Crypto.Configuration>(Identifiers.Cryptography.Configuration).set("network.wif", 170);
 
 		await context.app.resolve(CoreValidation).register();
 		await context.app.resolve<ECDSA>(ECDSA).register();
