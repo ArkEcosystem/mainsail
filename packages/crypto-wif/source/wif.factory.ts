339b65cb5234ea0de9f2058822c6911dbff8b855
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-04-02 20:04:25 +0000

    Remove toPrivateKey

--- packages/crypto-wif/source/wif.factory.ts
+++ packages/crypto-wif/source/wif.factory.ts
@@ -2,8 +2,7 @@ import type { Contracts } from "@mainsail/contracts";
 
 import { Identifiers } from "@mainsail/constants";
 import { inject, injectable, tagged } from "@mainsail/container";
-import { WifNetworkError } from "@mainsail/exceptions";
-import { encode, decode } from "wif";
+import { encode } from "wif";
 
 @injectable()
 export class WIFFactory implements Contracts.Crypto.WIFFactory {
@@ -30,22 +29,5 @@ export class WIFFactory implements Contracts.Crypto.WIFFactory {
 			privateKey: Buffer.from(keys.privateKey, "hex"),
 			version: this.configuration.get("network.wif"),
 		});
-	}
-
-	public async toPrivateKey(wif: string): Promise<{
-		compressed: boolean;
-		privateKey: string;
-	}> {
-		const networkVersion = this.configuration.get<number>("network.wif");
-		const decoded = decode(wif);
-
-		if (decoded.version !== networkVersion) {
-			throw new WifNetworkError(networkVersion, decoded.version);
-		}
-
-		return {
-			compressed: decoded.compressed,
-			privateKey: Buffer.from(decoded.privateKey).toString("hex"),
-		};
 	}
 }
