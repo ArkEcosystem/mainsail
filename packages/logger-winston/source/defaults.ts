421f8e9da3651099e4f73aa26ca3fe555c2f215a
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-04-24 00:37:24 +0900

    refactor(logger-winston): remove package (#1280)
    * delete `@mainsail/logger-winston`
    
    * update pnpm-lock.yaml
    
    * remove leftovers

--- packages/logger-winston/source/defaults.ts
+++
@@ -1,9 +0,0 @@
-import { EnvironmentVariables } from "@mainsail/constants";
-import { Environment } from "@mainsail/kernel";
-
-export const defaults = {
-	levels: {
-		console: Environment.get(EnvironmentVariables.MAINSAIL_LOG_LEVEL, "info"),
-		file: Environment.get(EnvironmentVariables.MAINSAIL_LOG_LEVEL_FILE, "debug"),
-	},
-};
