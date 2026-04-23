7526757fe7b05dc2252ed3b6182d81c82642104b
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-04-23 17:50:41 +0900

    delete `@mainsail/logger-winston`

--- packages/logger-winston/source/index.test.ts
+++
@@ -1,8 +0,0 @@
-import * as index from "./index";
-import { describe } from "@mainsail/test-runner";
-
-describe("Index", ({ assert, it }) => {
-	it("should export ServiceProvider", () => {
-		assert.defined(index.ServiceProvider);
-	});
-});
