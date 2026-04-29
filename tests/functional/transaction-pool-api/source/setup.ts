48ba6365467786f0e3665406a0f50fd944b19e14
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-04-29 11:46:41 +0000

    Ignore

--- tests/functional/transaction-pool-api/source/setup.ts
+++ tests/functional/transaction-pool-api/source/setup.ts
@@ -10,6 +10,7 @@ import { Worker } from "./worker.js";
 
 type PluginOptions = Record<string, any>;
 
+// @ts-ignore
 BigInt.prototype.toJSON = function () {
   const int = Number.parseInt(this.toString());
   return int ?? this.toString();
