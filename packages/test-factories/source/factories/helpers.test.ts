b44782f2291291fa7b10ce4fb375f13edfc34145
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-05-11 09:49:19 +0000

    Remove from tests

--- packages/test-factories/source/factories/helpers.test.ts
+++ packages/test-factories/source/factories/helpers.test.ts
@@ -11,7 +11,6 @@ describe("Helpers", ({ it, assert }) => {
 		// assert.defined(await factory("ValidatorResignation", cryptoConfig));
 		// assert.defined(await factory("Vote", cryptoConfig));
 		// assert.defined(await factory("Unvote", cryptoConfig));
-		// assert.defined(await factory("MultiSignature", cryptoConfig));
 		// assert.defined(await factory("MultiPayment", cryptoConfig));
 		assert.defined(await factory("EvmCall", cryptoConfig));
 	});
