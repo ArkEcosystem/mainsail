17f40a7ce1b146ba0447373a030493419433c1ba
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-05-15 13:07:09 +0900

    make shuffle inline

--- packages/utils/source/shuffle.test.ts
+++ packages/utils/source/shuffle.test.ts
@@ -4,7 +4,7 @@ import { shuffle } from "./shuffle";
 describe("shuffle", async ({ assert, it, nock, loader }) => {
 	it("should return a new array with items in random order", () => {
 		const possibleValues = numberArray(100);
-		const shuffledValues = shuffle(possibleValues);
+		const shuffledValues = shuffle([...possibleValues]);
 
 		assert.includeAllMembers(shuffledValues, possibleValues);
 		assert.not.equal(shuffledValues, possibleValues);
