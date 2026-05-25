03cbdfff5e18fdffcb1c824ea71697a9e65a8719
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-05-25 10:26:45 +0000

    Move transaction validator

--- packages/forger/source/service-provider.ts
+++ packages/forger/source/service-provider.ts
@@ -1,14 +1,25 @@
 import { Identifiers } from "@mainsail/constants";
 import { injectable } from "@mainsail/container";
+import { Contracts } from "@mainsail/contracts";
 import { Providers } from "@mainsail/kernel";
 import Joi from "joi";
 
 import { BlockForger } from "./block-forger.js";
+import { TransactionValidator } from "./transaction-validator.js";
 
 @injectable()
 export class ServiceProvider extends Providers.ServiceProvider {
 	public async register(): Promise<void> {
 		this.app.bind(Identifiers.Forger.Block).to(BlockForger).inSingletonScope();
+
+		this.app.bind(Identifiers.Transaction.Validator.Instance).to(TransactionValidator);
+
+		this.app
+			.bind<() => TransactionValidator>(Identifiers.Transaction.Validator.Factory)
+			.toFactory(
+				(context: Contracts.Kernel.Container.ResolutionContext) => () =>
+					context.get(Identifiers.Transaction.Validator.Instance),
+			);
 	}
 
 	public configSchema(): Joi.AnySchema {
