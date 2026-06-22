d0525fad2bf57a15dac66b87e247247f62f396eb
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-06-22 13:04:13 +0900

    cleanup resync

--- tests/functional/resync/source/resync-legacy.test.ts
+++ tests/functional/resync/source/resync-legacy.test.ts
@@ -2,66 +2,70 @@ import type { Contracts } from "@mainsail/contracts";
 import { describe } from "@mainsail/test-runner";
 import { setupLegacySyncNode } from "./setup.js";
 import { verifyLegacyNodeIntegrity } from "./integrity.js";
-import { forgeTransactions, getWallets, waitBlock } from "./utilities.js"
+import { forgeTransactions, getWallets } from "./utilities.js";
 import { dirSync } from "tmp";
 import { parseEther } from "viem";
 import { EvmCalls, Utils } from "@mainsail/test-transaction-builders";
 import { Identifiers } from "@mainsail/constants";
 
 describe<{
-    syncNode: Contracts.Kernel.Application;
-    dataDirectory: string;
+	syncNode: Contracts.Kernel.Application;
+	dataDirectory: string;
 }>("Resync Legacy", ({ beforeEach, afterEach, it, assert }) => {
-    beforeEach(async (context) => {
-        const dataDirectory = dirSync({ unsafeCleanup: true }).name
+	beforeEach(async (context) => {
+		const dataDirectory = dirSync({ unsafeCleanup: true }).name;
 
-        context.dataDirectory = dataDirectory;
-        context.syncNode = await setupLegacySyncNode(dataDirectory);
-    });
+		context.dataDirectory = dataDirectory;
+		context.syncNode = await setupLegacySyncNode(dataDirectory);
+	});
 
-    afterEach(async ({ syncNode, dataDirectory }) => {
-        await verifyLegacyNodeIntegrity(assert, syncNode, dataDirectory);
-    });
+	afterEach(async ({ syncNode, dataDirectory }) => {
+		await verifyLegacyNodeIntegrity(assert, syncNode, dataDirectory);
+	});
 
-    it.only("should be ok", async ({ syncNode }) => {
-        await waitBlock(syncNode, 5);
-    });
+	it.only("should be ok", async ({ syncNode }) => {
+		await Utils.waitBlock({ app: syncNode }, 5);
+	});
 
-    it("should be ok with transfer from legacy cold wallet", async ({ syncNode }) => {
-        const wallets = await getWallets(syncNode);
+	it("should be ok with transfer from legacy cold wallet", async ({ syncNode }) => {
+		const wallets = await getWallets(syncNode);
 
-        const mnemonic = "this is a top secret passphrase";
-        // base58: D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib
-        // pub: 034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192
-        // priv: 814857ce48e291893feab95df02e1dbf7ad3994ba46f247f77e4eefd5d8734a2
+		const mnemonic = "this is a top secret passphrase";
+		// base58: D61mfSggzbvQgTUe6JhYKH2doHaqJ3Dyib
+		// pub: 034151a3ec46b5670a682b0a63394f863587d1bc97483b1b6c70eb58e7f0aed192
+		// priv: 814857ce48e291893feab95df02e1dbf7ad3994ba46f247f77e4eefd5d8734a2
 
-        const legacyWalletKeyPair = await syncNode
-            .getTagged<Contracts.Crypto.KeyPairFactory>(Identifiers.Cryptography.Identity.KeyPair.Factory, "type", "wallet")
-            .fromMnemonic(mnemonic);
+		const legacyWalletKeyPair = await syncNode
+			.getTagged<Contracts.Crypto.KeyPairFactory>(
+				Identifiers.Cryptography.Identity.KeyPair.Factory,
+				"type",
+				"wallet",
+			)
+			.fromMnemonic(mnemonic);
 
-        // const legacyWalletAddress = await syncNode.get<Contracts.Crypto.AddressFactory>(Identifiers.Cryptography.Identity.Address.Factory)
-        //     .fromPublicKey(legacyWalletKeyPair.publicKey);
+		// const legacyWalletAddress = await syncNode.get<Contracts.Crypto.AddressFactory>(Identifiers.Cryptography.Identity.Address.Factory)
+		//     .fromPublicKey(legacyWalletKeyPair.publicKey);
 
-        // console.log(legacyWalletAddress, legacyWalletKeyPair);
+		// console.log(legacyWalletAddress, legacyWalletKeyPair);
 
-        const context = { app: syncNode, wallets };
-        const randomWallet = await Utils.getRandomColdWallet(context);
-        const randomWallet2 = await Utils.getRandomColdWallet(context);
+		const context = { app: syncNode, wallets };
+		const randomWallet = await Utils.getRandomColdWallet(context);
+		const randomWallet2 = await Utils.getRandomColdWallet(context);
 
-        // Send legacy funds to a new wallet
-        const txFromLegacyWallet = await EvmCalls.makeEvmCall(context, {
-            sender: legacyWalletKeyPair,
-            recipient: randomWallet.address,
-            value: parseEther("0.001"),
-        });
-        assert.true(await forgeTransactions(context, [txFromLegacyWallet]));
+		// Send legacy funds to a new wallet
+		const txFromLegacyWallet = await EvmCalls.makeEvmCall(context, {
+			sender: legacyWalletKeyPair,
+			recipient: randomWallet.address,
+			value: parseEther("0.001"),
+		});
+		assert.true(await forgeTransactions(context, [txFromLegacyWallet]));
 
-        // Spent funds
-        const txFromReceivedLegacyFunds = await EvmCalls.makeEvmCall(context, {
-            sender: randomWallet.keyPair,
-            recipient: randomWallet2.address,
-            value: parseEther("0.0005"),
-        });
-        assert.true(await forgeTransactions(context, [txFromReceivedLegacyFunds]));
-    });
+		// Spent funds
+		const txFromReceivedLegacyFunds = await EvmCalls.makeEvmCall(context, {
+			sender: randomWallet.keyPair,
+			recipient: randomWallet2.address,
+			value: parseEther("0.0005"),
+		});
+		assert.true(await forgeTransactions(context, [txFromReceivedLegacyFunds]));
+	});
 });
--- tests/functional/resync/source/utilities.ts
+++ tests/functional/resync/source/utilities.ts
@@ -1,96 +1,78 @@
-import { Identifiers } from "@mainsail/constants";
 import type { Contracts } from "@mainsail/contracts";
-import { assert, sleep } from "@mainsail/utils";
+
+import { Identifiers } from "@mainsail/constants";
+import { Utils } from "@mainsail/test-transaction-builders";
+import { assert } from "@mainsail/utils";
 
 export const forgeTransactions = async (
-    { app }: { app: Contracts.Kernel.Application },
-    transactions: Contracts.Crypto.Transaction[],
+	{ app }: { app: Contracts.Kernel.Application },
+	transactions: Contracts.Crypto.Transaction[],
 ): Promise<boolean> => {
-    const processor = app.get<Contracts.TransactionPool.Processor>(Identifiers.TransactionPool.Processor);
-    await processor.process(transactions.map((t) => t.serialized));
-    await waitBlock(app);
+	const processor = app.get<Contracts.TransactionPool.Processor>(Identifiers.TransactionPool.Processor);
+	await processor.process(transactions.map((t) => t.serialized));
+	await Utils.waitBlock({ app });
 
-    for (const tx of transactions) {
-        if (!await isTransactionCommitted({ app }, tx)) {
-            return false;
-        }
-    }
+	for (const tx of transactions) {
+		if (!(await isTransactionCommitted({ app }, tx))) {
+			return false;
+		}
+	}
 
-    return true;
-};
-
-export const waitBlock = async (app: Contracts.Kernel.Application, count: number = 1): Promise<void> => {
-    const store = app.get<Contracts.State.Store>(Identifiers.State.Store);
-    const query = app.get<Contracts.TransactionPool.Query>(Identifiers.TransactionPool.Query);
-
-    let remainingTransactions = await query.getAll().all();
-
-    let currentBlockNumber = store.getBlockNumber();
-    let targetBlockNumber = currentBlockNumber + count;
-
-    do {
-        await sleep(100);
-        currentBlockNumber = store.getBlockNumber();
-        remainingTransactions = await query.getAll().all();
-
-        if (remainingTransactions.length > 0) {
-            targetBlockNumber = Math.max(currentBlockNumber, targetBlockNumber) + 1;
-        }
-    } while (currentBlockNumber < targetBlockNumber);
+	return true;
 };
 
 export const isTransactionCommitted = async (
-    { app }: { app: Contracts.Kernel.Application },
-    { hash }: Contracts.Crypto.Transaction,
+	{ app }: { app: Contracts.Kernel.Application },
+	{ hash }: Contracts.Crypto.Transaction,
 ): Promise<boolean> => {
-    const evm = app.getTagged<Contracts.Evm.Instance>(Identifiers.Evm.Instance, "instance", "evm");
-    const database = app.get<Contracts.Database.DatabaseService>(Identifiers.Database.Service);
-    const transaction = await database.getTransactionByHash(hash);
-    if (!transaction) {
-        return false;
-    }
+	const evm = app.getTagged<Contracts.Evm.Instance>(Identifiers.Evm.Instance, "instance", "evm");
+	const database = app.get<Contracts.Database.DatabaseService>(Identifiers.Database.Service);
+	const transaction = await database.getTransactionByHash(hash);
+	if (!transaction) {
+		return false;
+	}
 
-    const result = await evm.getReceipt(BigInt(transaction.blockNumber), hash);
-    if (!result.receipt || result.receipt.status !== 1) {
-        console.log("unexpected result", result);
-        console.log(result.receipt?.output?.toString("hex"));
-        return false;
-    }
+	const result = await evm.getReceipt(BigInt(transaction.blockNumber), hash);
+	if (!result.receipt || result.receipt.status !== 1) {
+		console.log("unexpected result", result);
+		console.log(result.receipt?.output?.toString("hex"));
+		return false;
+	}
 
-    return true;
+	return true;
 };
 
 export const getWallets = async (app: Contracts.Kernel.Application): Promise<Contracts.Crypto.KeyPair[]> => {
-    const walletKeyPairFactory = app.getTagged<Contracts.Crypto.KeyPairFactory>(
-        Identifiers.Cryptography.Identity.KeyPair.Factory,
-        "type",
-        "wallet",
-    );
+	const walletKeyPairFactory = app.getTagged<Contracts.Crypto.KeyPairFactory>(
+		Identifiers.Cryptography.Identity.KeyPair.Factory,
+		"type",
+		"wallet",
+	);
 
-    const secrets = app.config<string[]>("validators.secrets");
-    assert.defined(secrets);
+	const secrets = app.config<string[]>("validators.secrets");
+	assert.defined(secrets);
 
-    const wallets: Contracts.Crypto.KeyPair[] = [];
-    for (const secret of secrets.values()) {
-        const walletKeyPair = await walletKeyPairFactory.fromMnemonic(secret);
-        wallets.push(walletKeyPair);
-    }
+	const wallets: Contracts.Crypto.KeyPair[] = [];
+	for (const secret of secrets.values()) {
+		const walletKeyPair = await walletKeyPairFactory.fromMnemonic(secret);
+		wallets.push(walletKeyPair);
+	}
 
-    return wallets;
+	return wallets;
 };
 
 export const getRandomConsensusKeyPair = async ({
-    app,
+	app,
 }: {
-    app: Contracts.Kernel.Application;
+	app: Contracts.Kernel.Application;
 }): Promise<Contracts.Crypto.KeyPair> => {
-    const seed = Array.from({ length: 12 }).fill(Date.now().toString()).join(" ");
+	const seed = Array.from({ length: 12 }).fill(Date.now().toString()).join(" ");
 
-    return app
-        .getTagged<Contracts.Crypto.KeyPairFactory>(
-            Identifiers.Cryptography.Identity.KeyPair.Factory,
-            "type",
-            "consensus",
-        )
-        .fromMnemonic(seed);
+	return app
+		.getTagged<Contracts.Crypto.KeyPairFactory>(
+			Identifiers.Cryptography.Identity.KeyPair.Factory,
+			"type",
+			"consensus",
+		)
+		.fromMnemonic(seed);
 };
