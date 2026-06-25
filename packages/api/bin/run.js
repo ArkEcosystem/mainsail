fc9e8ce797d1801fbf7095842740e05d678e07ec
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-06-25 10:58:25 +0900

    preload jemalloc and lazy-load the CLI

--- packages/api/bin/run.js
+++ packages/api/bin/run.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import child_process from "child_process";
-import { CommandLineInterface } from "../distribution/index.js";
 
 const jemallocPath = child_process
 	.spawnSync(
@@ -13,6 +12,7 @@ const jemallocPath = child_process
 	.shift()
 	.trim();
 
+const alreadyPreloaded = (process.env.LD_PRELOAD || "").includes("libjemalloc");
 if (jemallocPath) {
 	if (process.env.LD_PRELOAD !== jemallocPath) {
 		process.env.LD_PRELOAD = jemallocPath;
@@ -33,6 +33,7 @@ if (jemallocPath) {
 
 const main = async () => {
 	try {
+		const { CommandLineInterface } = await import("../distribution/index.js");
 		const cmd = new CommandLineInterface(process.argv.slice(2));
 		await cmd.execute();
 	} catch (error) {
