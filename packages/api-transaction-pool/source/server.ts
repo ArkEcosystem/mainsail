88395b28d36fb4ef7ca113763358a1a2a5e3c895
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-07-07 15:58:04 +0900

    remove obsolete c8 ignores

--- packages/api-transaction-pool/source/server.ts
+++ packages/api-transaction-pool/source/server.ts
@@ -37,13 +37,11 @@ export class Server extends AbstractServer {
 			},
 			routes: {
 				payload: {
-					/* c8 ignore next 3 */
 					async failAction(request: Hapi.Request, h: Hapi.ResponseToolkit, error: Error) {
 						return badData(error.message);
 					},
 				},
 				validate: {
-					/* c8 ignore next 3 */
 					async failAction(request: Hapi.Request, h: Hapi.ResponseToolkit, error: Error) {
 						return badData(error.message);
 					},
