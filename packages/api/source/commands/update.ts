e8ca06744522397b1a83331c66f6c23fc9c6303f
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-07-07 15:30:33 +0000

    Fix update

--- packages/api/source/commands/update.ts
+++ packages/api/source/commands/update.ts
@@ -19,7 +19,7 @@ export class Command extends Commands.Command {
 		this.definition
 			.setFlag("force", "Force an update.", Joi.boolean().default(false))
 			.setFlag("updateProcessManager", "Update process manager.", Joi.boolean().default(false))
-			.setFlag("restart", "Restart all running processes.", Joi.boolean());
+			.setFlag("restart", "Restart all running processes.", Joi.boolean().default(false));
 	}
 
 	public async execute(): Promise<void> {
@@ -28,7 +28,7 @@ export class Command extends Commands.Command {
 		if (hasNewVersion) {
 			await this.updater.update(this.getFlag<boolean>("updateProcessManager"), this.getFlag<boolean>("force"));
 
-			if (this.hasFlag("restart")) {
+			if (this.getFlag<boolean>("restart")) {
 				this.actions.restartRunningProcess(`mainsail-api`);
 			} else if (!this.getFlag<boolean>("force")) {
 				await this.actions.restartRunningProcessWithPrompt(`mainsail-api`);
