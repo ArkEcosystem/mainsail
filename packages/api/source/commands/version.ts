163e5f5fa965459d55635a6e25baf973ff33d5bc
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-07-07 15:38:23 +0000

    Use logger in version

--- packages/api/source/commands/version.ts
+++ packages/api/source/commands/version.ts
@@ -2,15 +2,18 @@ import type { Contracts } from "@mainsail/contracts";
 
 import { Commands } from "@mainsail/cli";
 import { Identifiers } from "@mainsail/constants";
-import { injectable } from "@mainsail/container";
+import { injectable, inject } from "@mainsail/container";
 
 @injectable()
 export class Command extends Commands.Command {
+	@inject(Identifiers.Cli.Service.Logger)
+	private readonly logger!: Contracts.Cli.Logger;
+
 	public signature = "version";
 
 	public description = "Display the current installed version of API.";
 
 	public async execute(): Promise<void> {
-		console.log(this.app.get<Contracts.Types.PackageJson>(Identifiers.Cli.Package).version);
+		this.logger.info(this.app.get<{ version: string }>(Identifiers.Cli.Package).version);
 	}
 }
