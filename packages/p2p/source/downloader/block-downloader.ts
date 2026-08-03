336a03c433fdc27e0672ca76a8eb487f7ff019ac
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-08-03 15:43:45 +0900

    remove dead field

--- packages/p2p/source/downloader/block-downloader.ts
+++ packages/p2p/source/downloader/block-downloader.ts
@@ -15,7 +15,6 @@ enum JobStatus {
 
 type DownloadJob = {
 	peer: Contracts.P2P.Peer;
-	peerBlockNumber: number;
 	blockNumberFrom: number;
 	blockNumberTo: number;
 	blocks: Buffer[];
@@ -87,7 +86,6 @@ export class BlockDownloader implements Contracts.P2P.Downloader {
 			blockNumberTo: this.#calculateBlockNumberTo(peer, this.#getLastRequestedBlockNumber()),
 			blocks: [],
 			peer,
-			peerBlockNumber: peer.header.blockNumber - 1,
 			status: JobStatus.Downloading,
 		};
 
@@ -304,7 +302,6 @@ export class BlockDownloader implements Contracts.P2P.Downloader {
 			blockNumberTo,
 			blocks: [],
 			peer,
-			peerBlockNumber: peer.header.blockNumber - 1,
 			status: JobStatus.Downloading,
 		};
 
