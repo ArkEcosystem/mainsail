5e82483bf3bb3ead448e43309bbaec31a07f1de3
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-08-25 14:48:31 +0900

    pack the validator bitmaps on the wire

--- packages/p2p/source/socket-server/codecs/get-blocks.ts
+++ packages/p2p/source/socket-server/codecs/get-blocks.ts
@@ -1,17 +1,25 @@
+import { packHeadersBitmaps, unpackHeadersBitmaps } from "./headers.js";
 import { getBlocks as proto } from "./proto/protos.js";
 
 export const getBlocks = {
 	request: {
-		deserialize: (payload: Buffer): proto.IGetBlocksRequest => proto.GetBlocksRequest.decode(payload),
+		deserialize: (payload: Buffer): proto.IGetBlocksRequest => {
+			const decoded = proto.GetBlocksRequest.decode(payload);
+			unpackHeadersBitmaps(decoded);
+			return decoded;
+		},
 		serialize: (object: proto.IGetBlocksRequest): Buffer =>
-			Buffer.from(proto.GetBlocksRequest.encode(object).finish()),
+			Buffer.from(proto.GetBlocksRequest.encode(packHeadersBitmaps(object)).finish()),
 	},
 	response: {
-		deserialize: (payload: Buffer): proto.IGetBlocksResponse =>
-			proto.GetBlocksResponse.toObject(proto.GetBlocksResponse.decode(payload), {
+		deserialize: (payload: Buffer): proto.IGetBlocksResponse => {
+			const decoded = proto.GetBlocksResponse.toObject(proto.GetBlocksResponse.decode(payload), {
 				defaults: true,
-			}),
+			});
+			unpackHeadersBitmaps(decoded);
+			return decoded;
+		},
 		serialize: (object: proto.IGetBlocksResponse): Buffer =>
-			Buffer.from(proto.GetBlocksResponse.encode(object).finish()),
+			Buffer.from(proto.GetBlocksResponse.encode(packHeadersBitmaps(object)).finish()),
 	},
 };
--- packages/p2p/source/socket-server/codecs/get-proposal.ts
+++ packages/p2p/source/socket-server/codecs/get-proposal.ts
@@ -1,17 +1,25 @@
+import { packHeadersBitmaps, unpackHeadersBitmaps } from "./headers.js";
 import { getProposal as proto } from "./proto/protos.js";
 
 export const getProposal = {
 	request: {
-		deserialize: (payload: Buffer): proto.IGetProposalRequest => proto.GetProposalRequest.decode(payload),
+		deserialize: (payload: Buffer): proto.IGetProposalRequest => {
+			const decoded = proto.GetProposalRequest.decode(payload);
+			unpackHeadersBitmaps(decoded);
+			return decoded;
+		},
 		serialize: (object: proto.IGetProposalRequest): Buffer =>
-			Buffer.from(proto.GetProposalRequest.encode(object).finish()),
+			Buffer.from(proto.GetProposalRequest.encode(packHeadersBitmaps(object)).finish()),
 	},
 	response: {
-		deserialize: (payload: Buffer): proto.IGetProposalResponse =>
-			proto.GetProposalResponse.toObject(proto.GetProposalResponse.decode(payload), {
+		deserialize: (payload: Buffer): proto.IGetProposalResponse => {
+			const decoded = proto.GetProposalResponse.toObject(proto.GetProposalResponse.decode(payload), {
 				defaults: true,
-			}),
+			});
+			unpackHeadersBitmaps(decoded);
+			return decoded;
+		},
 		serialize: (object: proto.IGetProposalResponse): Buffer =>
-			Buffer.from(proto.GetProposalResponse.encode(object).finish()),
+			Buffer.from(proto.GetProposalResponse.encode(packHeadersBitmaps(object)).finish()),
 	},
 };
