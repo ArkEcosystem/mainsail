b8e5b2cababc20b62bdb86dc673da8c1ab6cad58
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-08-25 01:45:42 +0000

    update p2p

--- packages/p2p/source/peer-repository.ts
+++ packages/p2p/source/peer-repository.ts
@@ -3,7 +3,7 @@ import type { Contracts } from "@mainsail/contracts";
 import { Identifiers } from "@mainsail/constants";
 import { inject, injectable, tagged } from "@mainsail/container";
 import { assert, IpAddress } from "@mainsail/utils";
-import ip from "ip";
+import ipaddr from "ipaddr.js";
 
 @injectable()
 export class PeerRepository implements Contracts.P2P.PeerRepository {
@@ -86,10 +86,15 @@ export class PeerRepository implements Contracts.P2P.PeerRepository {
 	public getSameSubnetPeers(peerIp: string): Contracts.P2P.Peer[] {
 		return this.getPeers().filter((peer) => {
 			if (!IpAddress.isIPv6Address(peer.ip) && !IpAddress.isIPv6Address(peerIp)) {
-				return ip.cidr(`${peer.ip}/24`) === ip.cidr(`${peerIp}/24`);
+				return this.#networkAddress(peer.ip) === this.#networkAddress(peerIp);
 			}
 
 			return false;
 		});
+	}
+
+	// TODO: canonicalise peer IPs before they reach the repository.
+	#networkAddress(ip: string): string {
+		return ipaddr.IPv4.networkAddressFromCIDR(`${ip}/24`).toString();
 	}
 }
