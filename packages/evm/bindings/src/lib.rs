bde07aa99905b8cd8ad6a97421f65abb04262b3f
oXtxNt9U <120286271+oXtxNt9U@users.noreply.github.com>
2026-08-27 12:02:40 +0900

    expose block.timestamp in unix seconds instead of milliseconds

--- packages/evm/bindings/src/lib.rs
+++ packages/evm/bindings/src/lib.rs
@@ -1204,7 +1204,7 @@ impl EvmInner {
 
                 block_env.number = U256::from(block_ctx.commit_key.0);
                 block_env.beneficiary = block_ctx.validator_address;
-                block_env.timestamp = U256::from(block_ctx.timestamp);
+                block_env.timestamp = U256::from(block_ctx.timestamp / 1000);
                 block_env.gas_limit = block_ctx.gas_limit;
                 block_env.difficulty = U256::ZERO;
                 block_env.prevrandao = Some(block_ctx.prevrandao);
@@ -1324,7 +1324,7 @@ impl EvmInner {
                 };
                 block_env.number = U256::from(block_ctx.commit_key.0);
                 block_env.beneficiary = block_ctx.validator_address;
-                block_env.timestamp = U256::from(block_ctx.timestamp);
+                block_env.timestamp = U256::from(block_ctx.timestamp / 1000);
                 block_env.gas_limit = block_ctx.gas_limit;
                 block_env.difficulty = U256::ZERO;
                 block_env.prevrandao = Some(block_ctx.prevrandao);
