e9f4b6f3691f42ec4564376cd1728d2b3791f659
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-09-18 10:29:21 +0000

    Use app.fail

--- packages/evm-consensus/source/services/consensus-contract-caller.ts
+++ packages/evm-consensus/source/services/consensus-contract-caller.ts
@@ -39,9 +39,8 @@ export class ConsensusContractCaller {
 			to: this.consensusContractAddress,
 		});
 
-		// TODO: Check if terminate is the right way to handle this, or if we should throw an error instead.
 		if (!result.success) {
-			await this.app.terminate(`${functionName} failed`);
+			this.app.fail(`${functionName} failed`, new Error(`Consensus contract call ${functionName} failed`));
 		}
 
 		return decodeFunctionResult({
