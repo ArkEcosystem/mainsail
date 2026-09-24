b06756134458de92f3e373bd774071aa2a87ce35
sebastijankuzner <sebastijan.kuzner@outlook.com>
2026-09-24 09:56:47 +0000

    Use integer arithmetic on majority/minority

--- packages/blockchain-utils/source/is-majority.ts
+++ packages/blockchain-utils/source/is-majority.ts
@@ -1,1 +1,1 @@
-export const isMajority = (size: number, roundValidators: number): boolean => size > (roundValidators / 3) * 2;
+export const isMajority = (size: number, roundValidators: number): boolean => size * 3 > roundValidators * 2;
